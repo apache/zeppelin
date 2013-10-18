d7088d7ce2d3a1dfabbff510c25a4cb91113f157
Lee moon soo <leemoonsoo@gmail.com>
2013-10-18 12:14:14 +0900

    Interactive shell

--- zeppelin-cli/src/main/java/com/nflabs/zeppelin/cli/ZeppelinCli.java
+++ zeppelin-cli/src/main/java/com/nflabs/zeppelin/cli/ZeppelinCli.java
@@ -2,10 +2,14 @@ package com.nflabs.zeppelin.cli;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
+
+import jline.ConsoleReader;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -69,8 +73,58 @@ public class ZeppelinCli {
 		} else if(cmd.hasOption("h")){
 			HelpFormatter formatter = new HelpFormatter();
 			formatter.printHelp("ZeppelinCli", options);
-		} 
+		} else {
+			ZeppelinCli cli = new ZeppelinCli();
+			cli.run();
+		}
 		
 
 	}
+	
+	public ZeppelinCli(){
+		
+	}
+	
+	 private String readLine(ConsoleReader reader, String promtMessage)
+             throws IOException {
+	     String line = reader.readLine(promtMessage + "\nzeppelin> ");
+	     if(line==null) return null;
+	     else return line.trim();
+	 }
+	 
+	 public void run() throws IOException{
+		 printWelcomeMessage();
+         ConsoleReader reader = new ConsoleReader();
+         reader.setBellEnabled(false);
+         String line;
+
+         while ((line = readLine(reader, "")) != null) {
+        	 ZQL zql;
+			try {
+				zql = new ZQL(line);
+	 			List<Z> zs = zql.eval();			
+				
+	 			for(Z z : zs){
+	 				List<ResultSet> results = z.execute();
+	 				for(ResultSet r : results){
+	 					new ResultDataStream(r, System.out).load();
+	 				}
+	 			}
+			} catch (ZException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (ResultDataException e) {
+				e.printStackTrace();
+			} catch (ZQLException e) {
+				e.printStackTrace();
+			}
+
+         }
+	 }
+
+	 private void printWelcomeMessage() {
+		System.out.println("Welcome to Zeppelin.");
+
+	 }
 }
