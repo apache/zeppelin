abff7dc3198808463694088ae2bc5f4f2bea5e6d
Lee moon soo <leemoonsoo@gmail.com>
2013-10-18 09:25:21 +0900

    Refactoring packagename and version

--- zeppelin-cli/src/main/java/com/nflabs/zeppelin/cli/ZeppelinCli.java
+++ zeppelin-cli/src/main/java/com/nflabs/zeppelin/cli/ZeppelinCli.java
@@ -1,5 +1,36 @@
 package com.nflabs.zeppelin.cli;
 
-public class ZeppelinCli {
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
+public class ZeppelinCli {
+	public static void main(String args[]) throws ParseException{
+	
+		Options options = new Options();
+		options.addOption(OptionBuilder.withArgName("File")
+									   .withDescription("Run ZQL in given text file")
+									   .hasArg(true)
+									   .create("e"));
+		options.addOption(OptionBuilder.withDescription("See this messages")
+									   .withLongOpt("help")
+									   .create("h"));
+									 
+				
+		
+		CommandLineParser parser = new BasicParser();
+		CommandLine cmd = parser.parse(options, args);
+		
+		if(cmd.hasOption("e")){
+			
+		} else if(cmd.hasOption("h")){
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("ZeppelinCli", options);
+		}
+		
+	}
 }
