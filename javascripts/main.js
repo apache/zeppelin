eaf764d978858ad88c7e8e0a9fdc4507cf25d8b2
Lee moon soo <leemoonsoo@gmail.com>
2013-10-26 08:24:19 +0900

    Removing gray colors from homepage. Fill out install menu

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,9 +5,9 @@ App.Router.map(function(){
     this.route("zengine", { path:"/zengine"});
     this.route("zql", { path:"/zql"});
     this.route("zan", { path:"/zan"});
+    this.route("download", { path:"/download"});
     this.route("docZeppelinGettingstarted", { path:"/doc/zeppelin/getting_started"});
     this.route("docZeppelinInstall", { path:"/doc/zeppelin/install"});
-    this.route("docZeppelinConfigure", { path:"/doc/zeppelin/configure"});
 });
 
 
