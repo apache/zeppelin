c46a252739524bc02d86374d393f31182f415726
Lee moon soo <leemoonsoo@gmail.com>
2013-10-26 13:19:18 +0900

    Add analytics code

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,7 +13,29 @@ App.Router.map(function(){
 
 
 App.ApplicationController = Ember.Controller.extend({
-    
+    currentPathChanged: function() {
+	var page;
+
+	// window.location gets updated later in the current run loop, so we will
+	// wait until the next run loop to inspect its value and make the call
+	// to track the page view
+	Ember.run.next(function() {
+	    console.log("Changed %o", ga);
+	    // Track the page in Google Analytics
+	    if (!Ember.isNone(ga)) {
+		// Assume that if there is a hash component to the url then we are using
+		// the hash location strategy. Otherwise, we'll assume the history
+		// strategy.
+		page = window.location.hash.length > 0 ?
+		    window.location.hash.substring(1) :
+		    window.location.pathname;
+
+		// You need your Google Analytics code already loaded for _ga to be initialized
+		ga(['_trackPageview', page]);
+	    }
+	});
+    }.observes('currentPath')
+
 });
 
 
