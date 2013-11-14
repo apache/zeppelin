1d7deb5b165529313e71a92cb41db594b618a813
Lee moon soo <leemoonsoo@gmail.com>
2013-11-14 16:47:03 +0900

    applying hashbang ZEPPELIN-49

--- javascripts/main.js
+++ javascripts/main.js
@@ -20,6 +20,10 @@ App.Router.map(function(){
     this.route("howtocontribute", { path:"/development/howtocontribute"});
 });
 
+App.Router.reopen({
+    location: 'hashbang'
+});
+
 
 App.ApplicationController = Ember.Controller.extend({
     currentPathChanged: function() {
@@ -46,3 +50,43 @@ App.ApplicationController = Ember.Controller.extend({
 
 
 
+// hashbang
+(function() {
+
+var get = Ember.get, set = Ember.set;
+
+Ember.Location.registerImplementation('hashbang', Ember.HashLocation.extend({   
+
+  getURL: function() {
+    return get(this, 'location').hash.substr(2);
+  },
+
+  setURL: function(path) {
+    get(this, 'location').hash = "!"+path;
+    set(this, 'lastSetURL', path);
+  },
+
+  onUpdateURL: function(callback) {
+    var self = this;
+    var guid = Ember.guidFor(this);
+
+    Ember.$(window).bind('hashchange.ember-location-'+guid, function() {
+      Ember.run(function() {
+        var path = location.hash.substr(2);
+        if (get(self, 'lastSetURL') === path) { return; }
+
+        set(self, 'lastSetURL', null);
+
+        callback(path);
+      });
+    });
+  },
+
+  formatURL: function(url) {
+    return '#!'+url;
+  }
+
+  })
+);
+
+})();
