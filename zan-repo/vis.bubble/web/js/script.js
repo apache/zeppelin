fb3a9b9a1c3657c3fce604bf9fa8bb5d91d4aedf
Lee moon soo <leemoonsoo@gmail.com>
2013-12-03 23:20:24 +0900

    Bubble chart visualization Come from https://github.com/vlandham/gates_bubbles

---
+++ zan-repo/vis.bubble/web/js/script.js
@@ -0,0 +1,36 @@
+function addCommas(nStr)
+{
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
