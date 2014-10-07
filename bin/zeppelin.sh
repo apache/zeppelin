8b52a1571b81313310574420354aefdd56e4131d
anthony corbacho <corbacho.anthony@gmail.com>
2014-10-07 12:38:50 +0900

    Add zeppelin foreground script

---
+++ bin/zeppelin.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Copyright 2007 The Apache Software Foundation
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Run Zeppelin 
+#
+
+
+bin=`dirname "${BASH_SOURCE-$0}"`
+bin=`cd "$bin">/dev/null; pwd`
+
+. $bin/common.sh
+
+HOSTNAME=`hostname`
+ZEPPELIN_LOGFILE=$ZEPPELIN_LOG_DIR/zeppelin-$ZEPPELIN_IDENT_STRING-$HOSTNAME.log
+log=$ZEPPELIN_LOG_DIR/zeppelin-cli-$ZEPPELIN_IDENT_STRING-$HOSTNAME.out
+pid=$ZEPPELIN_PID_DIR/zeppelin-cli-$ZEPPELIN_IDENT_STRING-$HOSTNAME.pid
+
+ZEPPELIN_SERVER=com.nflabs.zeppelin.server.ZeppelinServer
+JAVA_OPTS+=" -Dzeppelin.log.file=$ZEPPELIN_LOGFILE"
+
+if [[ ! -d "$ZEPPELIN_LOG_DIR" ]]; then
+  echo "Log dir doesn't exist, create $ZEPPELIN_LOG_DIR"
+  mkdir -p "$ZEPPELIN_LOG_DIR"
+fi
+
+if [[ ! -d "$ZEPPELIN_PID_DIR" ]]; then
+  echo "Pid dir doesn't exist, create $ZEPPELIN_PID_DIR"
+  mkdir -p "$ZEPPELIN_PID_DIR"
+fi
+
+if [[ ! -d "$ZEPPELIN_NOTEBOOK_DIR" ]]; then
+  echo "Pid dir doesn't exist, create $ZEPPELIN_NOTEBOOK_DIR"
+  mkdir -p "$ZEPPELIN_NOTEBOOK_DIR"
+fi
+
+$ZEPPELIN_RUNNER $JAVA_OPTS -cp $CLASSPATH $ZEPPELIN_SERVER "$@"
