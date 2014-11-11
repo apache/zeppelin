cc57b01de22f07c19d91af9864668cd2c309b38c
Lee moon soo <leemoonsoo@gmail.com>
2014-11-11 17:54:36 +0900

    Fix problem finding war

--- bin/common.sh
+++ bin/common.sh
@@ -46,7 +46,7 @@ if [ "x$ZEPPELIN_WAR" == "x" ]; then
     if [ -d "${ZEPPELIN_HOME}/zeppelin-web/src/main/webapp" ]; then
 	    export ZEPPELIN_WAR="${ZEPPELIN_HOME}/zeppelin-web/src/main/webapp"
     else
-        export ZEPPELIN_WAR=`find ${ZEPPELIN_HOME}/zeppelin-web -name "zeppelin-web*.war"`
+        export ZEPPELIN_WAR=`find ${ZEPPELIN_HOME} -name "zeppelin-web*.war"`
     fi
 fi
 
@@ -54,7 +54,7 @@ if [ "x$ZEPPELIN_API_WAR" == "x" ]; then
     if [ -d "${ZEPPELIN_HOME}/zeppelin-docs/src/main/swagger" ]; then
 	    export ZEPPELIN_API_WAR="${ZEPPELIN_HOME}/zeppelin-docs/src/main/swagger"
     else
-        export ZEPPELIN_API_WAR=`find ${ZEPPELIN_HOME}/zeppelin-docs -name "zeppelin-api-ui*.war"`
+        export ZEPPELIN_API_WAR=`find ${ZEPPELIN_HOME}/ -name "zeppelin-api-ui*.war"`
     fi
 fi
 
