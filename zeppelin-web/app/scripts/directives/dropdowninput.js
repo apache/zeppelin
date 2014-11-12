efeebcf144b65ad0a97ff1bb42a3bca1e61f4721
Lee moon soo <leemoonsoo@gmail.com>
2014-11-12 19:08:13 +0900

    add dropdowninput directive

---
+++ zeppelin-web/app/scripts/directives/dropdowninput.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('zeppelinWebApp').directive('dropdownInput', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            element.bind('click', function (event) {
+                event.stopPropagation();
+            });
+        }
+    };
+});
