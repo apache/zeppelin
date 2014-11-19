68ea4b509102e2196072de880b826d097367e713
Damien Corneau <corneadoug@gmail.com>
2014-11-19 12:10:00 +0900

    Remove Grunt's cdnify

--- zeppelin-web/Gruntfile.js
+++ zeppelin-web/Gruntfile.js
@@ -299,13 +299,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -414,7 +407,6 @@ module.exports = function (grunt) {
     'concat',
     'ngmin',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     /*'filerev',*/
