0c34f1685fb53906f20d219346e8f8dc7cbcbf7e
anthony corbacho <corbacho.anthony@gmail.com>
2014-11-20 15:08:27 +0900

    Update shell ::: convention code

--- shell/src/main/java/com/nflabs/zeppelin/shell/ShellInterpreter.java
+++ shell/src/main/java/com/nflabs/zeppelin/shell/ShellInterpreter.java
@@ -20,82 +20,85 @@ import com.nflabs.zeppelin.interpreter.InterpreterResult.Code;
 import com.nflabs.zeppelin.scheduler.Scheduler;
 import com.nflabs.zeppelin.scheduler.SchedulerFactory;
 
+/**
+ * Shell interpreter for Zeppelin.
+ * 
+ * @author Leemoonsoo
+ * @author anthonycorbacho
+ *
+ */
 public class ShellInterpreter extends Interpreter {
-	Logger logger = LoggerFactory.getLogger(ShellInterpreter.class);
-	int CMD_TIMEOUT = 600000;
-	
-	static {
-		Interpreter.register("sh", ShellInterpreter.class.getName());
-	}
-	
-	public ShellInterpreter(Properties property) {
-		super(property);
-	}
+  Logger logger = LoggerFactory.getLogger(ShellInterpreter.class);
+  int commandTimeOut = 600000;
 
-	@Override
-	public void open() {
-	}
+  static {
+    Interpreter.register("sh", ShellInterpreter.class.getName());
+  }
 
-	@Override
-	public void close() {
-	}
+  public ShellInterpreter(Properties property) {
+    super(property);
+  }
 
-	@Override
-	public Object getValue(String name) {
-		return null;
-	}
+  @Override
+  public void open() {}
 
-	@Override
-	public InterpreterResult interpret(String cmd) {
-		logger.info("Run shell command '"+cmd+"'");
-		long start = System.currentTimeMillis();
-		CommandLine cmdLine = CommandLine.parse("bash");
-		cmdLine.addArgument("-c", false);
-		cmdLine.addArgument(cmd, false);
-		DefaultExecutor executor = new DefaultExecutor();
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		executor.setStreamHandler(new PumpStreamHandler(outputStream));
+  @Override
+  public void close() {}
 
-		executor.setWatchdog(new ExecuteWatchdog(CMD_TIMEOUT));
-		try {
-			int exitValue = executor.execute(cmdLine);
-			return new InterpreterResult(InterpreterResult.Code.SUCCESS, outputStream.toString());
-		} catch (ExecuteException e) {
-			logger.error("Can not run "+cmd, e);
-			return new InterpreterResult(Code.ERROR, e.getMessage());
-		} catch (IOException e) {
-			logger.error("Can not run "+cmd, e);
-			return new InterpreterResult(Code.ERROR, e.getMessage());
-		}
-	}
-	
+  @Override
+  public Object getValue(String name) {
+    return null;
+  }
 
-	@Override
-	public void cancel() {
-	}
+  @Override
+  public InterpreterResult interpret(String cmd) {
+    logger.info("Run shell command '" + cmd + "'");
+    long start = System.currentTimeMillis();
+    CommandLine cmdLine = CommandLine.parse("bash");
+    cmdLine.addArgument("-c", false);
+    cmdLine.addArgument(cmd, false);
+    DefaultExecutor executor = new DefaultExecutor();
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    executor.setStreamHandler(new PumpStreamHandler(outputStream));
 
-	@Override
-	public void bindValue(String name, Object o) {
-	}
+    executor.setWatchdog(new ExecuteWatchdog(commandTimeOut));
+    try {
+      int exitValue = executor.execute(cmdLine);
+      return new InterpreterResult(InterpreterResult.Code.SUCCESS, outputStream.toString());
+    } catch (ExecuteException e) {
+      logger.error("Can not run " + cmd, e);
+      return new InterpreterResult(Code.ERROR, e.getMessage());
+    } catch (IOException e) {
+      logger.error("Can not run " + cmd, e);
+      return new InterpreterResult(Code.ERROR, e.getMessage());
+    }
+  }
 
-	@Override
-	public FormType getFormType() {
-		return FormType.SIMPLE;
-	}
+  @Override
+  public void cancel() {}
 
-	@Override
-	public int getProgress() {
-		return 0;
-	}
-	
-	@Override
-	public Scheduler getScheduler() {
-		return SchedulerFactory.singleton().createOrGetFIFOScheduler(ShellInterpreter.class.getName()+this.hashCode());
-	}
+  @Override
+  public void bindValue(String name, Object o) {}
 
-	@Override
-	public List<String> completion(String buf, int cursor) {
-		return null;
-	}
+  @Override
+  public FormType getFormType() {
+    return FormType.SIMPLE;
+  }
+
+  @Override
+  public int getProgress() {
+    return 0;
+  }
+
+  @Override
+  public Scheduler getScheduler() {
+    return SchedulerFactory.singleton().createOrGetFIFOScheduler(
+        ShellInterpreter.class.getName() + this.hashCode());
+  }
+
+  @Override
+  public List<String> completion(String buf, int cursor) {
+    return null;
+  }
 
 }
