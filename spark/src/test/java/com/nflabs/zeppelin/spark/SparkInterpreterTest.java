6315964963ed5ca6e05ff2cf101cdcec1342a0d0
Lee moon soo <leemoonsoo@gmail.com>
2015-01-13 12:36:18 +0900

    Fix unittest

--- spark/src/test/java/com/nflabs/zeppelin/spark/SparkInterpreterTest.java
+++ spark/src/test/java/com/nflabs/zeppelin/spark/SparkInterpreterTest.java
@@ -9,12 +9,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.nflabs.zeppelin.interpreter.InterpreterContext;
 import com.nflabs.zeppelin.interpreter.InterpreterResult;
 import com.nflabs.zeppelin.interpreter.InterpreterResult.Code;
+import com.nflabs.zeppelin.notebook.Paragraph;
 
 
 public class SparkInterpreterTest {
 	public static SparkInterpreter repl;
+  private InterpreterContext context;
 
 	@Before
 	public void setUp() throws Exception {
@@ -24,6 +27,8 @@ public class SparkInterpreterTest {
 	    repl = new SparkInterpreter(p);
   	  repl.open();
 	  }
+
+    context = new InterpreterContext(new Paragraph(null, null));
 	}
 
 	@After
@@ -32,10 +37,10 @@ public class SparkInterpreterTest {
 
 	@Test
 	public void testBasicIntp() {
-		assertEquals(InterpreterResult.Code.SUCCESS, repl.interpret("val a = 1\nval b = 2", null).code());
+		assertEquals(InterpreterResult.Code.SUCCESS, repl.interpret("val a = 1\nval b = 2", context).code());
 
 		// when interpret incomplete expression
-		InterpreterResult incomplete = repl.interpret("val a = \"\"\"", null);
+		InterpreterResult incomplete = repl.interpret("val a = \"\"\"", context);
 		assertEquals(InterpreterResult.Code.INCOMPLETE, incomplete.code());
 		assertTrue(incomplete.message().length()>0); // expecting some error message
 		/*
@@ -49,23 +54,23 @@ public class SparkInterpreterTest {
 
 	@Test
 	public void testEndWithComment() {
-		assertEquals(InterpreterResult.Code.SUCCESS, repl.interpret("val c=1\n//comment", null).code());
+		assertEquals(InterpreterResult.Code.SUCCESS, repl.interpret("val c=1\n//comment", context).code());
 	}
 
 	@Test
 	public void testSparkSql(){
-		repl.interpret("case class Person(name:String, age:Int)", null);
-		repl.interpret("val people = sc.parallelize(Seq(Person(\"moon\", 33), Person(\"jobs\", 51), Person(\"gates\", 51), Person(\"park\", 34)))", null);
-		assertEquals(Code.SUCCESS, repl.interpret("people.take(3)", null).code());
+		repl.interpret("case class Person(name:String, age:Int)", context);
+		repl.interpret("val people = sc.parallelize(Seq(Person(\"moon\", 33), Person(\"jobs\", 51), Person(\"gates\", 51), Person(\"park\", 34)))", context);
+		assertEquals(Code.SUCCESS, repl.interpret("people.take(3)", context).code());
 
 		// create new interpreter
 		Properties p = new Properties();
 		SparkInterpreter repl2 = new SparkInterpreter(p);
 		repl2.open();
 
-		repl.interpret("case class Man(name:String, age:Int)", null);
-		repl.interpret("val man = sc.parallelize(Seq(Man(\"moon\", 33), Man(\"jobs\", 51), Man(\"gates\", 51), Man(\"park\", 34)))", null);
-		assertEquals(Code.SUCCESS, repl.interpret("man.take(3)", null).code());
+		repl.interpret("case class Man(name:String, age:Int)", context);
+		repl.interpret("val man = sc.parallelize(Seq(Man(\"moon\", 33), Man(\"jobs\", 51), Man(\"gates\", 51), Man(\"park\", 34)))", context);
+		assertEquals(Code.SUCCESS, repl.interpret("man.take(3)", context).code());
 		repl2.getSparkContext().stop();
 	}
 
@@ -73,7 +78,7 @@ public class SparkInterpreterTest {
 	public void testReferencingUndefinedVal(){
 		InterpreterResult result = repl.interpret("def category(min: Int) = {" +
 				       "    if (0 <= value) \"error\"" +
-                       "}", null);
+                       "}", context);
 		assertEquals(Code.ERROR, result.code());
 		System.out.println("msg="+result.message());
 	}
--- spark/src/test/java/com/nflabs/zeppelin/spark/SparkSqlInterpreterTest.java
+++ spark/src/test/java/com/nflabs/zeppelin/spark/SparkSqlInterpreterTest.java
@@ -8,14 +8,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.nflabs.zeppelin.interpreter.InterpreterContext;
 import com.nflabs.zeppelin.interpreter.InterpreterGroup;
 import com.nflabs.zeppelin.interpreter.InterpreterResult;
 import com.nflabs.zeppelin.interpreter.InterpreterResult.Type;
+import com.nflabs.zeppelin.notebook.Paragraph;
 
 public class SparkSqlInterpreterTest {
 
 	private SparkSqlInterpreter sql;
   private SparkInterpreter repl;
+  private InterpreterContext context;
 
 	@Before
 	public void setUp() throws Exception {
@@ -39,6 +42,7 @@ public class SparkSqlInterpreterTest {
 		  intpGroup.add(sql);
 		  sql.setInterpreterGroup(intpGroup);
 		}
+		context = new InterpreterContext(new Paragraph(null, null));
 	}
 
 	@After
@@ -47,40 +51,40 @@ public class SparkSqlInterpreterTest {
 
 	@Test
 	public void test() {
-		repl.interpret("case class Test(name:String, age:Int)", null);
-		repl.interpret("val test = sc.parallelize(Seq(Test(\"moon\", 33), Test(\"jobs\", 51), Test(\"gates\", 51), Test(\"park\", 34)))", null);
-		repl.interpret("test.registerAsTable(\"test\")", null);
+		repl.interpret("case class Test(name:String, age:Int)", context);
+		repl.interpret("val test = sc.parallelize(Seq(Test(\"moon\", 33), Test(\"jobs\", 51), Test(\"gates\", 51), Test(\"park\", 34)))", context);
+		repl.interpret("test.registerAsTable(\"test\")", context);
 
-		InterpreterResult ret = sql.interpret("select name, age from test where age < 40", null);
+		InterpreterResult ret = sql.interpret("select name, age from test where age < 40", context);
 		assertEquals(InterpreterResult.Code.SUCCESS, ret.code());
 		assertEquals(Type.TABLE, ret.type());
 		assertEquals("name\tage\nmoon\t33\npark\t34\n", ret.message());
 
-		assertEquals(InterpreterResult.Code.ERROR, sql.interpret("select wrong syntax", null).code());
-		assertEquals(InterpreterResult.Code.ERROR, sql.interpret("select case when name==\"aa\" then name else name end from people", null).code());
+		assertEquals(InterpreterResult.Code.ERROR, sql.interpret("select wrong syntax", context).code());
+		assertEquals(InterpreterResult.Code.ERROR, sql.interpret("select case when name==\"aa\" then name else name end from people", context).code());
 	}
 
 	@Test
 	public void testStruct(){
-		repl.interpret("case class Person(name:String, age:Int)", null);
-		repl.interpret("case class People(group:String, person:Person)", null);
-		repl.interpret("val gr = sc.parallelize(Seq(People(\"g1\", Person(\"moon\",33)), People(\"g2\", Person(\"sun\",11))))", null);
-		repl.interpret("gr.registerAsTable(\"gr\")", null);
-		InterpreterResult ret = sql.interpret("select * from gr", null);
+		repl.interpret("case class Person(name:String, age:Int)", context);
+		repl.interpret("case class People(group:String, person:Person)", context);
+		repl.interpret("val gr = sc.parallelize(Seq(People(\"g1\", Person(\"moon\",33)), People(\"g2\", Person(\"sun\",11))))", context);
+		repl.interpret("gr.registerAsTable(\"gr\")", context);
+		InterpreterResult ret = sql.interpret("select * from gr", context);
 		assertEquals(InterpreterResult.Code.SUCCESS, ret.code());
 	}
 
 	@Test
 	public void test_null_value_in_row() {
-		repl.interpret("import org.apache.spark.sql._", null);
-		repl.interpret("def toInt(s:String): Any = {try { s.trim().toInt} catch {case e:Exception => null}}", null);
-		repl.interpret("val schema = StructType(Seq(StructField(\"name\", StringType, false),StructField(\"age\" , IntegerType, true),StructField(\"other\" , StringType, false)))", null);
-		repl.interpret("val csv = sc.parallelize(Seq((\"jobs, 51, apple\"), (\"gates, , microsoft\")))", null);
-		repl.interpret("val raw = csv.map(_.split(\",\")).map(p => Row(p(0),toInt(p(1)),p(2)))", null);
-		repl.interpret("val people = z.sqlContext.applySchema(raw, schema)", null);
-		repl.interpret("people.registerTempTable(\"people\")", null);
+		repl.interpret("import org.apache.spark.sql._", context);
+		repl.interpret("def toInt(s:String): Any = {try { s.trim().toInt} catch {case e:Exception => null}}", context);
+		repl.interpret("val schema = StructType(Seq(StructField(\"name\", StringType, false),StructField(\"age\" , IntegerType, true),StructField(\"other\" , StringType, false)))", context);
+		repl.interpret("val csv = sc.parallelize(Seq((\"jobs, 51, apple\"), (\"gates, , microsoft\")))", context);
+		repl.interpret("val raw = csv.map(_.split(\",\")).map(p => Row(p(0),toInt(p(1)),p(2)))", context);
+		repl.interpret("val people = z.sqlContext.applySchema(raw, schema)", context);
+		repl.interpret("people.registerTempTable(\"people\")", context);
 
-		InterpreterResult ret = sql.interpret("select name, age from people where name = 'gates'", null);
+		InterpreterResult ret = sql.interpret("select name, age from people where name = 'gates'", context);
 		System.err.println("RET=" + ret.message());
 		assertEquals(InterpreterResult.Code.SUCCESS, ret.code());
 		assertEquals(Type.TABLE, ret.type());
