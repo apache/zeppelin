6c8172c7d0e260ff8ac2d19e1263cc4f3c21ef24
Lee moon soo <leemoonsoo@gmail.com>
2015-01-23 11:43:52 +0900

    Add rest api test

--- zeppelin-server/src/test/java/com/nflabs/zeppelin/rest/ZeppelinRestApiTest.java
+++ zeppelin-server/src/test/java/com/nflabs/zeppelin/rest/ZeppelinRestApiTest.java
@@ -1,9 +1,10 @@
 package com.nflabs.zeppelin.rest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,8 +16,9 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.nflabs.zeppelin.notebook.Note;
+import com.nflabs.zeppelin.server.ZeppelinServer;
 /**
  * BASIC Zeppelin rest api tests
  * TODO: Add Post,Put,Delete test and method
@@ -26,6 +28,7 @@ import com.google.gson.JsonParser;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ZeppelinRestApiTest extends AbstractTestRestApi {
+  Gson gson = new Gson();
 
   @BeforeClass
   public static void init() throws Exception {
@@ -47,5 +50,46 @@ public class ZeppelinRestApiTest extends AbstractTestRestApi {
     // then
     assertThat(httpGetRoot, isAllowed());
     httpGetRoot.releaseConnection();
+  }
+
+
+  @Test
+  public void getAvailableInterpreters() throws IOException {
+    // when
+    GetMethod get = httpGet("/interpreter");
+
+    // then
+    assertThat(get, isAllowed());
+    Map<String, Object> resp = gson.fromJson(get.getResponseBodyAsString(), new TypeToken<Map<String, Object>>(){}.getType());
+    Map<String, Object> body = (Map<String, Object>) resp.get("body");
+    assertEquals(4, body.size());
+    get.releaseConnection();
+  }
+
+  @Test
+  public void getSettings() throws IOException {
+    // when
+    GetMethod get = httpGet("/interpreter/setting");
+
+    // then
+    Map<String, Object> resp = gson.fromJson(get.getResponseBodyAsString(), new TypeToken<Map<String, Object>>(){}.getType());
+    assertThat(get, isAllowed());
+    get.releaseConnection();
+  }
+
+
+  @Test
+  public void testInterpreterAutoBinding() throws IOException {
+    // create note
+    Note note = ZeppelinServer.notebook.createNote();
+
+    // check interpreter is bindded
+    GetMethod get = httpGet("/notebook/interpreter/bind/"+note.id());
+    assertThat(get, isAllowed());
+    Map<String, Object> resp = gson.fromJson(get.getResponseBodyAsString(), new TypeToken<Map<String, Object>>(){}.getType());
+    List<Map<String, String>> body = (List<Map<String, String>>) resp.get("body");
+    assertTrue(0 < body.size());
+
+    get.releaseConnection();
   }
 }
