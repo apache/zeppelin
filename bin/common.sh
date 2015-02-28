d7b7289f9fc901dba39442d5a177d0d90837beb6
Lee moon soo <leemoonsoo@gmail.com>
2015-03-01 08:49:09 +0900

    Env variable for interpreter process

--- bin/common.sh
+++ bin/common.sh
@@ -115,8 +115,16 @@ if [[ -z "$ZEPPELIN_MEM" ]]; then
   export ZEPPELIN_MEM="-Xmx1024m -XX:MaxPermSize=512m"
 fi
 
-JAVA_OPTS+="${ZEPPELIN_JAVA_OPTS} -Dfile.encoding=${ZEPPELIN_ENCODING} ${ZEPPELIN_MEM}"
+JAVA_OPTS+=" ${ZEPPELIN_JAVA_OPTS} -Dfile.encoding=${ZEPPELIN_ENCODING} ${ZEPPELIN_MEM}"
 export JAVA_OPTS
+
+if [[ -z "$ZEPPELIN_INTP_MEM" ]]; then
+  export ZEPPELIN_INTP_MEM="${ZEPPELIN_MEM}"
+fi
+
+JAVA_INTP_OPTS+=" ${ZEPPELIN_INTP_JAVA_OPTS} -Dfile.encoding=${ZEPPELIN_ENCODING} ${ZEPPELIN_INTP_MEM}"
+export JAVA_INTP_OPTS
+
 
 if [[ -n "${JAVA_HOME}" ]]; then
   ZEPPELIN_RUNNER="${JAVA_HOME}/bin/java"
--- bin/interpreter.sh
+++ bin/interpreter.sh
@@ -45,7 +45,7 @@ ZEPPELIN_SERVER=com.nflabs.zeppelin.interpreter.remote.RemoteInterpreterServer
 RANDOM_ID=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 ZEPPELIN_PID="${ZEPPELIN_PID_DIR}/zeppelin-interpreter-${RANDOM_ID}-${ZEPPELIN_IDENT_STRING}-${HOSTNAME}.pid"
 ZEPPELIN_LOGFILE="${ZEPPELIN_LOG_DIR}/zeppelin-interpreter-${RANDOM_ID}-${ZEPPELIN_IDENT_STRING}-${HOSTNAME}.log"
-JAVA_OPTS+=" -Dzeppelin.log.file=${ZEPPELIN_LOGFILE}"
+JAVA_INTP_OPTS+=" -Dzeppelin.log.file=${ZEPPELIN_LOGFILE}"
 
 if [[ ! -d "${ZEPPELIN_LOG_DIR}" ]]; then
   echo "Log dir doesn't exist, create ${ZEPPELIN_LOG_DIR}"
@@ -54,7 +54,7 @@ fi
 
 
 
-${ZEPPELIN_RUNNER} ${JAVA_OPTS} -cp ${CLASSPATH} ${ZEPPELIN_SERVER} ${PORT} &
+${ZEPPELIN_RUNNER} ${JAVA_INTP_OPTS} -cp ${CLASSPATH} ${ZEPPELIN_SERVER} ${PORT} &
 pid=$!
 if [[ -z "${pid}" ]]; then
   return 1;
