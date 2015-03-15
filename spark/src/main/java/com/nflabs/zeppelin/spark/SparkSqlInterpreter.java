c6350b625edbecbf07b5c6f089bde1d5f6b9c0aa
Lee moon soo <leemoonsoo@gmail.com>
2015-03-15 15:30:29 +0900

    Get progress information in %sql interpreter

--- spark/src/main/java/com/nflabs/zeppelin/spark/SparkSqlInterpreter.java
+++ spark/src/main/java/com/nflabs/zeppelin/spark/SparkSqlInterpreter.java
@@ -225,6 +225,8 @@ public class SparkSqlInterpreter extends Interpreter {
           progressInfo = getProgressFromStage_1_1x(sparkListener, job.finalStage());
         } else if (sc.version().startsWith("1.2")) {
           progressInfo = getProgressFromStage_1_1x(sparkListener, job.finalStage());
+        } else if (sc.version().startsWith("1.3")) {
+          progressInfo = getProgressFromStage_1_1x(sparkListener, job.finalStage());
         } else {
           logger.warn("Spark {} getting progress information not supported" + sc.version());
           continue;
