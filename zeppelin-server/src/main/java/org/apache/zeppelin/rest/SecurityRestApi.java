5960221e8f14ac1d25142f4379d3fedf3e4ce2e2
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-04-27 12:53:04 +0200

    Add Apache Licence

--- zeppelin-server/src/main/java/org/apache/zeppelin/rest/SecurityRestApi.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/rest/SecurityRestApi.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.zeppelin.rest;
 
 import org.apache.zeppelin.server.JsonResponse;
@@ -12,11 +29,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Zeppelin root rest api endpoint.
- *
- * @author anthonycorbacho
- * @since 0.3.4
+ * Created by hayssams on 24/04/15.
  */
+
 @Path("/security")
 @Produces("application/json")
 public class SecurityRestApi {
