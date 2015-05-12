8dd6ebd1c51849c292bae71926149414e083b0e6
Khalid Huseynov <khalidhnv@nflabs.com>
2015-05-12 15:37:12 +0900

    getProgress() implementation

--- tajo-thrift/src/main/java/org/apache/zeppelin/tajo/thrift/TajoInterpreter.java
+++ tajo-thrift/src/main/java/org/apache/zeppelin/tajo/thrift/TajoInterpreter.java
@@ -311,7 +311,25 @@ public class TajoInterpreter extends Interpreter {
   @Override
   public int getProgress(InterpreterContext context) {
 
-    return 0;
+    int result = 0;
+    logger.info("Getting progress information...");
+    try {
+      List<TBriefQueryInfo> queryList = tajoClient.getQueryList();
+
+      logger.info("\t " + queryList.size() + " queries");
+      if (queryList.isEmpty()) {
+        return result;
+      }
+
+      String queryId = queryList.get(queryList.size() - 1).queryId;
+      TGetQueryStatusResponse query = tajoClient.getQueryStatus(queryId);
+      logger.info("Done! " + query);
+      result = (int) query.progress * 100;
+
+    } catch (Exception e) {
+      logger.error("getProgress failed ", e);
+    }
+    return result;
   }
 
   @Override
