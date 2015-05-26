2524ddbecd817af3f7500c63bd0c6a8cc2fb37c8
Emmanuelle Raffenne <eraffenne@keedio.com>
2015-05-26 14:44:53 +0200

    Fix column name for group.

--- zeppelin-web/app/scripts/controllers/paragraph.js
+++ zeppelin-web/app/scripts/controllers/paragraph.js
@@ -1346,7 +1346,7 @@ angular.module('zeppelinWebApp')
       if (groups.length === 1 && values.length === 1) {
         for (d3gIndex = 0; d3gIndex < d3g.length; d3gIndex++) {
           var colName = d3g[d3gIndex].key;
-          colName = colName.split('.')[0];
+          colName = colName.substring(0, colName.lastIndexOf("."));
           d3g[d3gIndex].key = colName;
         }
       }
