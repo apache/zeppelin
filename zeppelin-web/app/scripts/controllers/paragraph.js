b74bdb66f0b56b5718f185782e19ca6152f2576c
Hyungu Roh <hyungu.roh@navercorp.com>
2015-05-28 11:23:12 +0900

    Fix abnormal height of chart in IE
    I fixed https://issues.apache.org/jira/browse/ZEPPELIN-91 issue.
    
    This issue is related to abnormal height of chart in IE. Please refer to the link below for more details.
    https://github.com/NFLabs/zeppelin/issues/417
    
    Author: Hyungu Roh <hyungu.roh@navercorp.com>
    
    Closes #85 from 1teed/fix_graph_height and squashes the following commits:
    
    29cc711 [Hyungu Roh] Fix abnormal height of chart in IE

--- zeppelin-web/app/scripts/controllers/paragraph.js
+++ zeppelin-web/app/scripts/controllers/paragraph.js
@@ -912,11 +912,11 @@ angular.module('zeppelinWebApp')
 
       var chartEl = d3.select('#p'+$scope.paragraph.id+'_'+type+' svg')
           .attr('height', $scope.paragraph.config.graph.height)
+          .style('height', height + 'px')
           .datum(d3g)
           .transition()
           .duration(animationDuration)
           .call($scope.chart[type]);
-      d3.select('#p'+$scope.paragraph.id+'_'+type+' svg').style.height = height+'px';
       nv.utils.windowResize($scope.chart[type].update);
     };
 
