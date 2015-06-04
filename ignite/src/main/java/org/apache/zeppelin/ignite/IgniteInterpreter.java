0f8c90e7af1dd3516d25a1a2c4f1bb73a005bcf5
Lee moon soo <moon@apache.org>
2015-06-04 18:08:58 +0900

    Ignite interpreter initial implementation with scala compiler

--- ignite/src/main/java/org/apache/zeppelin/ignite/IgniteInterpreter.java
+++ ignite/src/main/java/org/apache/zeppelin/ignite/IgniteInterpreter.java
@@ -16,8 +16,14 @@
  */
 package org.apache.zeppelin.ignite;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.ignite.Ignite;
@@ -25,14 +31,27 @@ import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.zeppelin.interpreter.Interpreter;
 import org.apache.zeppelin.interpreter.InterpreterContext;
-import org.apache.zeppelin.interpreter.InterpreterException;
 import org.apache.zeppelin.interpreter.InterpreterPropertyBuilder;
 import org.apache.zeppelin.interpreter.InterpreterResult;
+import org.apache.zeppelin.interpreter.InterpreterResult.Code;
+import org.apache.zeppelin.interpreter.InterpreterUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import scala.Console;
+import scala.None;
+import scala.Some;
+import scala.tools.nsc.Settings;
+import scala.tools.nsc.interpreter.IMain;
+import scala.tools.nsc.settings.MutableSettings.BooleanSetting;
+import scala.tools.nsc.settings.MutableSettings.PathSetting;
 
 /**
  * Interpreter for Apache Ignite (http://ignite.incubator.apache.org/)
  */
 public class IgniteInterpreter extends Interpreter {
+  Logger logger = LoggerFactory.getLogger(IgniteInterpreter.class);
+
   static {
     Interpreter.register(
         "ignite",
@@ -44,6 +63,10 @@ public class IgniteInterpreter extends Interpreter {
   }
 
   private Ignite ignite;
+  private Settings settings;
+  private ByteArrayOutputStream out;
+  private IMain imain;
+  private Map<String, Object> binder;
 
   public IgniteInterpreter(Properties property) {
     super(property);
@@ -51,21 +74,82 @@ public class IgniteInterpreter extends Interpreter {
 
   @Override
   public void open() {
-    getIgnite();
+    URL[] urls = getClassloaderUrls();
+    this.settings = new Settings();
+
+    // set classpath
+    PathSetting pathSettings = settings.classpath();
+    String classpath = "";
+    List<File> paths = currentClassPath();
+    for (File f : paths) {
+      if (classpath.length() > 0) {
+        classpath += File.pathSeparator;
+      }
+      classpath += f.getAbsolutePath();
+    }
+
+    if (urls != null) {
+      for (URL u : urls) {
+        if (classpath.length() > 0) {
+          classpath += File.pathSeparator;
+        }
+        classpath += u.getFile();
+      }
+    }
+
+    pathSettings.v_$eq(classpath);
+    settings.scala$tools$nsc$settings$ScalaSettings$_setter_$classpath_$eq(pathSettings);
+    settings.explicitParentLoader_$eq(new Some<ClassLoader>(Thread.currentThread()
+        .getContextClassLoader()));
+    BooleanSetting b = (BooleanSetting) settings.usejavacp();
+    b.v_$eq(true);
+    settings.scala$tools$nsc$settings$StandardScalaSettings$_setter_$usejavacp_$eq(b);
+
+    out = new ByteArrayOutputStream();
+    imain = new IMain(settings, new PrintWriter(out));
+
+    initializeIgnite();
   }
 
-  @Override
-  public void close() {
-    synchronized (this) {
-      ignite.close();
+  private List<File> currentClassPath() {
+    List<File> paths = classPath(Thread.currentThread().getContextClassLoader());
+    String[] cps = System.getProperty("java.class.path").split(File.pathSeparator);
+    if (cps != null) {
+      for (String cp : cps) {
+        paths.add(new File(cp));
+      }
+    }
+    return paths;
+  }
+
+  private List<File> classPath(ClassLoader cl) {
+    List<File> paths = new LinkedList<File>();
+    if (cl == null) {
+      return paths;
+    }
+
+    if (cl instanceof URLClassLoader) {
+      URLClassLoader ucl = (URLClassLoader) cl;
+      URL[] urls = ucl.getURLs();
+      if (urls != null) {
+        for (URL url : urls) {
+          paths.add(new File(url.getFile()));
+        }
+      }
+    }
+    return paths;
+  }
+
+  public Object getValue(String name) {
+    Object ret = imain.valueOfTerm(name);
+    if (ret instanceof None) {
+      return null;
+    } else if (ret instanceof Some) {
+      return ((Some) ret).get();
+    } else {
+      return ret;
     }
   }
-
-
-  public boolean isClientMode() {
-    return Boolean.parseBoolean(getProperty("ignite.clientMode"));
-  }
-
 
   public Ignite getIgnite() {
     synchronized (this) {
@@ -78,14 +162,97 @@ public class IgniteInterpreter extends Interpreter {
     }
   }
 
+  private void initializeIgnite() {
+    imain.interpret("@transient var _binder = new java.util.HashMap[String, Object]()");
+    binder = (Map<String, Object>) getValue("_binder");
+
+    getIgnite();
+
+    binder.put("ignite", ignite);
+
+    imain.interpret("@transient val ignite = "
+        + "_binder.get(\"ignite\")"
+        + ".asInstanceOf[org.apache.ignite.Ignite]");
+  }
+
+
   @Override
-  public InterpreterResult interpret(String st, InterpreterContext context) {
-    throw new InterpreterException("Not implemented yet");
+  public void close() {
+    synchronized (this) {
+      ignite.close();
+      imain.close();
+    }
+  }
+
+
+  public boolean isClientMode() {
+    return Boolean.parseBoolean(getProperty("ignite.clientMode"));
+  }
+
+
+
+  @Override
+  public InterpreterResult interpret(String line, InterpreterContext context) {
+    if (line == null || line.trim().length() == 0) {
+      return new InterpreterResult(Code.SUCCESS);
+    }
+
+    InterpreterResult result = interpret(line.split("\n"), context);
+    return result;
   }
 
   @Override
   public void cancel(InterpreterContext context) {
 
+  }
+
+  public InterpreterResult interpret(String[] lines, InterpreterContext context) {
+    String[] linesToRun = new String[lines.length + 1];
+    for (int i = 0; i < lines.length; i++) {
+      linesToRun[i] = lines[i];
+    }
+    linesToRun[lines.length] = "print(\"\")";
+
+    Console.setOut(out);
+    out.reset();
+    Code r = null;
+
+    String incomplete = "";
+    for (String s : linesToRun) {
+      scala.tools.nsc.interpreter.Results.Result res = null;
+      try {
+        res = imain.interpret(incomplete + s);
+      } catch (Exception e) {
+        logger.info("Interpreter exception", e);
+        return new InterpreterResult(Code.ERROR, InterpreterUtils.getMostRelevantMessage(e));
+      }
+
+      r = getResultCode(res);
+
+      if (r == Code.ERROR) {
+        return new InterpreterResult(r, out.toString());
+      } else if (r == Code.INCOMPLETE) {
+        incomplete += s + "\n";
+      } else {
+        incomplete = "";
+      }
+    }
+
+    if (r == Code.INCOMPLETE) {
+      return new InterpreterResult(r, "Incomplete expression");
+    } else {
+      return new InterpreterResult(r, out.toString());
+    }
+  }
+
+  private Code getResultCode(scala.tools.nsc.interpreter.Results.Result r) {
+    if (r instanceof scala.tools.nsc.interpreter.Results.Success$) {
+      return Code.SUCCESS;
+    } else if (r instanceof scala.tools.nsc.interpreter.Results.Incomplete$) {
+      return Code.INCOMPLETE;
+    } else {
+      return Code.ERROR;
+    }
   }
 
   @Override
