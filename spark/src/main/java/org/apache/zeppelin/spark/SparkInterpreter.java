23922ae488fc63b50d87524421cd93c3dba3024c
DuyHai DOAN <doanduyhai@gmail.com>
2015-06-12 13:35:48 +0200

    Add Scala utility functions for display
    Until now, to display data as a table, there are 2 alternatives:
    
    1. Either use **Spark DataFrame** and Zeppeline built-in support
    2. Or generate manually a `println(%table ...)`. As an example of displaying an `RDD[(String,String,Int)]` representing a collection of users:
    
    ```scala
    val data = new java.lang.StringBuilder("%table Login\tName\tAge\n")
    rdd.foreach {
       case (login,name,age) => data.append(s"$login\t$name\t$age\n")
    }
    
    println(data.toString())
    ```
    
    My proposal is to add a new utility function to make creating tables easier that the code example above. Of course one can always use **Spark DataFrame** but I find it quite restrictive. People using Spark versions lesser than 1.3 cannot rely on DataFrame and sometimes one does not want to transform an RDD to DataFrame for display.
    
    How are the utility functions implemented ?
    
    1. I added a new module **spark-utils** which provide Scala code for display utility functions. This module will use the **maven-scala-plugin** to compile all the classes in package `org.apache.zeppelin.spark.utils`.
    
    2. Right now the package `org.apache.zeppelin.spark.utils` only contains 1 object `DisplayUtils` which augments RDDs/**Scala Traversable** of Tuples or case classes (all of them sub-class of trait `Product`) with the new method `displayAsTable(columnLabels: String*)`.
    
    3. The `DisplayUtils` object is imported automatically into the `SparkInterpreter` with `intp.interpret("import org.apache.zeppelin.spark.utils.DisplayUtils._");`
    
    4. The Maven module **interpreter** will now have a **runtime** dependency on the module **spark-utils** so that the utility class will be loaded at runtime
    
    5. Usage of the new display utility function is:
    
        **Paragraph1**
        ```scala
        case class Person(login: String, name: String, age: Int)
        val rddTuples:RDD[(String,String,Int)] = sc.parallelize(List(("jdoe","John DOE",32),("hsue","Helen     SUE",27))
        val rddCaseClass:RDD[Person] = sc.parallelize(List(Person("jdoe","John DOE",32),Person("hsue","Helen SUE",27))
        ```
        **Paragraph2**
        ```scala
        rddTuples.displayAsTable("Login","Name","Age")
        ```
    
        **Paragraph3**
        ```scala
        rddCaseClass.displayAsTable("Login","Name","Age")
        ```
    
    6. The `displayAsTable()` method is error-proof, meaning that if the user provides **more** columns label that the number of elements in the tuples/case class, the extra column labels will ignored. If the user provides **less** column labels than expected, the method will pad missing column headers with **Column2**, **Column3** etc ...
    
    7. In addition to the `displayAsTable` methods, I added some other utility methods to make it easier to handle custom HTML and images:
        a. calling `html()` will generate the string `"%html "`
        b. calling `html("<p> This is a test</p>)` will generate the string `"%html <p> This is a test</p>"`
        c. calling `img("http://www.google.com")` will generate the string `"<img src='http://www.google.com' />"`
       d. calling `img64()` will generate the string `"%img "`
       e. calling `img64("ABCDE123")` will generate the string `"%img ABCDE123"`
    
    Of course the `DisplayUtils` object can be extended with new other functions to support future advanced displaying features
    
    Author: DuyHai DOAN <doanduyhai@gmail.com>
    
    Closes #80 from doanduyhai/DisplayUtils and squashes the following commits:
    
    62a2311 [DuyHai DOAN] Add default limit to RDD using zeppelin.spark.maxResult property
    47a1b1f [DuyHai DOAN] Rename displayAsTable() to display()
    a15294e [DuyHai DOAN] Enhance display function utility to accept Scala Traversable in addition to RDD
    c1ee8fe [DuyHai DOAN] Add Scala code in module spark to expose utility functions for display

--- spark/src/main/java/org/apache/zeppelin/spark/SparkInterpreter.java
+++ spark/src/main/java/org/apache/zeppelin/spark/SparkInterpreter.java
@@ -461,6 +461,14 @@ public class SparkInterpreter extends Interpreter {
       intp.interpret("import org.apache.spark.sql.functions._");
     }
 
+    // Utility functions for display
+    intp.interpret("import org.apache.zeppelin.spark.utils.DisplayUtils._");
+
+    // Scala implicit value for spark.maxResult
+    intp.interpret("import org.apache.zeppelin.spark.utils.SparkMaxResult");
+    intp.interpret("implicit val sparkMaxResult = new SparkMaxResult(" +
+            Integer.parseInt(getProperty("zeppelin.spark.maxResult")) + ")");
+
     // add jar
     if (depInterpreter != null) {
       DependencyContext depc = depInterpreter.getDependencyContext();
