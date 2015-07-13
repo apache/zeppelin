61a361bb201adc6c2938147cd963fb39357415c4
madhuka <madhukaudantha@gmail.com>
2015-07-13 19:42:18 +0530

    adding skipTest for grunt

--- zeppelin-web/Gruntfile.js
+++ zeppelin-web/Gruntfile.js
@@ -414,6 +414,22 @@ module.exports = function (grunt) {
   ]);
 
   grunt.registerTask('build', [
+    'test',
+    'clean:dist',
+    'wiredep',
+    'useminPrepare',
+    'concurrent:dist',
+    'autoprefixer',
+    'concat',
+    'ngAnnotate',
+    'copy:dist',
+    'cssmin',
+    'uglify',
+    'usemin',
+    'htmlmin'
+  ]);
+
+  grunt.registerTask('buildSkipTests', [
     'clean:dist',
     'wiredep',
     'useminPrepare',
