8f82a38e89adf0c079f28158c835ddae0877fdb4
madhuka <madhukaudantha@gmail.com>
2015-08-01 23:08:06 +0530

    improving  map data validation

--- zeppelin-web/src/components/data-validator/map-data-validator-factory.js
+++ zeppelin-web/src/components/data-validator/map-data-validator-factory.js
@@ -13,71 +13,51 @@
  */
 'use strict';
 
-angular.module('zeppelinWebApp').factory('mapdataValidator', function($rootScope, DataValidator, dataModelSchemas) {
+angular.module('zeppelinWebApp').factory('mapdataValidator', function(
+  $rootScope, DataValidator, dataModelSchemas) {
 
   var msg = '';
   var errorStatus = false;
-  var mapdataValidator = new DataValidator(dataModelSchemas.MapSchema);
+  var mapSchema = dataModelSchemas.MapSchema;
+
+  var mapdataValidator = new DataValidator(mapSchema);
   mapdataValidator.checkLatiLong = function() {
-    //TODO add mapDataCheckValidator and latitudeValidator
-  };
-
-
-  function mapDataCheckValidator(record, schema) {
-    if (record) {
-      for (var i = 0; i < schema.type.length; i++) {
-        if (isNaN(record[i]) !== (schema.type[i] === 'string')) {
-          errorStatus = true;
-          msg += 'data record ' + (record[i]) + ' is not matching for schema | ';
-          return true;
-        }else{
-          if(i===2){
-            errorStatus = !latitudeValidator(record[i],schema.range);
-          }if(i===3){
-            errorStatus = !longitudeValidator(record[i],schema.range);
-          }
-          if(errorStatus){
-            return true;
-          }
-        }
-      }//end validation on data record
-      errorStatus = false;
-      return false;
-    } else {
-      msg += 'data record does not exisiting | ';
-      return true;
+    var data = mapdataValidator.data.rows;
+    msg = '';
+    for(var i = 0; i < data.length; i++) {
+      latitudeValidator(data[i][2], mapSchema.range);
+      longitudeValidator(data[i][3], mapSchema.range);
+      if(errorStatus) {
+        mapdataValidator.setError();
+      }
     }
-  }
+    mapdataValidator.setMsg(msg);
+    return;
+  };
 
   //Latitude measurements range from 0° to (+/–)90°.
   function latitudeValidator(record, schema) {
-    if(record) {
-      var latitude = parseFloat(record);
-      if(schema.latitude.low <= latitude <= schema.latitude.high) {
-        return true;
-      }
-      msg += 'Latitude ' + record + ' is not in range | ';
-      return false;
-    } else {
-      msg += 'Latitude record does not exisiting | ';
-      return true;
+    var latitude = parseFloat(record);
+    if(schema.latitude.low < latitude && latitude < schema.latitude.high) {
+      msg += 'latitudes are ok | ';
+      return;
     }
+    msg += 'Latitude ' + record + ' is not in range | ';
+    errorStatus = true;
+    return;
   }
 
   //Longitude measurements range from 0° to (+/–)180°.
   function longitudeValidator(record, schema) {
-    console.log(record);
-    if(record) {
-      var longitude = parseFloat(record);
-      if(schema.longitude.low <= longitude <= schema.longitude.high) {
-        return true;
-      }
-      msg += 'Longitude' + record + ' is not in range | ';
-      return false;
-    } else {
-      msg += 'Longitude record does not exisiting | ';
-      return true;
+    var longitude = parseFloat(record);
+    console.log(schema.longitude.low < longitude);
+    if(schema.longitude.low < longitude && longitude < schema.longitude.high) {
+      msg += 'longitude are ok | ';
+      return;
     }
+    msg += 'Longitude' + record + ' is not in range | ';
+    errorStatus = true;
+    return;
   }
 
   return mapdataValidator;
