e5b00673ab460ccb05a1ef0998924887582f4f69
caofangkun <caofangkun@gmail.com>
2015-08-07 16:29:41 +0800

    ZEPPELIN-207: travis-ci build log is too long to be displayed

---
+++ save-log-with-progress.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+function ProgressBar {
+	let _progress=(${1}*100/${2}*100)/100
+	let _done=(${_progress}*4)/10
+	let _left=40-$_done
+	_done=$(printf "%${_done}s")
+	_left=$(printf "%${_left}s")
+
+printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
+
+}
+
+# Variables
+_start=1
+
+# This accounts as the "totalState" variable for the ProgressBar function
+_end=100
+
+# Proof of concept
+for number in $(seq ${_start} ${_end})
+do
+	sleep 0.1
+	ProgressBar ${number} ${_end}
+done
+printf '\nFinished!\n'
