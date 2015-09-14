ca67374c2b0fe26f6bc6992910bb11d5658258e0
Lee moon soo <moon@apache.org>
2015-09-14 14:04:40 +0900

    ZEPPELIN-297 Dependency should be loaded in pypsark
    This PR fixes https://issues.apache.org/jira/browse/ZEPPELIN-297
    
    * [x] fix, by set context classloader with classes from dependency loader when initializing py4j gatewayserver
    * [x] test
    
    Author: Lee moon soo <moon@apache.org>
    
    Closes #298 from Leemoonsoo/ZEPPELIN-297 and squashes the following commits:
    
    0de89fe [Lee moon soo] Add logging
    1e8f52a [Lee moon soo] Add test
    163acfa [Lee moon soo] Set classloader for gatewayserver with classes from dependency loader

--- spark/src/main/java/org/apache/zeppelin/spark/PySparkInterpreter.java
+++ spark/src/main/java/org/apache/zeppelin/spark/PySparkInterpreter.java
@@ -25,7 +25,10 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.net.MalformedURLException;
 import java.net.ServerSocket;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -51,6 +54,7 @@ import org.apache.zeppelin.interpreter.InterpreterResult;
 import org.apache.zeppelin.interpreter.InterpreterResult.Code;
 import org.apache.zeppelin.interpreter.LazyOpenInterpreter;
 import org.apache.zeppelin.interpreter.WrappedInterpreter;
+import org.apache.zeppelin.spark.dep.DependencyContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -125,6 +129,44 @@ public class PySparkInterpreter extends Interpreter implements ExecuteResultHand
 
   @Override
   public void open() {
+    DepInterpreter depInterpreter = getDepInterpreter();
+
+    // load libraries from Dependency Interpreter
+    URL [] urls = new URL[0];
+
+    if (depInterpreter != null) {
+      DependencyContext depc = depInterpreter.getDependencyContext();
+      if (depc != null) {
+        List<File> files = depc.getFiles();
+        List<URL> urlList = new LinkedList<URL>();
+        if (files != null) {
+          for (File f : files) {
+            try {
+              urlList.add(f.toURI().toURL());
+            } catch (MalformedURLException e) {
+              logger.error("Error", e);
+            }
+          }
+
+          urls = urlList.toArray(urls);
+        }
+      }
+    }
+
+    ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
+    try {
+      URLClassLoader newCl = new URLClassLoader(urls, oldCl);
+      Thread.currentThread().setContextClassLoader(newCl);
+      createGatewayServerAndStartScript();
+    } catch (Exception e) {
+      logger.error("Error", e);
+      throw new InterpreterException(e);
+    } finally {
+      Thread.currentThread().setContextClassLoader(oldCl);
+    }
+  }
+
+  private void createGatewayServerAndStartScript() {
     // create python script
     createPythonScript();
 
@@ -398,6 +440,23 @@ public class PySparkInterpreter extends Interpreter implements ExecuteResultHand
     } else {
       return intp.getSQLContext();
     }
+  }
+
+  private DepInterpreter getDepInterpreter() {
+    InterpreterGroup intpGroup = getInterpreterGroup();
+    if (intpGroup == null) return null;
+    synchronized (intpGroup) {
+      for (Interpreter intp : intpGroup) {
+        if (intp.getClassName().equals(DepInterpreter.class.getName())) {
+          Interpreter p = intp;
+          while (p instanceof WrappedInterpreter) {
+            p = ((WrappedInterpreter) p).getInnerInterpreter();
+          }
+          return (DepInterpreter) p;
+        }
+      }
+    }
+    return null;
   }
 
 
