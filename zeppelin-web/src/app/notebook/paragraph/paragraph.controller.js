edeadbb3da76a78615c65abfe5f1be419bb15b98
Ruchi Ookalkar <rookalkar@gmail.com>
2015-09-22 16:43:33 +0530

    Update paragraph.controller.js

--- zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
+++ zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
@@ -720,6 +720,10 @@ angular.module('zeppelinWebApp')
       result.rows = rows;
     }
   };
+  
+  $scope.setColumnName = function(col_name) {
+    box_column = col_name;
+  };
 
   $scope.setGraphMode = function(type, emit, refresh) {
     if (emit) {
@@ -907,7 +911,7 @@ angular.module('zeppelinWebApp')
           }
         }
       } else if (type === 'boxPlotChart'){
-        d3g = pivotDatatoBoxFormat(data);
+        d3g = pivotDataToBoxFormat(data);
         $scope.chart[type].x(function(d) { return d.label;})
                           .y(function(d) { return d.value.Q3;})
                           .staggerLabels(false)
@@ -936,13 +940,19 @@ angular.module('zeppelinWebApp')
     
     var pivotDataToBoxFormat = function(data) {
       // parse in the required data here into 'val' array
-      var i; var val = [];
+      var val = [];
+      var col_index=0;
+      var i;
+      while ( box_column != data.columnNames[col_index].name)
+      {
+          col_index++;
+      }
       for(i=0;i<data.rows.length;i++){
-        val.push(parseInt(data.rows[i][0]))
+         val.push(parseInt(data.rows[i][col_index]))
       } ;
-     return  [ 
+      return  [ 
         {
-          label: "Sample A",
+          label: "data.columnNames[col_index].name",
           values: computeBoxValues(val),
         }
       ];
@@ -950,46 +960,46 @@ angular.module('zeppelinWebApp')
     
     var computeBoxValues = function (values){
       var Q1,Q2,Q3;
-        var iqr,low,high,whisker_high,whisker_low;
-        var outliers = [];
-        var i=0; var j=0;
+      var iqr,low,high,whisker_high,whisker_low;
+      var outliers = [];
+      var i=0; var j=0;
 
-        var q1Arr = (values.length % 2 == 0) ? values.slice(0, (values.length / 2)) : values.slice(0, Math.floor(values.length / 2));
-        var q2Arr =  values;
-        var q3Arr = (values.length % 2 == 0) ? values.slice((values.length / 2), values.length) : values.slice(Math.ceil(values.length / 2), values.length);
-        Q1 = medianX(q1Arr);
-        Q2 =medianX(q2Arr);
-        Q3 =medianX(q3Arr);
+      var q1Arr = (values.length % 2 == 0) ? values.slice(0, (values.length / 2)) : values.slice(0, Math.floor(values.length / 2));
+      var q2Arr =  values;
+      var q3Arr = (values.length % 2 == 0) ? values.slice((values.length / 2), values.length) : values.slice(Math.ceil(values.length / 2), values.length);
+      Q1 = medianX(q1Arr);
+      Q2 =medianX(q2Arr);
+      Q3 =medianX(q3Arr);
                 
-        iqr = Q3-Q1;
-        low = Q1 - 1.5 * iqr;
-        high = Q3 + 1.5 * iqr;
+      iqr = Q3-Q1;
+      low = Q1 - 1.5 * iqr;
+      high = Q3 + 1.5 * iqr;
 
-       while(q1Arr[i]<low)
-            {
-                outliers.push(q1Arr[i]);
-                i++;
-            }
-        whisker_low = q1Arr[i];
+      while(q1Arr[i]<low)
+        {
+          outliers.push(q1Arr[i]);
+          i++;
+        }
+      whisker_low = q1Arr[i];
 
-        while(q3Arr[j]<high) { j++;}
+      while(q3Arr[j]<high) { j++;}
 
-        whisker_high = q3Arr[j-1];
+      whisker_high = q3Arr[j-1];
 
-        outliers = outliers.concat(q3Arr.splice(j,q3Arr.length));
-        return {
-          Q1 : Q1,
-          Q2 : Q2,
-          Q3 : Q3,
-          whisker_low: whisker_low,
-          whisker_high: whisker_high,
-          outliers: outliers
-        };
+      outliers = outliers.concat(q3Arr.splice(j,q3Arr.length));
+      return {
+        Q1 : Q1,
+        Q2 : Q2,
+        Q3 : Q3,
+        whisker_low: whisker_low,
+        whisker_high: whisker_high,
+        outliers: outliers
+      };
     };
     
     var medianX = function (medianArr) {
-      var count = medianArr.length;
-      var median = (count % 2 == 0) ? (medianArr[(medianArr.length/2) - 1] + medianArr[(medianArr.length / 2)]) / 2:medianArr[Math.floor(medianArr.length / 2)];
+      var length = medianArr.length;
+      var median = (length % 2 == 0) ? (medianArr[(length/2) - 1] + medianArr[(length / 2)]) / 2:medianArr[Math.floor(length / 2)];
       return median;
     }
 
