43d4af2928bf14b68ceb0709cb6870f7d4f60924
Ruchi Ookalkar <rookalkar@gmail.com>
2015-10-06 12:20:43 +0530

    Update paragraph.controller.js

--- zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
+++ zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
@@ -720,10 +720,10 @@ angular.module('zeppelinWebApp')
       result.rows = rows;
     }
   };
-  
+  /*
   $scope.setColumnName = function(col_name) {
     box_column = col_name;
-  };
+  };*/
 
   $scope.setGraphMode = function(type, emit, refresh) {
     if (emit) {
@@ -843,7 +843,69 @@ angular.module('zeppelinWebApp')
 
   $scope.setDonutFlag = function(flag){
     donutFlag = flag;
-  }
+  };
+    
+  var computeBoxValues = function (values){
+    /*jshint camelcase: false */
+    var Q1,Q2,Q3;
+    var iqr,low,high,whiskerHigh,whiskerLow;
+    var i = 0; var j = 0;
+
+    var firstHalfOfArr = (values.length % 2 === 0) ? values.slice(0, (values.length / 2)) : values.slice(0, Math.floor(values.length / 2));
+    var fullArr =  values;
+    var secondHalfOfArr = (values.length % 2 === 0) ? values.slice((values.length / 2), values.length) : values.slice(Math.ceil(values.length / 2), values.length);
+    Q1 = medianX(firstHalfOfArr);
+    Q2 = medianX(fullArr);
+    Q3 = medianX(secondHalfOfArr);
+              
+    iqr = Q3-Q1;
+    low = Q1 - 1.5 * iqr;
+    high = Q3 + 1.5 * iqr;
+
+    var outliers = firstHalfOfArr.filter(function(n){
+      return (n<low);
+    });     
+    whiskerLow = firstHalfOfArr[i];
+    while(secondHalfOfArr[j]<high) { j++;}
+    whiskerHigh = secondHalfOfArr[j-1];
+    outliers = outliers.concat(secondHalfOfArr.splice(j,secondHalfOfArr.length));
+    return {
+      Q1 : Q1,
+      Q2 : Q2,
+      Q3 : Q3,
+      whisker_low : whiskerLow,
+      whisker_high : whiskerHigh,
+      outliers : outliers
+    };
+  };
+  
+  var medianX = function (medianArr) {
+    var length = medianArr.length;
+    var median = (length % 2 === 0) ? (medianArr[(length/2) - 1] + medianArr[(length / 2)]) / 2:medianArr[Math.floor(length / 2)];
+    return median;
+  };
+  
+  var pivotDataToBoxFormat = function(data) {
+      // parse in the required data here into 'val' array
+      var columnIndex=0;
+      /* commented the code for column selection as it is WIP. Will add once ready.
+      while ( box_column != data.columnNames[col_index].name)
+      {
+          col_index++;
+      }*/
+      
+      var val = data.rows.map(function(row) {
+        return parseInt(row[columnIndex]); 
+      });
+      
+      return  [ 
+        {
+          label: data.columnNames[columnIndex].name,
+          values: computeBoxValues(val),
+        }
+      ];
+    };
+    
   var setD3Chart = function(type, data, refresh) {
     /*jshint camelcase: false */
     if (!$scope.chart[type]) {
@@ -937,67 +999,6 @@ angular.module('zeppelinWebApp')
         $scope.chart[type].useInteractiveGuideline(true); // for better UX and performance issue. (https://github.com/novus/nvd3/issues/691)
         $scope.chart[type].forceY([0]); // force y-axis minimum to 0 for line chart.
       }
-    }
-    
-    var pivotDataToBoxFormat = function(data) {
-      // parse in the required data here into 'val' array
-      var columnIndex=0;
-      /* commented the code for column selection as it is WIP. Will add once ready.
-      while ( box_column != data.columnNames[col_index].name)
-      {
-          col_index++;
-      }*/
-      
-      var val = data.rows.map(function(row) {
-        return parseInt(row[columnIndex]); 
-      });
-      
-      return  [ 
-        {
-          label: data.columnNames[columnIndex].name,
-          values: computeBoxValues(val),
-        }
-      ];
-    };
-    
-    var computeBoxValues = function (values){
-      /*jshint camelcase: false */
-      var Q1,Q2,Q3;
-      var iqr,low,high,whiskerHigh,whiskerLow;
-      var i = 0; var j = 0;
-
-      var firstHalfOfArr = (values.length % 2 == 0) ? values.slice(0, (values.length / 2)) : values.slice(0, Math.floor(values.length / 2));
-      var fullArr =  values;
-      var secondHalfOfArr = (values.length % 2 == 0) ? values.slice((values.length / 2), values.length) : values.slice(Math.ceil(values.length / 2), values.length);
-      Q1 = medianX(firstHalfOfArr);
-      Q2 = medianX(fullArr);
-      Q3 = medianX(secondHalfOfArr);
-                
-      iqr = Q3-Q1;
-      low = Q1 - 1.5 * iqr;
-      high = Q3 + 1.5 * iqr;
-
-      var outliers = firstHalfOfArr.filter(function(n){
-        return (n<low);
-      });     
-      whiskerLow = firstHalfOfArr[i];
-      while(secondHalfOfArr[j]<high) { j++;}
-      whiskerHigh = secondHalfOfArr[j-1];
-      outliers = outliers.concat(secondHalfOfArrr.splice(j,secondHalfOfArr.length));
-      return {
-        Q1 : Q1,
-        Q2 : Q2,
-        Q3 : Q3,
-        whisker_low : whiskerLow,
-        whisker_high : whiskerHigh,
-        outliers : outliers
-      };
-    };
-    
-    var medianX = function (medianArr) {
-      var length = medianArr.length;
-      var median = (length % 2 == 0) ? (medianArr[(length/2) - 1] + medianArr[(length / 2)]) / 2:medianArr[Math.floor(length / 2)];
-      return median;
     }
 
     var renderChart = function() {
