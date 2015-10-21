dd4b361d8bf5f375d66860435d7a915d0e16d3dd
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2015-10-21 15:14:18 +0530

    ZEPPELIN-265 auto save on page navigation

--- zeppelin-web/src/app/notebook/notebook.controller.js
+++ zeppelin-web/src/app/notebook/notebook.controller.js
@@ -98,10 +98,12 @@ angular.module('zeppelinWebApp').controller('NotebookCtrl', function($scope, $ro
   };
 
   $scope.saveNote = function() {
-    _.forEach($scope.note.paragraphs, function(n, key) {
-      angular.element('#' + n.id + '_paragraphColumn_main').scope().saveParagraph();
-    });
-    $scope.isNoteDirty = null;
+    if ($scope.note && $scope.note.paragraphs) {
+      _.forEach($scope.note.paragraphs, function(n, key) {
+        angular.element('#' + n.id + '_paragraphColumn_main').scope().saveParagraph();
+      });
+      $scope.isNoteDirty = null;
+    }
   };
 
   $scope.toggleAllEditor = function() {
@@ -151,7 +153,7 @@ angular.module('zeppelinWebApp').controller('NotebookCtrl', function($scope, $ro
   };
 
   $scope.killSaveTimer = function() {
-    if($scope.saveTimer){
+    if ($scope.saveTimer) {
       $timeout.cancel($scope.saveTimer);
       $scope.saveTimer = null;
     }
@@ -165,6 +167,16 @@ angular.module('zeppelinWebApp').controller('NotebookCtrl', function($scope, $ro
       $scope.saveNote();
     }, 10000);
   };
+
+  jQuery(window).on('unload', function(e) {
+    $scope.saveNote();
+  });
+
+  $scope.$on('$destroy', function() {
+    jQuery(window).off('unload');
+    $scope.killSaveTimer();
+    $scope.saveNote();
+  });
 
   $scope.setLookAndFeel = function(looknfeel) {
     $scope.note.config.looknfeel = looknfeel;
