361291a8dde26cb6c799823828cdc2b7915ca8b3
Patrick Ethier <nuttybrewer@gmail.com>
2015-11-02 15:25:30 +0100

    Update Repository.java
    Fix AND condition typo

--- spark/src/main/java/org/apache/zeppelin/spark/dep/Repository.java
+++ spark/src/main/java/org/apache/zeppelin/spark/dep/Repository.java
@@ -65,7 +65,7 @@ public class Repository {
   }
   
   protected Authentication getCredentials() {
-    if(this.username != null & this.password != null)
+    if(this.username != null && this.password != null)
       return new Authentication(this.username, this.password)
     return null;
   }
