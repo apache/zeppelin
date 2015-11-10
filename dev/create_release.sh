5ec2acae58fb48a3f2bcf39a9c220a7480c5fb14
Lee moon soo <moon@apache.org>
2015-11-10 19:35:19 +0900

    Add binary license

--- dev/create_release.sh
+++ dev/create_release.sh
@@ -103,9 +103,12 @@ function make_binary_release() {
         exit 1
     fi
 
-    # re-create package with proper dir name
+    # re-create package with proper dir name with binary license
     cd zeppelin-distribution/target/zeppelin-*
     mv zeppelin-* zeppelin-${RELEASE_NAME}-bin-${BIN_RELEASE_NAME}
+    cat ../../src/bin_license/LICENSE >> zeppelin-${RELEASE_NAME}-bin-${BIN_RELEASE_NAME}/LICENSE
+    cat ../../src/bin_license/NOTICE >> zeppelin-${RELEASE_NAME}-bin-${BIN_RELEASE_NAME}/NOTICE
+    cp ../../src/license/licenses/* zeppelin-${RELEASE_NAME}-bin-${BIN_RELEASE_NAME}/licenses/
     ${TAR} cvzf zeppelin-${RELEASE_NAME}-bin-${BIN_RELEASE_NAME}.tgz zeppelin-${RELEASE_NAME}-bin-${BIN_RELEASE_NAME}
 
     # sign bin package
