ee1d70494b8bad7033b7534712e82f1f1486c874
Lee moon soo <moon@apache.org>
2015-11-15 15:55:15 +0900

    ZEPPELIN-422 Add missing Apache Header
    Some files are missing an Apache Header. https://issues.apache.org/jira/browse/ZEPPELIN-422
    
    Author: Lee moon soo <moon@apache.org>
    
    Closes #433 from Leemoonsoo/ZEPPELIN-422 and squashes the following commits:
    
    8abde29 [Lee moon soo] Add Apache Header

--- zeppelin-zengine/src/test/java/org/apache/zeppelin/notebook/repo/mock/VFSNotebookRepoMock.java
+++ zeppelin-zengine/src/test/java/org/apache/zeppelin/notebook/repo/mock/VFSNotebookRepoMock.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.zeppelin.notebook.repo.mock;
 
 import java.io.File;
@@ -11,7 +27,7 @@ import org.apache.zeppelin.conf.ZeppelinConfiguration.ConfVars;
 import org.apache.zeppelin.notebook.repo.VFSNotebookRepo;
 
 public class VFSNotebookRepoMock extends VFSNotebookRepo {
-  
+
   private static ZeppelinConfiguration modifyNotebookDir(ZeppelinConfiguration conf) {
     String secNotebookDir = conf.getNotebookDir() + "_secondary";
     System.setProperty(ConfVars.ZEPPELIN_NOTEBOOK_DIR.getVarName(), secNotebookDir);
@@ -22,5 +38,5 @@ public class VFSNotebookRepoMock extends VFSNotebookRepo {
   public VFSNotebookRepoMock(ZeppelinConfiguration conf) throws IOException {
     super(modifyNotebookDir(conf));
   }
-  
+
 }
