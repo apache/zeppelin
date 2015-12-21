3bfd97e236b66675c9da96f7ab00c45f053bd887
Jungtaek Lim <kabhwan@gmail.com>
2015-12-21 16:16:05 +0900

    ZEPPELIN-511 REST API: Insert / Retrieve / Move / Delete operation for paragraph
    ### What is this PR for?
    
    This issue is intended to fill gap between REST API and WebSocket operations.
    For now we can only access notebook and paragraph to READONLY (not writing) via REST API but after this PR, we can insert / retrieve / move / delete paragraph via REST API.
    
    ### What type of PR is it?
    
    Feature
    
    ### Todos
    
    ### Is there a relevant Jira issue?
    
    https://issues.apache.org/jira/browse/ZEPPELIN-511
    
    ### How should this be tested?
    
    Please follow the explanation of added REST APIs from rest-notebook.md.
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? (No)
    * Is there breaking changes for older versions? (No)
    * Does this needs documentation? (Yes, I've addressed it.)
    
    Author: Jungtaek Lim <kabhwan@gmail.com>
    
    Closes #550 from HeartSaVioR/ZEPPELIN-511 and squashes the following commits:
    
    59f5f61 [Jungtaek Lim] ZEPPELIN-511 Address @prabhjyotsingh reviews
    01feed4 [Jungtaek Lim] ZEPPELIN-511 explain new APIs to read-notebook.md
    cc71e06 [Jungtaek Lim] ZEPPELIN-511 "insert paragraph" api: return created paragraph id
    3658b00 [Jungtaek Lim] ZEPPELIN-511 REST API: Insert / Retrieve / Move / Delete operation for paragraph

--- zeppelin-server/src/test/java/org/apache/zeppelin/rest/ZeppelinRestApiTest.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/rest/ZeppelinRestApiTest.java
@@ -602,5 +602,127 @@ public class ZeppelinRestApiTest extends AbstractTestRestApi {
 
     ZeppelinServer.notebook.removeNote(note.getId());
   }
+
+  @Test
+  public void testInsertParagraph() throws IOException {
+    Note note = ZeppelinServer.notebook.createNote();
+
+    String jsonRequest = "{\"title\": \"title1\", \"text\": \"text1\"}";
+    PostMethod post = httpPost("/notebook/" + note.getId() + "/paragraph", jsonRequest);
+    LOG.info("testInsertParagraph response\n" + post.getResponseBodyAsString());
+    assertThat("Test insert method:", post, isCreated());
+    post.releaseConnection();
+
+    Map<String, Object> resp = gson.fromJson(post.getResponseBodyAsString(), new TypeToken<Map<String, Object>>() {
+    }.getType());
+
+    String newParagraphId = (String) resp.get("body");
+    LOG.info("newParagraphId:=" + newParagraphId);
+
+    Note retrNote = ZeppelinServer.notebook.getNote(note.getId());
+    Paragraph newParagraph = retrNote.getParagraph(newParagraphId);
+    assertNotNull("Can not find new paragraph by id", newParagraph);
+
+    assertEquals("title1", newParagraph.getTitle());
+    assertEquals("text1", newParagraph.getText());
+
+    Paragraph lastParagraph = note.getLastParagraph();
+    assertEquals(newParagraph.getId(), lastParagraph.getId());
+
+    // insert to index 0
+    String jsonRequest2 = "{\"index\": 0, \"title\": \"title2\", \"text\": \"text2\"}";
+    PostMethod post2 = httpPost("/notebook/" + note.getId() + "/paragraph", jsonRequest2);
+    LOG.info("testInsertParagraph response2\n" + post2.getResponseBodyAsString());
+    assertThat("Test insert method:", post2, isCreated());
+    post2.releaseConnection();
+
+    Paragraph paragraphAtIdx0 = note.getParagraphs().get(0);
+    assertEquals("title2", paragraphAtIdx0.getTitle());
+    assertEquals("text2", paragraphAtIdx0.getText());
+
+    ZeppelinServer.notebook.removeNote(note.getId());
+  }
+
+  @Test
+  public void testGetParagraph() throws IOException {
+    Note note = ZeppelinServer.notebook.createNote();
+
+    Paragraph p = note.addParagraph();
+    p.setTitle("hello");
+    p.setText("world");
+    note.persist();
+
+    GetMethod get = httpGet("/notebook/" + note.getId() + "/paragraph/" + p.getId());
+    LOG.info("testGetParagraph response\n" + get.getResponseBodyAsString());
+    assertThat("Test get method: ", get, isAllowed());
+    get.releaseConnection();
+
+    Map<String, Object> resp = gson.fromJson(get.getResponseBodyAsString(), new TypeToken<Map<String, Object>>() {
+    }.getType());
+
+    assertNotNull(resp);
+    assertEquals("OK", resp.get("status"));
+
+    Map<String, Object> body = (Map<String, Object>) resp.get("body");
+
+    assertEquals(p.getId(), body.get("id"));
+    assertEquals("hello", body.get("title"));
+    assertEquals("world", body.get("text"));
+
+    ZeppelinServer.notebook.removeNote(note.getId());
+  }
+
+  @Test
+  public void testMoveParagraph() throws IOException {
+    Note note = ZeppelinServer.notebook.createNote();
+
+    Paragraph p = note.addParagraph();
+    p.setTitle("title1");
+    p.setText("text1");
+
+    Paragraph p2 = note.addParagraph();
+    p2.setTitle("title2");
+    p2.setText("text2");
+
+    note.persist();
+
+    PostMethod post = httpPost("/notebook/" + note.getId() + "/paragraph/" + p2.getId() + "/move/" + 0, "");
+    assertThat("Test post method: ", post, isAllowed());
+    post.releaseConnection();
+
+    Note retrNote = ZeppelinServer.notebook.getNote(note.getId());
+    Paragraph paragraphAtIdx0 = retrNote.getParagraphs().get(0);
+
+    assertEquals(p2.getId(), paragraphAtIdx0.getId());
+    assertEquals(p2.getTitle(), paragraphAtIdx0.getTitle());
+    assertEquals(p2.getText(), paragraphAtIdx0.getText());
+
+    PostMethod post2 = httpPost("/notebook/" + note.getId() + "/paragraph/" + p2.getId() + "/move/" + 10, "");
+    assertThat("Test post method: ", post2, isBadRequest());
+    post.releaseConnection();
+
+    ZeppelinServer.notebook.removeNote(note.getId());
+  }
+
+  @Test
+  public void testDeleteParagraph() throws IOException {
+    Note note = ZeppelinServer.notebook.createNote();
+
+    Paragraph p = note.addParagraph();
+    p.setTitle("title1");
+    p.setText("text1");
+
+    note.persist();
+
+    DeleteMethod delete = httpDelete("/notebook/" + note.getId() + "/paragraph/" + p.getId());
+    assertThat("Test delete method: ", delete, isAllowed());
+    delete.releaseConnection();
+
+    Note retrNote = ZeppelinServer.notebook.getNote(note.getId());
+    Paragraph retrParagrah = retrNote.getParagraph(p.getId());
+    assertNull("paragraph should be deleted", retrParagrah);
+
+    ZeppelinServer.notebook.removeNote(note.getId());
+  }
 }
 
