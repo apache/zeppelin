1f419b601a04677262049add6c9152263de3c417
Lee moon soo <moon@apache.org>
2015-12-22 12:10:53 +0900

    Add InterpreterOutput

---
+++ zeppelin-interpreter/src/main/java/org/apache/zeppelin/interpreter/InterpreterOutput.java
@@ -0,0 +1,198 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.zeppelin.interpreter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * InterpreterOutput
+ */
+public class InterpreterOutput extends OutputStream {
+  private final List<Object> outList = new LinkedList<Object>();
+  private InterpreterOutputChangeWatcher watcher;
+  Object header;
+
+  public InterpreterOutput() {
+    clear();
+  }
+
+  public InterpreterOutput(InterpreterOutputChangeListener listener) throws IOException {
+    clear();
+    watcher = new InterpreterOutputChangeWatcher(listener);
+    watcher.start();
+  }
+
+  public void clear() {
+    header = null;
+    synchronized (outList) {
+      outList.clear();
+      if (watcher != null) {
+        watcher.clear();
+      }
+    }
+  }
+
+  public void setHeader(String o) {
+    this.header = o.getBytes();
+  }
+
+  @Override
+  public void write(int b) throws IOException {
+    synchronized (outList) {
+      outList.add(b);
+    }
+  }
+
+  @Override
+  public void write(byte [] b) throws IOException {
+    synchronized (outList) {
+      outList.add(b);
+    }
+  }
+
+  @Override
+  public void write(byte [] b, int off, int len) throws IOException {
+    synchronized (outList) {
+      byte[] buf = new byte[len];
+      System.arraycopy(b, off, buf, 0, len);
+      outList.add(buf);
+    }
+  }
+
+  /**
+   * In dev mode, it monitors file and update ZeppelinServer
+   * @param file
+   * @throws IOException
+   */
+  public void write(File file) throws IOException {
+    outList.add(file);
+    if (watcher != null) {
+      watcher.watch(file);
+    }
+  }
+
+  public void write(String string) throws IOException {
+    write(string.getBytes());
+  }
+
+  /**
+   * write contents in the resource file in the classpath
+   * @param url
+   * @throws IOException
+   */
+  public void write(URL url) throws IOException {
+    if ("file".equals(url.getProtocol())) {
+      write(new File(url.getPath()));
+    } else {
+      outList.add(url);
+    }
+  }
+
+  public void writeResource(String resourceName) throws IOException {
+    // search file under resource dir first for dev mode
+    File mainResource = new File("./src/main/resources/" + resourceName);
+    File testResource = new File("./src/test/resources/" + resourceName);
+    if (mainResource.isFile()) {
+      write(mainResource);
+    } else if (testResource.isFile()) {
+      write(testResource);
+    } else {
+      // search from classpath
+      ClassLoader cl = Thread.currentThread().getContextClassLoader();
+      if (cl == null) {
+        cl = this.getClass().getClassLoader();
+      }
+      if (cl == null) {
+        cl = ClassLoader.getSystemClassLoader();
+      }
+
+      write(cl.getResource(resourceName));
+    }
+  }
+
+  public byte[] toByteArray() throws IOException {
+    return toByteArray(false);
+  }
+
+  public byte[] toByteArray(boolean clear) throws IOException {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    List<Object> all = new LinkedList<Object>();
+
+    synchronized (outList) {
+      if (header != null) {
+        all.add(header);
+      }
+      all.addAll(outList);
+    }
+
+    for (Object o : all) {
+      if (o instanceof File) {
+        File f = (File) o;
+        FileInputStream fin = new FileInputStream(f);
+        copyStream(fin, out);
+        fin.close();
+      } else if (o instanceof byte[]) {
+        out.write((byte[]) o);
+      } else if (o instanceof Integer) {
+        out.write((int) o);
+      } else if (o instanceof URL) {
+        InputStream fin = ((URL) o).openStream();
+        copyStream(fin, out);
+        fin.close();
+      } else {
+        // can not handle the object
+      }
+    }
+
+    if (clear) {
+      clear();
+    }
+
+    out.close();
+    return out.toByteArray();
+  }
+
+  private void copyStream(InputStream in, OutputStream out) throws IOException {
+    int bufferSize = 8192;
+    byte[] buffer = new byte[bufferSize];
+
+    while (true) {
+      int bytesRead = in.read(buffer);
+      if (bytesRead == -1) {
+        break;
+      } else {
+        out.write(buffer, 0, bytesRead);
+      }
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (watcher != null) {
+      watcher.clear();
+      watcher.shutdown();
+    }
+  }
+}
---
+++ zeppelin-interpreter/src/test/java/org/apache/zeppelin/interpreter/InterpreterOutputChangeWatcherTest.java
@@ -0,0 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.zeppelin.interpreter;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InterpreterOutputChangeWatcherTest implements InterpreterOutputChangeListener {
+  private File tmpDir;
+  private File fileChanged;
+  private int numChanged;
+  private InterpreterOutputChangeWatcher watcher;
+
+  @Before
+  public void setUp() throws Exception {
+    watcher = new InterpreterOutputChangeWatcher(this);
+    watcher.start();
+
+    tmpDir = new File(System.getProperty("java.io.tmpdir")+"/ZeppelinLTest_"+System.currentTimeMillis());
+    tmpDir.mkdirs();
+    fileChanged = null;
+    numChanged = 0;
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    watcher.shutdown();
+    delete(tmpDir);
+  }
+
+  private void delete(File file){
+    if(file.isFile()) file.delete();
+    else if(file.isDirectory()){
+      File [] files = file.listFiles();
+      if(files!=null && files.length>0){
+        for(File f : files){
+          delete(f);
+        }
+      }
+      file.delete();
+    }
+  }
+
+
+  @Test
+  public void test() throws IOException, InterruptedException {
+    assertNull(fileChanged);
+    assertEquals(0, numChanged);
+
+    Thread.sleep(1000);
+    // create new file
+    File file1 = new File(tmpDir, "test1");
+    file1.createNewFile();
+
+    File file2 = new File(tmpDir, "test2");
+    file2.createNewFile();
+
+    watcher.watch(file1);
+    Thread.sleep(1000);
+
+    FileOutputStream out1 = new FileOutputStream(file1);
+    out1.write(1);
+    out1.close();
+
+    FileOutputStream out2 = new FileOutputStream(file2);
+    out2.write(1);
+    out2.close();
+
+    synchronized (this) {
+      wait(30*1000);
+    }
+
+    assertNotNull(fileChanged);
+    assertEquals(1, numChanged);
+  }
+
+
+  @Override
+  public void fileChanged(File file) {
+    fileChanged = file;
+    numChanged++;
+
+    synchronized(this) {
+      notify();
+    }
+  }
+
+}
