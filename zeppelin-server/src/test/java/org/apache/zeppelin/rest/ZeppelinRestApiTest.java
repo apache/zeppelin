62e673b711d29fb7ce1fbefd5bce26230de6523e
astroshim <hsshim@nflabs.com>
2015-12-29 19:57:15 +0900

    to rebuild

--- zeppelin-server/src/test/java/org/apache/zeppelin/rest/ZeppelinRestApiTest.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/rest/ZeppelinRestApiTest.java
@@ -167,7 +167,6 @@ public class ZeppelinRestApiTest extends AbstractTestRestApi {
     }
     assertEquals("<p>markdown</p>\n", p.getResult().message());
 
-    
     // restart interpreter
     for (InterpreterSetting setting : note.getNoteReplLoader().getInterpreterSettings()) {
       if (setting.getName().equals("md")) {
