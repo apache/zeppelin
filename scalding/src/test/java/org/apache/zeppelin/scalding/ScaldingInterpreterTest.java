8fdaaba94584ec5c9b8b5d8acb96016700720af1
Sriram Krishnan <skrishnan@twitter.com>
2015-12-30 22:25:49 -0800

    Add a (local mode) Scalding Interpreter to Zeppelin
    ### What is this PR for?
    Scalding (https://github.com/twitter/scalding) is a Scala library for writing MapReduce jobs.
    This issue tracks the addition of a Scalding interpreter for Zeppelin. To keep this work incremental, this PR will focus on just a local mode implementation. The Hadoop mode can be a subsequent addition.
    
    ### What type of PR is it?
    Feature
    
    ### Todos
    * Addition of Hadoop mode for Scalding
    
    ### Is there a relevant Jira issue?
    https://issues.apache.org/jira/browse/ZEPPELIN-526
    
    ### How should this be tested?
    Run the tests in: scalding/src/test/java/org/apache/zeppelin/scalding/ScaldingInterpreterTest.java
    
    ### Screenshots
    <img width="1167" alt="scalding-example" src="https://cloud.githubusercontent.com/assets/1509691/11944979/8788d5c2-a7ff-11e5-9863-2a1216c51896.png">
    <img width="1151" alt="scalding-screenshot" src="https://cloud.githubusercontent.com/assets/1509691/11944978/8787e3ec-a7ff-11e5-8383-456adb16b977.png">
    
    ### Questions:
    * This could use documentation, which could just be the example in the screenshot. Where can I contribute that?
    
    Author: Sriram Krishnan <skrishnan@twitter.com>
    
    Closes #561 from sriramkrishnan/scalding and squashes the following commits:
    
    ffa698b [Sriram Krishnan] Whitespace cleanup
    1ad405b [Sriram Krishnan] Adding newline to remove redundant change in PR
    8eec3c2 [Sriram Krishnan] Updating docs to include the -Pscalding profile for Scalding
    006500d [Sriram Krishnan] Reverting all commits to LICENSE to be back to master
    bc31d1e [Sriram Krishnan] Getting rid of added licenses
    b30725f [Sriram Krishnan] Making the Scalding interpreter optional as part of a new -Pscalding profile
    9a7d733 [Sriram Krishnan] Moved tukanni license to a separate section and added license
    dd0bb9a [Sriram Krishnan] Changing licenses to text format
    aaae5d1 [Sriram Krishnan] Moving licenses to the right location
    8be8d22 [Sriram Krishnan] Went thru and added all licenses I could find
    6019cc8 [Sriram Krishnan] More licenses. Only remaining ones are the dependencies of hadoop-common.
    460658a [Sriram Krishnan] Adding Cascading dependencies
    dd8a4c8 [Sriram Krishnan] Adding Scalding licenses
    083f059 [Sriram Krishnan] Trimming deps down from hadoop-client to just hadoop-common. Scalding REPL needs org.apache.hadoop.conf.Configuration even in local mode.
    5c8056c [Sriram Krishnan] Making the Scalding scala jars same as the Spark ones for consistency
    d4cf308 [Sriram Krishnan] Adding docs for the Scalding interpreter
    8004b39 [Sriram Krishnan] Fixing a typo
    91b0692 [Sriram Krishnan] adding ScaldingInterpreter
    5fd1ae4 [Sriram Krishnan] Address comments on PR. Merge remote-tracking branch 'upstream/master' into scalding
    7a9ceeb [Sriram Krishnan] Adding some tests for the Scalding interpreter
    7ec2941 [Sriram Krishnan] More code cleanup
    368dc04 [Sriram Krishnan] Cleaning up imports, comments, etc
    c27ec48 [Sriram Krishnan] Formatting, license
    8944b0c [Sriram Krishnan] Merge remote-tracking branch 'upstream/master' into scalding
    36a2dac [Sriram Krishnan] Added a link to the scalding code where the ILoop was lifted from.
    d3916b7 [Sriram Krishnan] Adding modified version of ScaldingILoop for grabbing Console output - will want to move this to Scalding itself
    1ffbb3b [Sriram Krishnan] Fixing output of stdout from console
    b19fda4 [Sriram Krishnan] More cleanup - flushing output stream. Still can't seem to get the Scala console output. Need to figure that out.
    e13576f [Sriram Krishnan] Now seem to be getting the console out, but only for last line. Will need some debugging.
    35fc032 [Sriram Krishnan] Initial version of a ScaldingInterpreter running in local mode. Need to get console output next. And add tests, and make it work for HDFS.
    721dcb7 [Sriram Krishnan] Getting a basic interpreter going. Next step is to hook in the Scalding REPL.

---
+++ scalding/src/test/java/org/apache/zeppelin/scalding/ScaldingInterpreterTest.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.zeppelin.scalding;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Properties;
+
+import org.apache.zeppelin.display.AngularObjectRegistry;
+import org.apache.zeppelin.display.GUI;
+import org.apache.zeppelin.interpreter.InterpreterContext;
+import org.apache.zeppelin.interpreter.InterpreterContextRunner;
+import org.apache.zeppelin.interpreter.InterpreterGroup;
+import org.apache.zeppelin.interpreter.InterpreterResult;
+import org.apache.zeppelin.interpreter.InterpreterResult.Code;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+/**
+ * Tests for the Scalding interpreter for Zeppelin.
+ *
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ScaldingInterpreterTest {
+  public static ScaldingInterpreter repl;
+  private InterpreterContext context;
+  private File tmpDir;
+
+  @Before
+  public void setUp() throws Exception {
+    tmpDir = new File(System.getProperty("java.io.tmpdir") + "/ZeppelinLTest_" + System.currentTimeMillis());
+    System.setProperty("zeppelin.dep.localrepo", tmpDir.getAbsolutePath() + "/local-repo");
+
+    tmpDir.mkdirs();
+
+    if (repl == null) {
+      Properties p = new Properties();
+
+      repl = new ScaldingInterpreter(p);
+      repl.open();
+    }
+
+    InterpreterGroup intpGroup = new InterpreterGroup();
+    context = new InterpreterContext("note", "id", "title", "text",
+        new HashMap<String, Object>(), new GUI(), new AngularObjectRegistry(
+            intpGroup.getId(), null),
+        new LinkedList<InterpreterContextRunner>());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    delete(tmpDir);
+    repl.close();
+  }
+
+  private void delete(File file) {
+    if (file.isFile()) file.delete();
+    else if (file.isDirectory()) {
+      File[] files = file.listFiles();
+      if (files != null && files.length > 0) {
+        for (File f : files) {
+          delete(f);
+        }
+      }
+      file.delete();
+    }
+  }
+
+  @Test
+  public void testBasicIntp() {
+    assertEquals(InterpreterResult.Code.SUCCESS,
+        repl.interpret("val a = 1\nval b = 2", context).code());
+
+    // when interpret incomplete expression
+    InterpreterResult incomplete = repl.interpret("val a = \"\"\"", context);
+    assertEquals(InterpreterResult.Code.INCOMPLETE, incomplete.code());
+    assertTrue(incomplete.message().length() > 0); // expecting some error
+                                                   // message
+  }
+
+  @Test
+  public void testBasicScalding() {
+    assertEquals(InterpreterResult.Code.SUCCESS,
+        repl.interpret("case class Sale(state: String, name: String, sale: Int)\n" +
+          "val salesList = List(Sale(\"CA\", \"A\", 60), Sale(\"CA\", \"A\", 20), Sale(\"VA\", \"B\", 15))\n" +
+          "val salesPipe = TypedPipe.from(salesList)\n" +
+          "val results = salesPipe.map{x => (1, Set(x.state), x.sale)}.\n" +
+          "    groupAll.sum.values.map{ case(count, set, sum) => (count, set.size, sum) }\n" +
+          "results.dump", 
+          context).code());
+  }
+
+  @Test
+  public void testNextLineInvocation() {
+    assertEquals(InterpreterResult.Code.SUCCESS, repl.interpret("\"123\"\n.toInt", context).code());
+  }
+
+  @Test
+  public void testEndWithComment() {
+    assertEquals(InterpreterResult.Code.SUCCESS, repl.interpret("val c=1\n//comment", context).code());
+  }
+
+  @Test
+  public void testReferencingUndefinedVal() {
+    InterpreterResult result = repl.interpret("def category(min: Int) = {"
+        + "    if (0 <= value) \"error\"" + "}", context);
+    assertEquals(Code.ERROR, result.code());
+  }
+}
