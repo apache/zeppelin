89c592489fab38e087604159fa0ed33ccef16991
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2016-01-08 08:37:24 +0100

    Shiro security v2
    Added Authentication.
    Once authenticated, a user has access to all notes.
    HTTP & Websocket channels are secured and require auth.
    This PR is based  on #53 which also implements user ownership on notes.
    
    Author: Hayssam Saleh <hayssam.saleh@ebiznext.com>
    
    Closes #586 from hayssams/shiro-security-v2 and squashes the following commits:
    
    47421b8 [Hayssam Saleh] Rollback classpath change since zeppelin conf dir already in classpath
    5485dcd [Hayssam Saleh] Updates licences for shiro-core and shiro-web introduced in this PR
    7200e77 [Hayssam Saleh] Default ticket / principal to anonymous in websocket message
    30736a0 [Hayssam Saleh] Add support for cross site requests with credentials
    1372231 [Hayssam Saleh] Test mode requires to user baseUrlSrv to connect to the REST API
    96ec240 [Hayssam Saleh] use standard HTML tags for SECURITY-README.md
    01ba543 [Hayssam Saleh] get ticket before Angular is bootstrapped
    2a9e275 [Hayssam Saleh] Add implementation notes
    96d1fac [Hayssam Saleh] correct comment in SECURITY-README and keep anonymous policy by default in zeppelin-site.xml.template
    6fd9982 [Hayssam Saleh] Add minimal shiro.ini file for test phase
    8eee51d [Hayssam Saleh] Remove cache optimization in shiro since it references stormpath and comes from there.
    2017925 [Hayssam Saleh] exclude SECURITY-README from rat check
    f9b1952 [Hayssam Saleh] The Websocket channel is now as secure as the HTTP channel.
    e2affca [Hayssam Saleh] Securing the HTTP channel only. Websocket security is done in the next commit

--- zeppelin-server/src/main/java/org/apache/zeppelin/server/ZeppelinServer.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/server/ZeppelinServer.java
@@ -36,6 +36,7 @@ import org.apache.zeppelin.notebook.repo.NotebookRepo;
 import org.apache.zeppelin.notebook.repo.NotebookRepoSync;
 import org.apache.zeppelin.rest.InterpreterRestApi;
 import org.apache.zeppelin.rest.NotebookRestApi;
+import org.apache.zeppelin.rest.SecurityRestApi;
 import org.apache.zeppelin.rest.ZeppelinRestApi;
 import org.apache.zeppelin.scheduler.SchedulerFactory;
 import org.apache.zeppelin.search.SearchService;
@@ -227,6 +228,12 @@ public class ZeppelinServer extends Application {
 
     cxfContext.addFilter(new FilterHolder(CorsFilter.class), "/*",
         EnumSet.allOf(DispatcherType.class));
+
+    cxfContext.addFilter(org.apache.shiro.web.servlet.ShiroFilter.class, "/*",
+        EnumSet.allOf(DispatcherType.class));
+
+    cxfContext.addEventListener(new org.apache.shiro.web.env.EnvironmentLoaderListener());
+
     return cxfContext;
   }
 
@@ -273,6 +280,9 @@ public class ZeppelinServer extends Application {
 
     InterpreterRestApi interpreterApi = new InterpreterRestApi(replFactory);
     singletons.add(interpreterApi);
+
+    SecurityRestApi securityApi = new SecurityRestApi();
+    singletons.add(securityApi);
 
     return singletons;
   }
