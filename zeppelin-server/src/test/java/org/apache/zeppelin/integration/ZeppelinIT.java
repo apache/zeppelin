d3437eff52d42e4c69df6fbb2649bc1268b87015
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2016-02-16 14:38:11 +0530

    trying to make CI fail again       WebElement paragraph2Editor = driver.findElement(By.xpath(getParagraphXPath(10) + "//textarea"));

--- zeppelin-server/src/test/java/org/apache/zeppelin/integration/ZeppelinIT.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/integration/ZeppelinIT.java
@@ -95,8 +95,8 @@ public class ZeppelinIT extends AbstractZeppelinIT {
        * Bind variable
        * z.angularBind("myVar", 1)
        */
-      assertEquals(10, driver.findElements(By.xpath(getParagraphXPath(2) + "//textarea")).size());
-      WebElement paragraph2Editor = driver.findElement(By.xpath(getParagraphXPath(2) + "//textarea"));
+      assertEquals(1, driver.findElements(By.xpath(getParagraphXPath(2) + "//textarea")).size());
+      WebElement paragraph2Editor = driver.findElement(By.xpath(getParagraphXPath(10) + "//textarea"));
       paragraph2Editor.sendKeys("z.angularBind" + Keys.chord(Keys.SHIFT, "9") + "\"myVar\", 1)");
       paragraph2Editor.sendKeys(Keys.chord(Keys.SHIFT, Keys.ENTER));
       waitForParagraph(2, "FINISHED");
