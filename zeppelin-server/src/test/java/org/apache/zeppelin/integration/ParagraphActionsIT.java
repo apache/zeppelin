3a99ac2694087ede4fcd27327e7ff3944a11e29d
Ravi Ranjan <ranjanravi0308@gmail.com>
2016-02-17 11:23:40 +0530

    Add LOG.error message in catch statement of test

--- zeppelin-server/src/test/java/org/apache/zeppelin/integration/ParagraphActionsIT.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/integration/ParagraphActionsIT.java
@@ -84,7 +84,9 @@ public class ParagraphActionsIT extends AbstractZeppelinIT {
       deleteTestNotebook(driver);
 
     } catch (ElementNotVisibleException e) {
-      File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        LOG.error("Exception in ParagraphActionsIT while testClearOutputButton ", e);
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        throw e;
     }
 
   }
