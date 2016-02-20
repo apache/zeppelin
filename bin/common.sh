d700872761b52d0a83b315af997a9d4721660102
Luciano Resende <lresende@apache.org>
2016-02-20 00:40:46 -0800

    [ZEPPELIN-408] Properly honor notebook dir from xml configuration

--- bin/common.sh
+++ bin/common.sh
@@ -36,10 +36,6 @@ if [[ -z "${ZEPPELIN_LOG_DIR}" ]]; then
   export ZEPPELIN_LOG_DIR="${ZEPPELIN_HOME}/logs"
 fi
 
-if [[ -z "${ZEPPELIN_NOTEBOOK_DIR}" ]]; then
-  export ZEPPELIN_NOTEBOOK_DIR="${ZEPPELIN_HOME}/notebook"
-fi
-
 if [[ -z "$ZEPPELIN_PID_DIR" ]]; then
   export ZEPPELIN_PID_DIR="${ZEPPELIN_HOME}/run"
 fi
