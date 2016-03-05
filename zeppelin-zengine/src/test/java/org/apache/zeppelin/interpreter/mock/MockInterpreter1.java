7424af247b08afabbd9d080a288a88a7de4b7a1c
Lee moon soo <moon@apache.org>
2016-03-04 22:08:19 -0800

    Remove resource on note / paragraph removal

--- zeppelin-zengine/src/test/java/org/apache/zeppelin/interpreter/mock/MockInterpreter1.java
+++ zeppelin-zengine/src/test/java/org/apache/zeppelin/interpreter/mock/MockInterpreter1.java
@@ -1,19 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package org.apache.zeppelin.interpreter.mock;
 
@@ -29,30 +29,30 @@ import org.apache.zeppelin.scheduler.Scheduler;
 import org.apache.zeppelin.scheduler.SchedulerFactory;
 
 public class MockInterpreter1 extends Interpreter{
-  Map<String, Object> vars = new HashMap<String, Object>();
+Map<String, Object> vars = new HashMap<String, Object>();
 
-	public MockInterpreter1(Properties property) {
-		super(property);
+public MockInterpreter1(Properties property) {
+	super(property);
+}
+
+@Override
+public void open() {
+}
+
+@Override
+public void close() {
+}
+
+@Override
+public InterpreterResult interpret(String st, InterpreterContext context) {
+	InterpreterResult result;
+
+	if ("getId".equals(st)) {
+		// get unique id of this interpreter instance
+		result = new InterpreterResult(InterpreterResult.Code.SUCCESS, "" + this.hashCode());
+	} else {
+		result = new InterpreterResult(InterpreterResult.Code.SUCCESS, "repl1: " + st);
 	}
-
-	@Override
-	public void open() {
-	}
-
-	@Override
-	public void close() {
-	}
-
-	@Override
-	public InterpreterResult interpret(String st, InterpreterContext context) {
-		InterpreterResult result;
-
-		if ("getId".equals(st)) {
-			// get unique id of this interpreter instance
-			result = new InterpreterResult(InterpreterResult.Code.SUCCESS, "" + this.hashCode());
-		} else {
-			result = new InterpreterResult(InterpreterResult.Code.SUCCESS, "repl1: " + st);
-		}
 
 		if (context.getResourcePool() != null) {
 			context.getResourcePool().put(context.getNoteId(), context.getParagraphId(), "result", result);
