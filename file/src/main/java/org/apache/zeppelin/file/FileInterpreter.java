b45663d227c222d3d5c1e9b64fac5df48509bd1a
Tom Runyon <runyontr@gmail.com>
2016-03-13 22:13:00 -0400

    ZEPPELIN-198 HDFS File Interpreter
    ### What is this PR for?
    This pull request is a follow of https://github.com/apache/incubator-zeppelin/pull/276 started by raj-bains.  The additional commits address comments from the pull request regarding string creation and error propagation for bad object requests.
    
    ### What type of PR is it?
    [Bug Fix | Improvement | Feature | Documentation | Hot Fix | Refactoring]
    Feature/Subtask
    ### Todos
    
    ### Is there a relevant Jira issue?
    [ZEPPELIN-198](https://issues.apache.org/jira/browse/ZEPPELIN-198)
    ### How should this be tested?
    Outline the steps to test the PR here.
    
    ### Screenshots (if appropriate)
    
    ![selection_001](https://cloud.githubusercontent.com/assets/9200575/13370345/87a922ac-dcd2-11e5-9217-66ec21c016de.png)
    ![selection_002](https://cloud.githubusercontent.com/assets/9200575/13370347/89a5603e-dcd2-11e5-9555-9c38167e8667.png)
    ![selection_003](https://cloud.githubusercontent.com/assets/9200575/13370359/b9bbadc8-dcd2-11e5-97bd-47d216f0c7da.png)
    ![selection_004](https://cloud.githubusercontent.com/assets/9200575/13370364/16ff9274-dcd3-11e5-9b4c-f324e7035c20.png)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Tom Runyon <runyontr@gmail.com>
    Author: Raj Bains <rajbains@Rajs-MacBook-Pro.local>
    
    Closes #752 from runyontr/master and squashes the following commits:
    
    f7bfef8 [Tom Runyon] ZEPPELIN-198 added transitive dependency lincense information
    af16ce0 [Tom Runyon] ZEPPELIN-198 removed GPL2 reference from license file
    5e9e131 [Tom Runyon] ZEPPELIN-198 Updated ZeppelinConfiguration to hold HDFS interpreter
    9832622 [Tom Runyon] ZEPPELIN-198 Changed group for hdfs interpreter
    c34a913 [Tom Runyon] ZEPPELIN-198 Updated licenses file to include org.glassfish.jersey.core
    8d0ee3d [Tom Runyon] Merge https://github.com/apache/incubator-zeppelin
    9f66514 [Tom Runyon] ZEPPELIN-198 Removed extra copy of configuration table and fixed formatting issues for hdfs.md
    5938b0e [Tom Runyon] ZEPPELIN-198 Updated documentation to be consistent with naming between pom file and interpreter documentation.
    67bbc5b [Tom Runyon] ZEPPELIN-198 Added navigation to hdfs interpreter
    1c7a5c2 [Tom Runyon] ZEPPELIN-198 removed errant text in documentation.
    56a5174 [Tom Runyon] ZEPPELIN-198 fixed logging to match standards
    933c890 [Tom Runyon] MAINT Updated .gitignore file to remove zeppelin-server/derby.log
    29540df [Tom Runyon] ZEPPELIN-198 removed zeppelin-server/derby.log
    71d53d3 [Tom Runyon] ZEPPELIN-198 Changed pom name to Zeppelin File System Interpreters to match functionality
    aec0512 [Tom Runyon] ZEPPELIN-198 Fixed compile error for error logging.
    d24f4c0 [Tom Runyon] ZEPPELIN-198 Added error logging when returning error in interpet
    227b815 [Tom Runyon] ZEPPELIN-198 Updated interpreter documentation.
    b505391 [Tom Runyon] ZEPPELIN-198 Added completion functionality to HDFSInterpreter
    32ed7cb [Tom Runyon] ZEPPELIN-198 Added -h flag for human readable byte sizes.  Updated string creation to StringBuilder.
    797fd29 [Tom Runyon] Added org.glassfish.jersey.core to pom.xml file for hdfs intepretor
    27e0438 [Tom Runyon] Modified string creation to use StringBuilder
    79f0d90 [Tom Runyon] Merge branch 'master' of http://github.com/raj-bains/incubator-zeppelin
    70507a8 [Raj Bains] Add Documentation and a missing dependency for HDFS File Browser
    1239fe6 [Raj Bains] Merge remote-tracking branch 'upstream/master'
    7d61e5f [Raj Bains] This is the first reviewed version of File Interpreter that adds basic ls, cd and pwd functionality against WebHDFS. It addresses ZEPPELIN-198
    865e6ab [Raj Bains] Add File Interpreter, HDFS Interpreter and Tests

---
+++ file/src/main/java/org/apache/zeppelin/file/FileInterpreter.java
@@ -0,0 +1,171 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.zeppelin.file;
+
+import org.apache.zeppelin.interpreter.Interpreter;
+import org.apache.zeppelin.interpreter.InterpreterContext;
+import org.apache.zeppelin.interpreter.InterpreterResult;
+import org.apache.zeppelin.interpreter.InterpreterResult.Code;
+import org.apache.zeppelin.interpreter.InterpreterResult.Type;
+import org.apache.zeppelin.scheduler.Scheduler;
+import org.apache.zeppelin.scheduler.SchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+/**
+ * File interpreter for Zeppelin.
+ *
+ */
+public abstract class FileInterpreter extends Interpreter {
+  Logger logger = LoggerFactory.getLogger(FileInterpreter.class);
+  String currentDir = null;
+  CommandArgs args = null;
+
+  public FileInterpreter(Properties property) {
+    super(property);
+    currentDir = new String("/");
+  }
+
+  /**
+   * Handling the arguments of the command
+   */
+  public class CommandArgs {
+    public String input = null;
+    public String command = null;
+    public ArrayList<String> args = null;
+    public HashSet<Character> flags = null;
+
+    public CommandArgs(String cmd) {
+      input = cmd;
+      args = new ArrayList();
+      flags = new HashSet();
+    }
+
+    private void parseArg(String arg) {
+      if (arg.charAt(0) == '-') {                   // handle flags
+        for (int i = 0; i < arg.length(); i++) {
+          Character c = arg.charAt(i);
+          flags.add(c);
+        }
+      } else {                                      // handle other args
+        args.add(arg);
+      }
+    }
+
+    public void parseArgs() {
+      if (input == null)
+        return;
+      StringTokenizer st = new StringTokenizer(input);
+      if (st.hasMoreTokens()) {
+        command = st.nextToken();
+        while (st.hasMoreTokens())
+          parseArg(st.nextToken());
+      }
+    }
+  }
+
+  // Functions that each file system implementation must override
+
+  public abstract String listAll(String path);
+
+  public abstract boolean isDirectory(String path);
+
+  // Combine paths, takes care of arguments such as ..
+
+  protected String getNewPath(String argument){
+    Path arg = Paths.get(argument);
+    Path ret = arg.isAbsolute() ? arg : Paths.get(currentDir, argument);
+    return ret.normalize().toString();
+  }
+
+  // Handle the command handling uniformly across all file systems
+
+  @Override
+  public InterpreterResult interpret(String cmd, InterpreterContext contextInterpreter) {
+    logger.info("Run File command '" + cmd + "'");
+
+    args = new CommandArgs(cmd);
+    args.parseArgs();
+
+    if (args.command == null) {
+      logger.info("Error: No command");
+      return new InterpreterResult(Code.ERROR, Type.TEXT, "No command");
+    }
+
+    // Simple parsing of the command
+
+    if (args.command.equals("cd")) {
+
+      String newPath = !args.args.isEmpty() ? getNewPath(args.args.get(0)) : currentDir;
+      if (!isDirectory(newPath))
+        return new InterpreterResult(Code.ERROR, Type.TEXT, newPath + ": No such directory");
+
+      currentDir = newPath;
+      return new InterpreterResult(Code.SUCCESS, Type.TEXT, "OK");
+
+    } else if (args.command.equals("ls")) {
+
+      String newPath = !args.args.isEmpty() ? getNewPath(args.args.get(0)) : currentDir;
+      try {
+        String results = listAll(newPath);
+        return new InterpreterResult(Code.SUCCESS, Type.TEXT, results);
+      } catch (Exception e) {
+        logger.error("Error listing files in path " + newPath, e);
+        return new InterpreterResult(Code.ERROR, Type.TEXT, e.getMessage());
+      }
+
+    } else if (args.command.equals("pwd")) {
+
+      return new InterpreterResult(Code.SUCCESS, Type.TEXT, currentDir);
+
+    } else {
+
+      return new InterpreterResult(Code.ERROR, Type.TEXT, "Unknown command");
+
+    }
+  }
+
+  @Override
+  public void cancel(InterpreterContext context) {
+  }
+
+  @Override
+  public FormType getFormType() {
+    return FormType.SIMPLE;
+  }
+
+  @Override
+  public int getProgress(InterpreterContext context) {
+    return 0;
+  }
+
+  @Override
+  public Scheduler getScheduler() {
+    return SchedulerFactory.singleton().createOrGetFIFOScheduler(
+        FileInterpreter.class.getName() + this.hashCode());
+  }
+
+  @Override
+  public List<String> completion(String buf, int cursor) {
+    return null;
+  }
+}
--- zeppelin-zengine/src/main/java/org/apache/zeppelin/conf/ZeppelinConfiguration.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/conf/ZeppelinConfiguration.java
@@ -450,6 +450,7 @@ public class ZeppelinConfiguration extends XMLConfiguration {
         + "org.apache.zeppelin.shell.ShellInterpreter,"
         + "org.apache.zeppelin.hive.HiveInterpreter,"
         + "org.apache.zeppelin.alluxio.AlluxioInterpreter,"
+        + "org.apache.zeppelin.file.HDFSFileInterpreter,"
         + "org.apache.zeppelin.phoenix.PhoenixInterpreter,"
         + "org.apache.zeppelin.postgresql.PostgreSqlInterpreter,"
         + "org.apache.zeppelin.tajo.TajoInterpreter,"
