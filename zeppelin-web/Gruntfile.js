5bf0a29210ded8a665ecfee17582d6a89ebb7edb
Ben Tedder <ben.tedder@digitalreasoning.com>
2016-04-13 09:11:07 -0500

    fix

--- zeppelin-web/Gruntfile.js
+++ zeppelin-web/Gruntfile.js
@@ -336,7 +336,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,txt,svg}',
             '.htaccess',
             '*.html',
             'assets/styles/**/*',
