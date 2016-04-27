a1c5f52c6bffc9fe3a8386c9bd7b2f4e28d02d2c
Anthony Corbacho <corbacho.anthony@gmail.com>
2016-04-27 12:02:03 +0900

    prerequisite changes for starting websocket client
    it includes creating new websocket package, skeleton client and moving some existing classes (e.g. Message.java) from server to zengine

---
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/zeppelinhub/websocket/Client.java
@@ -0,0 +1,16 @@
+package org.apache.zeppelin.notebook.repo.zeppelinhub.websocket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO(xxx): Add description
+ * 
+ */
+public class Client {
+  private Logger LOG = LoggerFactory.getLogger(Client.class);
+  
+  public Client() {
+    LOG.debug("Init Client");
+  }
+}
