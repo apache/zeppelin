d6348c76f03b52302952f38541d0845072a00cdd
wind0727 <82651730@qq.com>
2016-05-19 18:26:04 +0800

    Update NotebookRestApi.java

--- zeppelin-server/src/main/java/org/apache/zeppelin/rest/NotebookRestApi.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/rest/NotebookRestApi.java
@@ -142,21 +142,18 @@ public class NotebookRestApi {
     HashSet readers = permMap.get("readers");
     HashSet owners = permMap.get("owners");
     HashSet writers = permMap.get("writers");
-    // Set readers, if writers and owners is empty -> set to user requesting the change
-    if (readers != null && !readers.isEmpty()) {
-      if (writers.isEmpty()) {
-        writers = Sets.newHashSet(SecurityUtils.getPrincipal());
-      }
+    String princial = SecurityUtils.getPrincipal();
+    if (!"anonymous".equals(princial)) {
       if (owners.isEmpty()) {
-        owners = Sets.newHashSet(SecurityUtils.getPrincipal());
+        owners = Sets.newHashSet(principal);
       }
-    }
-    // Set writers, if owners is empty -> set to user requesting the change
-    if ( writers != null && !writers.isEmpty()) {
-      if (owners.isEmpty()) {
-        owners = Sets.newHashSet(SecurityUtils.getPrincipal());
+      // Set writers, if owners is empty -> set to user requesting the change
+      if (readers != null && !readers.isEmpty()) {
+        if (writers.isEmpty()) {
+          writers = Sets.newHashSet(principal);
+        }
       }
-    }
+    }  
   
 
     notebookAuthorization.setReaders(noteId, readers);
@@ -720,9 +717,7 @@ public class NotebookRestApi {
     for (int i = 0; i < notebooksFound.size(); i++) {
       String[] Id = notebooksFound.get(i).get("id").split("/", 2);
       String noteId = Id[0];
-      if (!notebookAuthorization.isOwner(noteId, userAndRoles) &&
-              !notebookAuthorization.isReader(noteId, userAndRoles) &&
-              !notebookAuthorization.isWriter(noteId, userAndRoles)) {
+      if (!notebookAuthorization.checkNoteUser(noteId, userAndRoles)) {
         notebooksFound.remove(i);
         i--;
       }
