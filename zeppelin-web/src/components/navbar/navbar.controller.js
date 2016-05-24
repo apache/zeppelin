bca027b44bd91e826f3859e90129712bdf4dd134
CloverHearts <cloverheartsdev@gmail.com>
2016-05-24 10:59:33 +0900

    remove whitespace

--- zeppelin-web/src/components/navbar/navbar.controller.js
+++ zeppelin-web/src/components/navbar/navbar.controller.js
@@ -16,7 +16,6 @@
 
 angular.module('zeppelinWebApp').controller('NavCtrl', function($scope, $rootScope, $http, $routeParams,
     $location, notebookListDataFactory, baseUrlSrv, websocketMsgSrv, arrayOrderingSrv) {
-
   /** Current list of notes (ids) */
 
   $scope.showLoginWindow = function() {
