bff2217ee9d6b3432e33a49b1aaa710d5b2aea74
Lee moon soo <moon@apache.org>
2016-06-08 15:59:25 -0700

    Fix SparkParagraphIT

--- zeppelin-server/src/test/java/org/apache/zeppelin/integration/SparkParagraphIT.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/integration/SparkParagraphIT.java
@@ -165,7 +165,7 @@ public class SparkParagraphIT extends AbstractZeppelinIT {
       }
 
       WebElement paragraph1Result = driver.findElement(By.xpath(
-          getParagraphXPath(1) + "//div[@class=\"tableDisplay\"]"));
+          getParagraphXPath(1) + "//div[@class=\"tableDisplay\"]/div/div/div"));
       collector.checkThat("Paragraph from SparkParagraphIT of testSqlSpark result: ",
           paragraph1Result.getText().toString(), CoreMatchers.equalTo("age\njob\nmarital\neducation\nbalance\n30" +
                       " unemployed married primary 1,787\nage\njob\nmarital\neducation\nbalance"));
