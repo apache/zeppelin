99ce452b68c757c1959b9d4b7983f7d81000256e
Jongyoul Lee <jongyoul@gmail.com>
2016-06-09 23:13:21 +0900

    Updated license document

--- jdbc/src/main/java/org/apache/zeppelin/jdbc/SqlCompleter.java
+++ jdbc/src/main/java/org/apache/zeppelin/jdbc/SqlCompleter.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -40,7 +40,7 @@ import java.util.regex.Pattern;
 import static org.apache.commons.lang.StringUtils.isBlank;
 
 /**
- * SQL auto complete functionality for the PostgreSqlInterpreter.
+ * SQL auto complete functionality for the JdbcInterpreter.
  */
 public class SqlCompleter extends StringsCompleter {
 
