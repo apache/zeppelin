05e0f366bf1ad015e3406859c8d41b1a585710ca
AhyoungRyu <fbdkdud93@hanmail.net>
2016-06-12 11:00:00 -0700

    Change anchor placement right->left

--- docs/assets/themes/zeppelin/js/docs.js
+++ docs/assets/themes/zeppelin/js/docs.js
@@ -110,7 +110,7 @@ $(function() {
   // Display anchor links when hovering over headers. For documentation of the
   // configuration options, see the AnchorJS documentation.
   anchors.options = {
-    placement: 'right'
+    placement: 'left'
   };
   anchors.add();
 
