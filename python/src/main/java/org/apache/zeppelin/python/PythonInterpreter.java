ccbf7efa563f8ab3bb505eca4032b7d045ef8c14
Kwon, Yeong-Eon <happylogin@gmail.com>
2016-06-14 09:37:27 +0900

    Prevent NullPointerException if "gatewayServer" does not exist

--- python/src/main/java/org/apache/zeppelin/python/PythonInterpreter.java
+++ python/src/main/java/org/apache/zeppelin/python/PythonInterpreter.java
@@ -125,8 +125,10 @@ public class PythonInterpreter extends Interpreter {
 
     logger.info("closing Python interpreter .....");
     try {
-      process.close();
-      gatewayServer.shutdown();
+      if (process != null)
+        process.close();
+      if (gatewayServer != null)
+        gatewayServer.shutdown();
     } catch (IOException e) {
       logger.error("Can't close the interpreter", e);
     }
