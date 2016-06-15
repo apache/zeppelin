a3eb67673c30e10efe181810c554729b5d8250e9
Mina Lee <minalee@apache.org>
2016-06-15 16:01:23 -0700

    Include zeppelin-server module in publish artifiact

--- dev/publish_release.sh
+++ dev/publish_release.sh
@@ -53,7 +53,7 @@ RELEASE_VERSION="$1"
 GIT_TAG="$2"
 
 PUBLISH_PROFILES="-Pspark-1.6 -Phadoop-2.4 -Pyarn -Ppyspark -Psparkr -Pr"
-PROJECT_OPTIONS="-pl !zeppelin-server,!zeppelin-distribution"
+PROJECT_OPTIONS="-pl !zeppelin-distribution"
 NEXUS_STAGING="https://repository.apache.org/service/local/staging"
 NEXUS_PROFILE="153446d1ac37c4"
 
