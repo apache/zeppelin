21f5beeb5b95aebfe94a600b138558154fef8fa2
CloverHearts <cloverheartsdev@gmail.com>
2016-06-21 03:36:28 +0900

    fixed create new button test case 'automatically adds % interpreter tag'

--- zeppelin-server/src/test/java/org/apache/zeppelin/integration/ParagraphActionsIT.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/integration/ParagraphActionsIT.java
@@ -94,7 +94,7 @@ public class ParagraphActionsIT extends AbstractZeppelinIT {
 
       collector.checkThat("Paragraph is created above",
         driver.findElement(By.xpath(getParagraphXPath(1) + "//div[contains(@class, 'editor')]")).getText(),
-        CoreMatchers.equalTo(StringUtils.EMPTY));
+        CoreMatchers.equalTo("%spark"));
       setTextOfParagraph(1, " this is above ");
 
       newPara = driver.findElement(By.xpath(getParagraphXPath(2) + "//div[contains(@class,'new-paragraph')][2]"));
@@ -104,7 +104,7 @@ public class ParagraphActionsIT extends AbstractZeppelinIT {
 
       collector.checkThat("Paragraph is created below",
         driver.findElement(By.xpath(getParagraphXPath(3) + "//div[contains(@class, 'editor')]")).getText(),
-        CoreMatchers.equalTo(StringUtils.EMPTY));
+        CoreMatchers.equalTo("%spark"));
       setTextOfParagraph(3, " this is below ");
 
       collector.checkThat("The output field of paragraph1 contains",
@@ -128,39 +128,39 @@ public class ParagraphActionsIT extends AbstractZeppelinIT {
     }
 
   }
-//
-//  @Test
-//  public void testRemoveButton() throws Exception {
-//    if (!endToEndTestEnabled()) {
-//      return;
-//    }
-//    try {
-//      createNewNote();
-//
-//      waitForParagraph(1, "READY");
-//      driver.findElement(By.xpath(getParagraphXPath(1) + "//span[@class='icon-settings']")).click();
-//      driver.findElement(By.xpath(getParagraphXPath(1) + "//ul/li/a[@ng-click='insertNew()']")).click();
-//      waitForParagraph(2, "READY");
-//      Integer oldNosOfParas = driver.findElements(By.xpath("//div[@ng-controller=\"ParagraphCtrl\"]")).size();
-//      collector.checkThat("Before Remove : Number of paragraphs are ",
-//        oldNosOfParas,
-//        CoreMatchers.equalTo(2));
-//      driver.findElement(By.xpath(getParagraphXPath(1) + "//span[@class='icon-settings']")).click();
-//      driver.findElement(By.xpath(getParagraphXPath(1) + "//ul/li/a[@ng-click='removeParagraph()']")).click();
-//      sleep(1000, true);
-//      driver.findElement(By.xpath("//div[@class='modal-dialog'][contains(.,'delete this paragraph')]" +
-//        "//div[@class='modal-footer']//button[contains(.,'OK')]")).click();
-//      Integer newNosOfParas = driver.findElements(By.xpath("//div[@ng-controller=\"ParagraphCtrl\"]")).size();
-//      collector.checkThat("After Remove : Number of paragraphs are",
-//        oldNosOfParas-1,
-//        CoreMatchers.equalTo(newNosOfParas));
-//      ZeppelinITUtils.sleep(1000, false);
-//      deleteTestNotebook(driver);
-//
-//    } catch (Exception e) {
-//      handleException("Exception in ParagraphActionsIT while testRemoveButton ", e);
-//    }
-//  }
+
+  @Test
+  public void testRemoveButton() throws Exception {
+    if (!endToEndTestEnabled()) {
+      return;
+    }
+    try {
+      createNewNote();
+
+      waitForParagraph(1, "READY");
+      driver.findElement(By.xpath(getParagraphXPath(1) + "//span[@class='icon-settings']")).click();
+      driver.findElement(By.xpath(getParagraphXPath(1) + "//ul/li/a[@ng-click='insertNew()']")).click();
+      waitForParagraph(2, "READY");
+      Integer oldNosOfParas = driver.findElements(By.xpath("//div[@ng-controller=\"ParagraphCtrl\"]")).size();
+      collector.checkThat("Before Remove : Number of paragraphs are ",
+        oldNosOfParas,
+        CoreMatchers.equalTo(2));
+      driver.findElement(By.xpath(getParagraphXPath(1) + "//span[@class='icon-settings']")).click();
+      driver.findElement(By.xpath(getParagraphXPath(1) + "//ul/li/a[@ng-click='removeParagraph()']")).click();
+      sleep(1000, true);
+      driver.findElement(By.xpath("//div[@class='modal-dialog'][contains(.,'delete this paragraph')]" +
+        "//div[@class='modal-footer']//button[contains(.,'OK')]")).click();
+      Integer newNosOfParas = driver.findElements(By.xpath("//div[@ng-controller=\"ParagraphCtrl\"]")).size();
+      collector.checkThat("After Remove : Number of paragraphs are",
+        oldNosOfParas-1,
+        CoreMatchers.equalTo(newNosOfParas));
+      ZeppelinITUtils.sleep(1000, false);
+      deleteTestNotebook(driver);
+
+    } catch (Exception e) {
+      handleException("Exception in ParagraphActionsIT while testRemoveButton ", e);
+    }
+  }
 
   @Test
   public void testMoveUpAndDown() throws Exception {
