af8a16ef92ff271885e1c26cb55d56ca00cc38d0
CloverHearts <cloverheartsdev@gmail.com>
2016-06-28 16:50:04 +0900

    Ace-editor hidden auto-complete additional events.

--- zeppelin-web/src/app/notebook/notebook.controller.js
+++ zeppelin-web/src/app/notebook/notebook.controller.js
@@ -956,8 +956,12 @@ angular.module('zeppelinWebApp').controller('NotebookCtrl',
     $scope.suggestions = [];
   };
 
-angular.element(document).click(function(){
-     angular.element('.userlist').hide();
-});
+  angular.element(document).click(function(){
+       angular.element('.userlist').hide();
+  });
+
+  angular.element(document).click(function(){
+    angular.element('.ace_autocomplete').hide();
+  });
 
 });
