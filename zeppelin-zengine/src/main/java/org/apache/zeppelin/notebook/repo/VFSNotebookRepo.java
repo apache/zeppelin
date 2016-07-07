a9d064c90f8830779b4339dcf60edff94cc228a8
Renjith Kamath <renjith.kamath@gmail.com>
2016-07-07 17:58:56 +0530

    ZEPPELIN-1129 Zeppelin fails to load notebooks with old date format
    ### What is this PR for?
    Old notebook fails to load due to date parse exception
    
    ### What type of PR is it?
    Bug Fix
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/ZEPPELIN-1129
    
    ### How should this be tested?
    Keep a notebook with old date format in the notebook dir and reload notebooks on zeppelin home page
    
    ### Questions:
    * Does the licenses files need update? n/a
    * Is there breaking changes for older versions? n/a
    * Does this needs documentation? n/a
    
    Author: Renjith Kamath <renjith.kamath@gmail.com>
    
    Closes #1146 from r-kamath/ZEPPELIN-1129 and squashes the following commits:
    
    901bf2b [Renjith Kamath] ZEPPELIN-1129 Zeppelin fails to load notebooks with old date format
    5391d12 [Renjith Kamath] ZEPPELIN-1129 Zeppelin fails to load notebooks with old format

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/VFSNotebookRepo.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/VFSNotebookRepo.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -40,6 +41,7 @@ import org.apache.zeppelin.notebook.ApplicationState;
 import org.apache.zeppelin.notebook.Note;
 import org.apache.zeppelin.notebook.NoteInfo;
 import org.apache.zeppelin.notebook.Paragraph;
+import org.apache.zeppelin.notebook.NotebookImportDeserializer;
 import org.apache.zeppelin.scheduler.Job.Status;
 import org.apache.zeppelin.user.AuthenticationInfo;
 import org.slf4j.Logger;
@@ -158,7 +160,8 @@ public class VFSNotebookRepo implements NotebookRepo {
 
     GsonBuilder gsonBuilder = new GsonBuilder();
     gsonBuilder.setPrettyPrinting();
-    Gson gson = gsonBuilder.create();
+    Gson gson = gsonBuilder.registerTypeAdapter(Date.class, new NotebookImportDeserializer())
+        .create();
 
     FileContent content = noteJson.getContent();
     InputStream ins = content.getInputStream();
