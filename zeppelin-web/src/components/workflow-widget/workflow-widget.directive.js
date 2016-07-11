7423afd7bd89000d5d3cb34e3d464a10601f974f
CloverHearts <cloverheartsdev+github@gmail.com>
2016-07-12 07:59:03 +0900

    workflow web controller

---
+++ zeppelin-web/src/components/workflow-widget/workflow-widget.directive.js
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+angular.module('zeppelinWebApp').controller('workflowWidgetCtrl', function($scope, notebookListDataFactory,
+                                                                           $rootScope, $routeParams, websocketMsgSrv) {
+  console.log('data ', $scope.workflowJobInputData);
+
+  $scope.workflowJobLists = [];
+
+  $scope.$watch('workflowJobInputData', function () {
+    $scope.workflowJobLists = [];
+    $scope.workflowJobInputData.map(function (item) {
+      var workflowJob = item.value.split(':');
+      if (workflowJob[0] == undefined || workflowJob[1] == undefined) {
+        return;
+      }
+      $scope.workflowJobLists.push({notebookId :workflowJob[0], paragraphId:workflowJob[1]});
+    });
+  })
+
+
+  })
+  .directive('workflowWidget', function() {
+  return {
+    restrict: 'E',
+    scope : {
+      'workflowJobInputData' : '=workflowJobInputData',
+      'workflowJobResultData' : '=workflowJobResultData'
+    },
+    templateUrl : 'components/workflow-widget/workflow-widget.html',
+    link: function(scope, elem, attrs) {
+
+    }
+  };
+});
