1d291accdddf4ca90059dc8289ea753d38566916
AhyoungRyu <fbdkdud93@hanmail.net>
2016-07-11 18:31:58 +0900

    Redirect to home when unauthorized user click 'credentials'

--- zeppelin-web/src/app/credential/credential.controller.js
+++ zeppelin-web/src/app/credential/credential.controller.js
@@ -30,6 +30,16 @@ angular.module('zeppelinWebApp').controller('CredentialCtrl', function($scope, $
       console.log('Success %o %o', status, $scope.credentialInfo);
     }).
     error(function(data, status, headers, config) {
+      if (status === 401) {
+        ngToast.danger({
+          content: 'You don\'t have permission on this page',
+          verticalPosition: 'bottom',
+          timeout: '3000'
+        });
+        setTimeout(function() {
+          window.location.replace('/');
+        }, 3000);
+      }
       console.log('Error %o %o', status, data.message);
     });
   };
