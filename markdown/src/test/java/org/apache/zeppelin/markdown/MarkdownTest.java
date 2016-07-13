21240ceebfc59a40b7d4defe24fc50d39a7305c0
Jeff Zhang <zjffdu@apache.org>
2016-07-13 15:16:34 +0800

    remove unnecessary code

--- markdown/src/test/java/org/apache/zeppelin/markdown/MarkdownTest.java
+++ markdown/src/test/java/org/apache/zeppelin/markdown/MarkdownTest.java
@@ -43,7 +43,6 @@ public class MarkdownTest {
 		md.open();
 		InterpreterResult result = md.interpret("This is ~~deleted~~ text", null);
 		assertEquals("<p>This is <s>deleted</s> text</p>\n", result.message());
-		System.out.println(MarkdownTest.class.getName());
 	}
 
 }
