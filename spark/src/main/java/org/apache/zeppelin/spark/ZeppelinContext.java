f06a2fa8117bffb99b69d65f08914d51384962fe
Lee moon soo <moon@apache.org>
2016-07-16 08:51:16 +0900

    Spark 2.0 support

--- spark/src/main/java/org/apache/zeppelin/spark/ZeppelinContext.java
+++ spark/src/main/java/org/apache/zeppelin/spark/ZeppelinContext.java
@@ -31,7 +31,6 @@ import java.util.List;
 import org.apache.spark.SparkContext;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.catalyst.expressions.Attribute;
-import org.apache.spark.sql.hive.HiveContext;
 import org.apache.zeppelin.annotation.ZeppelinApi;
 import org.apache.zeppelin.display.AngularObject;
 import org.apache.zeppelin.display.AngularObjectRegistry;
@@ -70,7 +69,6 @@ public class ZeppelinContext {
 
   public SparkContext sc;
   public SQLContext sqlContext;
-  public HiveContext hiveContext;
   private GUI gui;
 
   @ZeppelinApi
