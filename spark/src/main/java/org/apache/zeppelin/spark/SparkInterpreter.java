ace14afa3c27f14c66a1a81af683aa1a1a9424ec
Karup <karuppayya@outlook.com>
2016-07-22 12:39:20 +0530

    ZEPPELIN-1224: Fix typo in method name
    ### What is this PR for?
    Get  the correct method to invoke, for autocompletion
    
    ### What type of PR is it?
    Bug Fix
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/ZEPPELIN-1224
    
    ### How should this be tested?
    Try to get the auto complete suggestion.(`cmd + .`)
    We should not get `NoSuchMethodError` and suggestions should be listed rightly
    
    ### Screenshots (if appropriate)
    NA
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Karup <karuppayya@outlook.com>
    
    Closes #1214 from karup1990/ZEPPELIN-1224 and squashes the following commits:
    
    5d68984 [Karup] Fix variable names
    cd3df44 [Karup] Fix typo in method name
    
    (cherry picked from commit 73d99b880634809d615a063911573cb68b5d4864)
    Signed-off-by: Lee moon soo <moon@apache.org>

--- spark/src/main/java/org/apache/zeppelin/spark/SparkInterpreter.java
+++ spark/src/main/java/org/apache/zeppelin/spark/SparkInterpreter.java
@@ -108,9 +108,9 @@ public class SparkInterpreter extends Interpreter {
   private SparkDependencyResolver dep;
 
   /**
-   * completor - org.apache.spark.repl.SparkJLineCompletion (scala 2.10)
+   * completer - org.apache.spark.repl.SparkJLineCompletion (scala 2.10)
    */
-  private Object completor;
+  private Object completer;
 
   private Map<String, Object> binder;
   private SparkVersion sparkVersion;
@@ -702,7 +702,7 @@ public class SparkInterpreter extends Interpreter {
           }
         }
 
-        completor = Utils.instantiateClass(
+        completer = Utils.instantiateClass(
             "org.apache.spark.repl.SparkJLineCompletion",
             new Class[]{Utils.findClass("org.apache.spark.repl.SparkIMain")},
             new Object[]{intp});
@@ -893,7 +893,7 @@ public class SparkInterpreter extends Interpreter {
       cursor = completionText.length();
     }
     if (Utils.isScala2_10()) {
-      ScalaCompleter c = (ScalaCompleter) Utils.invokeMethod(completor, "completor");
+      ScalaCompleter c = (ScalaCompleter) Utils.invokeMethod(completer, "completer");
       Candidates ret = c.complete(completionText, cursor);
 
       List<String> candidates = WrapAsJava$.MODULE$.seqAsJavaList(ret.candidates());
