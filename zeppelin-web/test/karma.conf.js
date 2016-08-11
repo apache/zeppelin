47b193151bb762998cc2ae5e4953505f07cb76a2
Matthew Penny <mpenny@apption.com>
2016-08-11 15:27:17 -0400

    [ZEPPELIN-1220] Add geographical map as visualization option
    ### What is this PR for?
    Added a 2D [Esri map](https://developers.arcgis.com/javascript/) as a graph option for paragraphs. This makes it easier to visualize relations in geographical data.
    Pins are plotted at points in specified longitude/latitude columns and customizable column values are shown when pins are clicked. Is this of interest?
    
    The angular-esri-map module is used, which is released under the Apache 2.0 license, and the JavaScript API is free to use (see [https://developers.arcgis.com/terms/](https://developers.arcgis.com/terms/)).
    
    ### What type of PR is it?
    Feature
    
    ### What is the Jira issue?
    [ZEPPELIN-1220](https://issues.apache.org/jira/browse/ZEPPELIN-1220)
    
    ### How should this be tested?
    Execute a query in a paragraph and select the map pin from the chart type selector.
    Set the longitude/latitude (and optionally, pin info) columns, then interact with the map.
    
    ### Screenshot
    ![screenshot](https://cloud.githubusercontent.com/assets/9356790/16997359/1262d786-4e83-11e6-8230-a906fe1da2e9.png)
    
    ### Questions:
    * Does the licenses files need update? __yes.__ For angular-esri-map.
    * Is there breaking changes for older versions? __no__
    * Does this needs documentation? __potentially.__ This feature follows the same layout as the other visualization modes, so users will likely be able to use it easily. However, screenshots of some supported visualizations modes are present in the current documentation. If this PR is merged, it may be desirable to display the map option as well. Interestingly the scatter chart is not included in screenshots in the documentation.
    
    Author: Matthew Penny <mpenny@apption.com>
    
    Closes #1210 from mwpenny/esri and squashes the following commits:
    
    f21ae26 [Matthew Penny] Re-use variable (as intended)
    c30b1b7 [Matthew Penny] Decrease top margin for bottom-docked map popups
    9f72f6f [Matthew Penny] Display error instead of map when there is no internet connection
    5a056c8 [Matthew Penny] Lazy load map CSS
    14f8a1f [Matthew Penny] Fit map popup to map
    a47a86c [Matthew Penny] Add padding to bottom-docked map popups
    3c12445 [Matthew Penny] Refactoring
    07d6ddb [Matthew Penny] Fix scatter size field height in pivot (accidentally changed)
    c976515 [Matthew Penny] Remove text from merge conflict
    fc59b7d [Matthew Penny] Use proper coordinates for map popup
    ec7f155 [Matthew Penny] Update map height when paragraph is resized
    fbdd41c [Matthew Penny] Make map pivot fields the same height and hide basemap selection when using other graph modes
    00054e9 [Matthew Penny] Disable mouse wheel zooming, and start map zoomed out
    75e3d49 [Matthew Penny] Update index.html and karma.conf.js to include angular-esri-map JS
    efdb889 [Matthew Penny] Add angular-esri-map to license document
    effcff3 [Matthew Penny] Add basemap selection, and some minor cleanup/renaming
    2360947 [Matthew Penny] Add tests
    984e2b1 [Matthew Penny] Only update pins after map is loaded, and try to detect latitude and logitude columns in data
    26bda50 [Matthew Penny] Fix CSS hiding pins
    78285ab [Matthew Penny] Add pin rendering to map based on user pivot selection for data
    45858e1 [Matthew Penny] Add Esri ArcGIS map rendering support to paragraphs
    cf6578e [Matthew Penny] Add map UI button and pivot fields

--- zeppelin-web/test/karma.conf.js
+++ zeppelin-web/test/karma.conf.js
@@ -66,6 +66,7 @@ module.exports = function(config) {
       'bower_components/moment-duration-format/lib/moment-duration-format.js',
       'bower_components/select2/dist/js/select2.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-esri-map/dist/angular-esri-map.js'
       // endbower
       'src/app/app.js',
       'src/app/app.controller.js',
