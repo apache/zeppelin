43dc32bd396df49d682bb768e972ef6c11f950da
astroshim <hsshim@nflabs.com>
2016-08-16 14:39:51 +0900

    [Zeppelin-945] Interpreter authorization
    ### What is this PR for?
    This PR is derived from https://github.com/apache/zeppelin/pull/1223
    
    ### What type of PR is it?
    Improvement
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/ZEPPELIN-945
    
    ### How should this be tested?
    You can change user permission of interpreter at interpreter setting page.
    
    ### Screenshots (if appropriate)
    ![result](https://cloud.githubusercontent.com/assets/3348133/17457867/904e2754-5c3e-11e6-948c-b2969c4b89c7.gif)
    
    ### Questions:
    * Does the licenses files need update? no
    * Is there breaking changes for older versions? no
    * Does this needs documentation? no
    
    Author: astroshim <hsshim@nflabs.com>
    
    Closes #1257 from astroshim/ZEPPELIN-945_1 and squashes the following commits:
    
    83097ab [astroshim] fix naming.
    2c48ded [astroshim] code refactor.
    7ed8ad6 [astroshim] fix js code style
    3e25159 [astroshim] move directive to componets
    f07542a [astroshim] remove comment
    febce0c [astroshim] fix js
    e42cb9e [astroshim] fix initialize user value
    e1e7a35 [astroshim] add interpreter create.
    e72c097 [astroshim] Merge branch 'master' into ZEPPELIN-945_1
    e1679b3 [astroshim] wip
    e56e5b1 [astroshim] wip
    635d523 [astroshim] Merge branch 'master' of https://github.com/apache/zeppelin into ZEPPELIN-945_1
    1ae1c6a [astroshim] fix for @AhyoungRyu pointed.
    b34fdf7 [astroshim] fix for checking intpname
    29d8f43 [astroshim] fix js
    407d260 [astroshim] fit UX

--- zeppelin-web/src/app/interpreter/interpreter.controller.js
+++ zeppelin-web/src/app/interpreter/interpreter.controller.js
@@ -22,8 +22,69 @@ angular.module('zeppelinWebApp').controller('InterpreterCtrl',
     $scope.showRepositoryInfo = false;
     $scope._ = _;
 
+    $scope.openPermissions = function() {
+      $scope.showInterpreterAuth = true;
+    };
+
+    $scope.closePermissions = function() {
+      $scope.showInterpreterAuth = false;
+    };
+
+    var getSelectJson = function() {
+      var selectJson = {
+        tags: false,
+        multiple: true,
+        tokenSeparators: [',', ' '],
+        minimumInputLength: 2,
+        ajax: {
+          url: function(params) {
+            if (!params.term) {
+              return false;
+            }
+            return baseUrlSrv.getRestApiBase() + '/security/userlist/' + params.term;
+          },
+          delay: 250,
+          processResults: function(data, params) {
+            var users = [];
+            if (data.body.users.length !== 0) {
+              for (var i = 0; i < data.body.users.length; i++) {
+                users.push({
+                  'id': data.body.users[i],
+                  'text': data.body.users[i]
+                });
+              }
+            }
+            return {
+              results: users,
+              pagination: {
+                more: false
+              }
+            };
+          },
+          cache: false
+        }
+      };
+      return selectJson;
+    };
+
+    $scope.togglePermissions = function(intpName) {
+      angular.element('#' + intpName + 'Users').select2(getSelectJson());
+      if ($scope.showInterpreterAuth) {
+        $scope.closePermissions();
+      } else {
+        $scope.openPermissions();
+      }
+    };
+
+    $scope.$on('ngRenderFinished', function(event, data) {
+      for (var setting = 0; setting < $scope.interpreterSettings.length; setting++) {
+        angular.element('#' + $scope.interpreterSettings[setting].name + 'Users').select2(getSelectJson());
+      }
+    });
+
     var getInterpreterSettings = function() {
-      $http.get(baseUrlSrv.getRestApiBase() + '/interpreter/setting').success(function(data, status, headers, config) {
+      $http.get(baseUrlSrv.getRestApiBase() + '/interpreter/setting')
+      .success(function(data, status, headers, config) {
         $scope.interpreterSettings = data.body;
         checkDownloadingDependencies();
       }).error(function(data, status, headers, config) {
@@ -114,7 +175,6 @@ angular.module('zeppelinWebApp').controller('InterpreterCtrl',
         var setting = $scope.interpreterSettings[index];
         option = setting.option;
       }
-
       if (option.perNoteSession) {
         return 'scoped';
       } else if (option.perNoteProcess) {
@@ -148,10 +208,15 @@ angular.module('zeppelinWebApp').controller('InterpreterCtrl',
             if (setting.option.isExistingProcess === undefined) {
               setting.option.isExistingProcess = false;
             }
+            if (setting.option.setPermission === undefined) {
+              setting.option.setPermission = false;
+            }
             if (setting.option.remote === undefined) {
               // remote always true for now
               setting.option.remote = true;
             }
+            setting.option.users = angular.element('#' + setting.name + 'Users').val();
+
             var request = {
               option: angular.copy(setting.option),
               properties: angular.copy(setting.properties),
@@ -168,6 +233,7 @@ angular.module('zeppelinWebApp').controller('InterpreterCtrl',
                 removeTMPSettings(index);
                 thisConfirm.close();
                 checkDownloadingDependencies();
+                $route.reload();
               })
               .error(function(data, status, headers, config) {
                 console.log('Error %o %o', status, data.message);
@@ -275,6 +341,10 @@ angular.module('zeppelinWebApp').controller('InterpreterCtrl',
       if (newSetting.depArtifact !== '' || newSetting.depArtifact) {
         $scope.addNewInterpreterDependency();
       }
+      if (newSetting.option.setPermission === undefined) {
+        newSetting.option.setPermission = false;
+      }
+      newSetting.option.users = angular.element('#newInterpreterUsers').val();
 
       var request = angular.copy($scope.newInterpreterSetting);
 
@@ -311,6 +381,7 @@ angular.module('zeppelinWebApp').controller('InterpreterCtrl',
         option: {
           remote: true,
           isExistingProcess: false,
+          setPermission: false,
           perNoteSession: false,
           perNoteProcess: false
 
@@ -487,6 +558,7 @@ angular.module('zeppelinWebApp').controller('InterpreterCtrl',
     var init = function() {
       $scope.resetNewInterpreterSetting();
       $scope.resetNewRepositorySetting();
+
       getInterpreterSettings();
       getAvailableInterpreters();
       getRepositories();
