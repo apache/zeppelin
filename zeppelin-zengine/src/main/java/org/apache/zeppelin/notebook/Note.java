0a73241ea75453c4b018eb3488cb56a3f15afe15
Jongyoul Lee <jongyoul@gmail.com>
2016-08-26 13:07:24 +0900

    Fixed conflict while rebasing.

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/Note.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/Note.java
@@ -472,7 +472,7 @@ public class Note implements Serializable, ParagraphJobListener {
         AuthenticationInfo authenticationInfo = new AuthenticationInfo();
         authenticationInfo.setUser(cronExecutingUser);
         p.setAuthenticationInfo(authenticationInfo);
-        run(cronExecutingUser, p.getId());
+        run(p.getId());
       }
     }
   }
