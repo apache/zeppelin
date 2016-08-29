dd59b35adb12da8f291964b99b8f7046922741a0
Jeff Zhang <zjffdu@apache.org>
2016-08-29 11:04:46 +0800

    fix code style

--- zeppelin-zengine/src/test/java/org/apache/zeppelin/interpreter/InterpreterFactoryTest.java
+++ zeppelin-zengine/src/test/java/org/apache/zeppelin/interpreter/InterpreterFactoryTest.java
@@ -18,7 +18,12 @@
 package org.apache.zeppelin.interpreter;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.NullArgumentException;
