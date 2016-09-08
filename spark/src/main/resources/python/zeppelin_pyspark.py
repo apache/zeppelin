207d6dd0dbe79f66b34088ff96db1fa8835ddb61
Ishmeet Kaur <ishmeetkaur77@gmail.com>
2016-09-08 07:31:59 -0700

    Revert "show function modification pyspark"
    This reverts commit 561860a2321409c596397e2eb3366ff7c1820e51.

--- spark/src/main/resources/python/zeppelin_pyspark.py
+++ spark/src/main/resources/python/zeppelin_pyspark.py
@@ -115,29 +115,29 @@ class PyZeppelinContext(dict):
     #)
     body_buf.close(); header_buf.close()
     
-  def show_matplotlib(self, p, fmt="png", width="auto", height="auto", 
-                      **kwargs):
-    """Matplotlib show function
-    """
-    if fmt == "png":
-      img = BytesIO()
-      p.savefig(img, format=fmt)
-      img_str = b"data:image/png;base64,"
-      img_str += base64.b64encode(img.getvalue().strip())
-      img_tag = "<img src={img} style='width={width};height:{height}'>"
-      # Decoding is necessary for Python 3 compability
-      img_str = img_str.decode("ascii")
-      img_str = img_tag.format(img=img_str, width=width, height=height)
-    elif fmt == "svg":
-      img = StringIO()
-      p.savefig(img, format=fmt)
-      img_str = img.getvalue()
-    else:
-      raise ValueError("fmt must be 'png' or 'svg'")
-    
-    html = "%html <div style='width:{width};height:{height}'>{img}<div>"
-    print(html.format(width=width, height=height, img=img_str))
-    img.close()
+    def show_matplotlib(self, p, fmt="png", width="auto", height="auto", 
+                        **kwargs):
+      """Matplotlib show function
+      """
+      if fmt == "png":
+        img = BytesIO()
+        p.savefig(img, format=fmt)
+        img_str = b"data:image/png;base64,"
+        img_str += base64.b64encode(img.getvalue().strip())
+        img_tag = "<img src={img} style='width={width};height:{height}'>"
+        # Decoding is necessary for Python 3 compability
+        img_str = img_str.decode("ascii")
+        img_str = img_tag.format(img=img_str, width=width, height=height)
+      elif fmt == "svg":
+        img = StringIO()
+        p.savefig(img, format=fmt)
+        img_str = img.getvalue()
+      else:
+        raise ValueError("fmt must be 'png' or 'svg'")
+      
+      html = "%html <div style='width:{width};height:{height}'>{img}<div>"
+      print(html.format(width=width, height=height, img=img_str))
+      img.close()
 
   def add(self, key, value):
     self.__setitem__(key, value)
