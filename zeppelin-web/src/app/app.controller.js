6adb5225ab6e05ecb4a777c95292f98d0cbc131d
Damien CORNEAU <corneadoug@gmail.com>
2016-09-27 17:48:36 +0900

    Move use strict outside IFFE

--- zeppelin-web/src/app/app.controller.js
+++ zeppelin-web/src/app/app.controller.js
@@ -11,8 +11,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- (function() {
-  'use strict';
+'use strict';
+(function() {
 
   angular.module('zeppelinWebApp').controller('MainCtrl', MainCtrl);
 
