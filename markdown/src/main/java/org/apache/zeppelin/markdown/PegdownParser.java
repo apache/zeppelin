3247c675acf859b32ba00a3da19daaa327d424c1
1ambda <1amb4a@gmail.com>
2016-11-04 01:36:15 +0900

    feat: Support webseq markdown plugin

--- markdown/src/main/java/org/apache/zeppelin/markdown/PegdownParser.java
+++ markdown/src/main/java/org/apache/zeppelin/markdown/PegdownParser.java
@@ -19,17 +19,21 @@ package org.apache.zeppelin.markdown;
 
 import org.pegdown.Extensions;
 import org.pegdown.PegDownProcessor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.pegdown.plugins.PegDownPlugins;
 
 /** Markdown Parser using pegdown processor. */
 public class PegdownParser implements MarkdownParser {
   private PegDownProcessor processor;
 
+  public static final long PARSING_TIMEOUT_AS_MILLIS = 5000;
+  public static final int OPTIONS = Extensions.ALL_WITH_OPTIONALS - Extensions.ANCHORLINKS;
+
   public PegdownParser() {
-    int pegdownOptions = Extensions.ALL_WITH_OPTIONALS - Extensions.ANCHORLINKS;
-    int parsingTimeoutAsMillis = 5000;
-    processor = new PegDownProcessor(pegdownOptions, parsingTimeoutAsMillis);
+    PegDownPlugins plugins = new PegDownPlugins.Builder()
+      .withPlugin(PegdownYumlPlugin.class)
+      .withPlugin(PegdownWebSequencelPlugin.class)
+      .build();
+    processor = new PegDownProcessor(OPTIONS, PARSING_TIMEOUT_AS_MILLIS, plugins);
   }
 
   @Override
