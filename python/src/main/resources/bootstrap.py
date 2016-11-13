f7af21e2197cb4d106fdb66fde016120d4d5748d
Alex Goodman <agoodm@users.noreply.github.com>
2016-11-13 15:48:29 -0800

    [HOTFIX][ZEPPELIN-1656] z.show in Python interpreter does not work
    ### What is this PR for?
    There have been reports of #1534 causing the python interpreter to always show an error because `z` is not being set. As it turns out this is a result of improperly handling the case when matplotlib isn't found when initializing the interpreter.
    
    ### What type of PR is it?
    Bug Fix
    
    ### What is the Jira issue?
    [ZEPPELIN-1656](https://issues.apache.org/jira/browse/ZEPPELIN-1656)
    
    ### How should this be tested?
    Run any simple python paragraph.
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Alex Goodman <agoodm@users.noreply.github.com>
    
    Closes #1628 from agoodm/patch-1 and squashes the following commits:
    
    67f2ad5 [Alex Goodman] python interpeter should work when matplotlib is not installed
    0a7a9d7 [Alex Goodman] Fix indent in bootstrap.py

--- python/src/main/resources/bootstrap.py
+++ python/src/main/resources/bootstrap.py
@@ -207,7 +207,7 @@ class PyZeppelinContext(object):
         try:
             import matplotlib
         except ImportError:
-            pass
+            return
         # Make sure custom backends are available in the PYTHONPATH
         rootdir = os.environ.get('ZEPPELIN_HOME', os.getcwd())
         mpl_path = os.path.join(rootdir, 'interpreter', 'lib', 'python')
@@ -218,7 +218,7 @@ class PyZeppelinContext(object):
         try:
             matplotlib.use('module://backend_zinline')
             import backend_zinline
-      
+            
             # Everything looks good so make config assuming that we are using
             # an inline backend
             self._displayhook = backend_zinline.displayhook
