7958bea76398ad1bf89411ce98198bc058c90ec2
1ambda <1amb4a@gmail.com>
2016-11-28 12:34:48 +0900

    style: Fix indents in alluxio
    [INFO] ------------------------------------------------------------------------
    [INFO] Building Zeppelin: Alluxio interpreter 0.7.0-SNAPSHOT
    [INFO] ------------------------------------------------------------------------
    [INFO]
    [INFO] --- maven-checkstyle-plugin:2.17:check (checkstyle-fail-build) @ zeppelin-alluxio ---
    [INFO] There is 1 error reported by Checkstyle 6.19 with _tools/checkstyle.xml ruleset.
    [ERROR] src/main/java/org/apache/zeppelin/alluxio/AlluxioInterpreter.java:[74] (indentation) Indentation: '" on port "' have incorrect indentation level 6, expected level should be 8.
    [INFO] ------------------------------------------------------------------------
    [INFO] Reactor Summary:
    [INFO]
    [INFO] Zeppelin ........................................... SUCCESS [  3.176 s]
    [INFO] Zeppelin: Interpreter .............................. SUCCESS [  1.072 s]
    [INFO] Zeppelin: Zengine .................................. SUCCESS [  0.946 s]
    [INFO] Zeppelin: Display system apis ...................... SUCCESS [  0.168 s]
    [INFO] Zeppelin: Spark dependencies ....................... SUCCESS [  0.633 s]
    [INFO] Zeppelin: Spark .................................... SUCCESS [  0.435 s]
    [INFO] Zeppelin: Markdown interpreter ..................... SUCCESS [  0.168 s]
    [INFO] Zeppelin: Angular interpreter ...................... SUCCESS [  0.145 s]
    [INFO] Zeppelin: Shell interpreter ........................ SUCCESS [  0.137 s]
    [INFO] Zeppelin: Livy interpreter ......................... SUCCESS [  0.602 s]
    [INFO] Zeppelin: HBase interpreter ........................ SUCCESS [  0.338 s]
    [INFO] Zeppelin: Apache Pig Interpreter ................... SUCCESS [  0.275 s]
    [INFO] Zeppelin: PostgreSQL interpreter ................... SUCCESS [  0.177 s]
    [INFO] Zeppelin: JDBC interpreter ......................... SUCCESS [  0.221 s]
    [INFO] Zeppelin: File System Interpreters ................. SUCCESS [  0.174 s]
    [INFO] Zeppelin: Flink .................................... SUCCESS [  0.315 s]
    [INFO] Zeppelin: Apache Ignite interpreter ................ SUCCESS [  0.215 s]
    [INFO] Zeppelin: Kylin interpreter ........................ SUCCESS [  0.144 s]
    [INFO] Zeppelin: Python interpreter ....................... SUCCESS [  0.191 s]
    [INFO] Zeppelin: Lens interpreter ......................... SUCCESS [  0.285 s]
    [INFO] Zeppelin: Apache Cassandra interpreter ............. SUCCESS [  0.276 s]
    [INFO] Zeppelin: Elasticsearch interpreter ................ SUCCESS [  0.295 s]
    [INFO] Zeppelin: BigQuery interpreter ..................... SUCCESS [  0.172 s]
    [INFO] Zeppelin: Alluxio interpreter ...................... FAILURE [  0.212 s]

--- alluxio/src/main/java/org/apache/zeppelin/alluxio/AlluxioInterpreter.java
+++ alluxio/src/main/java/org/apache/zeppelin/alluxio/AlluxioInterpreter.java
@@ -71,7 +71,7 @@ public class AlluxioInterpreter extends Interpreter {
   @Override
   public void open() {
     logger.info("Starting Alluxio shell to connect to " + alluxioMasterHostname +
-      " on port " + alluxioMasterPort);
+        " on port " + alluxioMasterPort);
 
     System.setProperty(ALLUXIO_MASTER_HOSTNAME, alluxioMasterHostname);
     System.setProperty(ALLUXIO_MASTER_PORT, alluxioMasterPort);
