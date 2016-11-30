13d77e07f834ff375941841e5b2e8cc344702749
Peilin Yang <yangpeilyn@gmail.com>
2016-11-30 08:45:03 -0500

    [ZEPPELIN-1371]add text/numeric conversion support to table display
    ### What is this PR for?
    
    We people in Twitter have great demand of adding a flexible drop down menu for the columns in the tables which enables the text/numeric conversion.
    This is because people want some columns to be of string type which, for example, fits to the underlying DB definition.
    The use cases of the change include:
    1. For now sorting on the columns is always lexicographically because Zeppelin front end treats the data as strings. It the values in the table can be convertible between string and number then we can sort the column by numeric values. I have filed another ticket for this: https://issues.apache.org/jira/browse/ZEPPELIN-1372
    2. Since the users know more about their data than us it would be nice if we let the users decide what is the real type of their data. It is annoying if user wants the column to be strings but the front end forcefully inserts commas in it. In some scenarios, users may also want to copy/paste the table to somewhere else. If people want to remove the commas before other actions then that will be a nightmare....
    ### What type of PR is it?
    
    [Improvement | Feature]
    ### Todos
    - [ ] - Task
    ### What is the Jira issue?
    - https://issues.apache.org/jira/browse/ZEPPELIN-1371
    ### How should this be tested?
    1. Click on the dropdown menu would convert the text/number of that column.
    2. Other functionalities esp. the sorting function should not be affected.
    ### Screenshots (if appropriate)
    
    ![image](https://cloud.githubusercontent.com/assets/3334391/18445617/76071f8e-78ec-11e6-93a6-fdcf7b85b6e9.png)
    ![image](https://cloud.githubusercontent.com/assets/3334391/18446601/16cff4f0-78f1-11e6-9fd6-4010b5d77f17.png)
    ### Questions:
    - Does the licenses files need update? No
    - Is there breaking changes for older versions? Probably a noticeable change in UI
    - Does this needs documentation? No
    
    Author: Peilin Yang <yangpeilyn@gmail.com>
    
    Closes #1500 from Peilin-Yang/ypeilin/table_num_cell_format and squashes the following commits:
    
    e3425ee [Peilin Yang] make clean code
    ba60cb0 [Peilin Yang] update test case
    cdc06b6 [Peilin Yang] refactor based on the most recent master
    06b4ae7 [Peilin Yang] merge
    4e2a403 [Peilin Yang] bug fix
    61824d3 [Peilin Yang] update test case
    df2effb [Peilin Yang] update the test case
    39c5408 [Peilin Yang] update the test case in SparkParagraphIT
    030a1e6 [Peilin Yang] add dropdown menu applied on the latest branch

--- zeppelin-server/src/test/java/org/apache/zeppelin/integration/SparkParagraphIT.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/integration/SparkParagraphIT.java
@@ -185,8 +185,7 @@ public class SparkParagraphIT extends AbstractZeppelinIT {
       WebElement paragraph1Result = driver.findElement(By.xpath(
           getParagraphXPath(1) + "//div[contains(@id,\"_graph\")]/div/div/div/div/div[1]"));
       collector.checkThat("Paragraph from SparkParagraphIT of testSqlSpark result: ",
-          paragraph1Result.getText().toString(), CoreMatchers.equalTo("age\njob\nmarital\neducation\nbalance\n" +
-          "30 unemployed married primary 1,787"));
+          paragraph1Result.getText().toString(), CoreMatchers.equalTo("age\n▼\njob\n▼\nmarital\n▼\neducation\n▼\nbalance\n▼\n30 unemployed married primary 1787"));
     } catch (Exception e) {
       handleException("Exception in SparkParagraphIT while testSqlSpark", e);
     }
--- zeppelin-web/src/app/tabledata/tabledata.js
+++ zeppelin-web/src/app/tabledata/tabledata.js
@@ -59,9 +59,8 @@ zeppelin.TableData.prototype.loadParagraphResult = function(paragraphResult) {
       if (i === 0) {
         columnNames.push({name: col, index: j, aggr: 'sum'});
       } else {
-        var parsedCol = this.parseTableCell(col);
-        cols.push(parsedCol);
-        cols2.push({key: (columnNames[i]) ? columnNames[i].name : undefined, value: parsedCol});
+        cols.push(col);
+        cols2.push({key: (columnNames[i]) ? columnNames[i].name : undefined, value: col});
       }
     }
     if (i !== 0) {
@@ -72,19 +71,4 @@ zeppelin.TableData.prototype.loadParagraphResult = function(paragraphResult) {
   this.comment = comment;
   this.columns = columnNames;
   this.rows = rows;
-};
-
-zeppelin.TableData.prototype.parseTableCell = function(cell) {
-  if (!isNaN(cell)) {
-    if (cell.length === 0 || Number(cell) > Number.MAX_SAFE_INTEGER || Number(cell) < Number.MIN_SAFE_INTEGER) {
-      return cell;
-    } else {
-      return Number(cell);
-    }
-  }
-  var d = moment(cell);
-  if (d.isValid()) {
-    return d;
-  }
-  return cell;
 };
