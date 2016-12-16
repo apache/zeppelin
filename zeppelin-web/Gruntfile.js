5552319378e8361a16c91df2ba3cdf5519b74d58
Myoungdo Park <cuspymd@gmail.com>
2016-12-16 22:09:26 +0900

    [ZEPPELIN-1239] Google WebFont: goog-webfont-dl Grunt task warning and build error
    ### What is this PR for?
    It is to fix a zeppelin-web build error in Windows 10.
    To fix it, I changed google webfont download module of grunt.
    
    ### What type of PR is it?
    [Bug Fix]
    
    ### Todos
    
    ### What is the Jira issue?
    [ZEPPELIN-1239](https://issues.apache.org/jira/browse/ZEPPELIN-1239)
    
    ### How should this be tested?
    zeppelin-web build in Windows 10
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    'spawn UNKNOWN' error occurs on zeppelin-web build processing in windows 10.
    It due to the function 'grunt.util.spawn()' which is called by the module 'grunt-goog-webfont-dl'.
    So I changed this module to 'grunt-google-fonts'.
    
    Author: Myoungdo Park <cuspymd@gmail.com>
    
    Closes #1731 from cuspymd/google-fonts and squashes the following commits:
    
    e1093fc [Myoungdo Park] Revert dangle-commas deletion
    7fb336b [Myoungdo Park] Move 'grunt googlefonts' into 'postinstall'
    1fea68f [Myoungdo Park] Change google font downloader to fix a build error in win 7

--- zeppelin-web/Gruntfile.js
+++ zeppelin-web/Gruntfile.js
@@ -103,50 +103,32 @@ module.exports = function(grunt) {
       }
     },
 
-    'goog-webfont-dl': {
-      patuaOne: {
+    googlefonts: {
+      build: {
         options: {
-          ttf: true,
-          eot: true,
-          woff: true,
-          woff2: true,
-          svg: true,
-          fontname: 'Patua One',
-          fontstyles: '400',
-          fontdest: '<%= yeoman.app %>/fonts/',
-          cssdest: '<%= yeoman.app %>/fonts/Patua-One.css',
-          cssprefix: '',
-          subset: ''
-        }
-      },
-      sourceCodePro: {
-        options: {
-          ttf: true,
-          eot: true,
-          woff: true,
-          woff2: true,
-          svg: true,
-          fontname: 'Source Code Pro',
-          fontstyles: '300, 400, 500',
-          fontdest: '<%= yeoman.app %>/fonts/',
-          cssdest: '<%= yeoman.app %>/fonts/Source-Code-Pro.css',
-          cssprefix: '',
-          subset: ''
-        }
-      },
-      roboto: {
-        options: {
-          ttf: true,
-          eot: true,
-          woff: true,
-          woff2: true,
-          svg: true,
-          fontname: 'Roboto',
-          fontstyles: '300, 400, 500',
-          fontdest: '<%= yeoman.app %>/fonts/',
-          cssdest: '<%= yeoman.app %>/fonts/Roboto.css',
-          cssprefix: '',
-          subset: ''
+          fontPath: '<%= yeoman.app %>/fonts/',
+          httpPath: '../fonts/',
+          cssFile: '<%= yeoman.app %>/fonts/google-fonts.css',
+          formats: {
+            eot: true,
+            ttf: true,
+            woff: true,
+            svg: true
+          },
+          fonts: [
+            {
+              family: 'Patua One',
+              styles: [400]
+            },
+            {
+              family: 'Source Code Pro',
+              styles: [300, 400, 500]
+            },
+            {
+              family: 'Roboto',
+              styles: [300, 400, 500]
+            }
+          ]
         }
       }
     },
@@ -624,7 +606,6 @@ module.exports = function(grunt) {
     'htmlhint',
     'clean:dist',
     'wiredep',
-    'goog-webfont-dl',
     'useminPrepare',
     'concurrent:dist',
     'postcss',
