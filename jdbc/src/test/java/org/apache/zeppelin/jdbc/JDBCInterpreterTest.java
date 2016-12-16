aa5d5e1016ff82e15ce6b867e221e7d0cd7fcc24
Peter McCaffrey <pmccaffrey@tests-MacBook-Pro-2.local>
2016-12-16 01:56:03 -0500

    test table content

--- jdbc/src/test/java/org/apache/zeppelin/jdbc/JDBCInterpreterTest.java
+++ jdbc/src/test/java/org/apache/zeppelin/jdbc/JDBCInterpreterTest.java
@@ -223,6 +223,7 @@ public class JDBCInterpreterTest extends BasicJDBCTestCaseAdapter {
 
     assertEquals(InterpreterResult.Code.SUCCESS, interpreterResult.code());
     assertEquals(InterpreterResult.Type.TABLE, interpreterResult.message().get(0).getType());
+    System.out.println(interpreterResult.message().get(0).getData());
     assertEquals("TABLE_CATALOG\tTABLE_SCHEMA\tTABLE_NAME\tCOLUMN_NAME\tDATA_TYPE\tTYPE_NAME\t" +
       "CHARACTER_MAXIMUM_LENGTH\tCHARACTER_MAXIMUM_LENGTH\tNUMERIC_SCALE\tNUMERIC_PRECISION_RADIX" +
       "\tNULLABLE\tREMARKS\tCOLUMN_DEFAULT\tDATA_TYPE\tSQL_DATETIME_SUB\tCHARACTER_OCTET_LENGTH\t" +
@@ -230,7 +231,7 @@ public class JDBCInterpreterTest extends BasicJDBCTestCaseAdapter {
       "IS_AUTOINCREMENT\tSCOPE_CATLOG\nH2-TEST-8001271961293557682\tPUBLIC\tTEST_TABLE\tID\t12\t" +
       "VARCHAR\t255\t255\t0\t10\t1\t\tnull\t12\t0\t255\t1\tYES\tnull\tnull\tnull\tnull\tNO\tnull\n" +
       "H2-TEST-8001271961293557682\tPUBLIC\tTEST_TABLE\tNAME\t12\tVARCHAR\t255\t255\t0\t10\t1\t\t" +
-      "null\t12\t0\t255\t2\tYES\tnull\tnull\tnull\tnull\tNO\tnull"
+      "null\t12\t0\t255\t2\tYES\tnull\tnull\tnull\tnull\tNO\tnull\n"
       , interpreterResult.message().get(0).getData());
   }
 
