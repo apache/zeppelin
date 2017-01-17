a163044d61de3db8c5c20e2007a417afb7964b97
1ambda <1amb4a@gmail.com>
2017-01-17 19:41:59 +0900

    feat: Add flowchart, translator examples

---
+++ zeppelin-examples/zeppelin-example-frontend-interpreter-translator/index.js
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+    AbstractFrontendInterpreter,
+    FrontendInterpreterResult,
+    DefaultDisplayType,
+} from 'zeppelin-frontend-interpreter';
+
+import 'whatwg-fetch';
+
+export default class TranslatorInterpreter extends AbstractFrontendInterpreter {
+    constructor() {
+        super("%translator", DefaultDisplayType.TEXT);
+    }
+
+    interpret(paragraphText) {
+        return [new FrontendInterpreterResult(
+            DefaultDisplayType.TEXT,
+            this.translate(paragraphText)
+        )];
+    }
+
+    translate(text) {
+        return fetch('https://translation.googleapis.com/language/translate/v2', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer YOUR_ACCESS_KEY_HERE',
+            },
+            body: JSON.stringify({
+                'q': text,
+                'source': 'en',
+                'target': 'ko',
+                'format': 'text'
+            })
+        }).then((response) => {
+            if (response.status === 200) {
+                return response.json()
+            }
+            throw new Error(`https://translation.googleapis.com/language/translate/v2 ${response.status} (${response.statusText})`);
+        }).then((json) => {
+          const extracted = json.data.translations.map(t => {
+            return t.translatedText;
+          });
+          return extracted.join('\n');
+        });
+    }
+}
+
