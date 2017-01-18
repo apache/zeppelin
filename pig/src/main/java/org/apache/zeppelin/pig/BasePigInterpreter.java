88a905caf2043d12a85462940c451b424d218dba
Jeff Zhang <zjffdu@apache.org>
2017-01-18 15:10:07 +0800

    ZEPPELIN-1977. spark 2.1 uses a more recent commons-lang3
    ### What is this PR for?
    The issue is that spark 2.1 use `commons-lang3` 3.5 while `zeppelin-interpreter` use 3.4. We can not just upgrade `commons-lang3` to 3.5, as it just make spark 2.1 work, but would fail other versions of spark. This PR remove `commons-lang3` from zeppelin-interpreter. We should keep zeppelin-interpreter's dependencies as minimum as possible. We can remove other dependencies (like `commons-lang`) from `zeppelin-interpreter` in a followup PR.
    
    ### What type of PR is it?
    [Bug Fix]
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/ZEPPELIN-1977
    
    ### How should this be tested?
    Tested manually
    ![2017-01-18_1448](https://cloud.githubusercontent.com/assets/164491/22054522/f125e836-dd90-11e6-9acf-d73541046d95.png)
     on 3 versions of spark (2.1, 2.0.2, 1.6.2)
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Jeff Zhang <zjffdu@apache.org>
    
    Closes #1910 from zjffdu/ZEPPELIN-1977 and squashes the following commits:
    
    8bac97f [Jeff Zhang] ZEPPELIN-1977. spark 2.1 uses a more recent commons-lang3

--- pig/src/main/java/org/apache/zeppelin/pig/BasePigInterpreter.java
+++ pig/src/main/java/org/apache/zeppelin/pig/BasePigInterpreter.java
@@ -17,7 +17,7 @@
 
 package org.apache.zeppelin.pig;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.pig.PigServer;
 import org.apache.pig.backend.BackendException;
