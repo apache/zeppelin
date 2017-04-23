cbae84097559d111964a838565521910add449e1
Jongyoul Lee <jongyoul@gmail.com>
2017-04-24 06:47:31 +0900

    Added license header for avoiding rat failed

--- zeppelin-cluster/yarn/src/main/java/org/apache/zeppelin/cluster/yarn/Client.java
+++ zeppelin-cluster/yarn/src/main/java/org/apache/zeppelin/cluster/yarn/Client.java
@@ -17,29 +17,5 @@
 
 package org.apache.zeppelin.cluster.yarn;
 
-/**
- *
- */
-public class DSConstants {
-  /**
-   * Environment key name pointing to the shell script's location
-   */
-  public static final String DISTRIBUTEDSHELLSCRIPTLOCATION = "DISTRIBUTEDSHELLSCRIPTLOCATION";
-
-  /**
-   * Environment key name denoting the file timestamp for the shell script.
-   * Used to validate the local resource.
-   */
-  public static final String DISTRIBUTEDSHELLSCRIPTTIMESTAMP = "DISTRIBUTEDSHELLSCRIPTTIMESTAMP";
-
-  /**
-   * Environment key name denoting the file content length for the shell script.
-   * Used to validate the local resource.
-   */
-  public static final String DISTRIBUTEDSHELLSCRIPTLEN = "DISTRIBUTEDSHELLSCRIPTLEN";
-
-  /**
-   * Environment key name denoting the timeline domain ID.
-   */
-  public static final String DISTRIBUTEDSHELLTIMELINEDOMAIN = "DISTRIBUTEDSHELLTIMELINEDOMAIN";
+public class Client {
 }
