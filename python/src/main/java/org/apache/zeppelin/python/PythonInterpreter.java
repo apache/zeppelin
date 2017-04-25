507c6e88ea2e0d5410cf02c20a177b8b0e93b2f6
Paul-Armand Verhaegen <paularmand@paularmandsmbp.rto.be>
2017-04-25 10:10:19 +0200

    fix python interpreter as to use max result setting

--- python/src/main/java/org/apache/zeppelin/python/PythonInterpreter.java
+++ python/src/main/java/org/apache/zeppelin/python/PythonInterpreter.java
@@ -96,6 +96,7 @@ public class PythonInterpreter extends Interpreter implements ExecuteResultHandl
 
   public PythonInterpreter(Properties property) {
     super(property);
+    this.maxResult = Integer.parseInt(property.getProperty(MAX_RESULT));
     try {
       File scriptFile = File.createTempFile("zeppelin_python-", ".py", new File("/tmp"));
       scriptPath = scriptFile.getAbsolutePath();
