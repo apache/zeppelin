14cfbdff6c0df14bedffe009fbd70def49e5c237
soralee <sora0728@zepl.com>
2017-05-08 17:16:15 +0900

    fix area chart

--- zeppelin-web/src/app/visualization/builtins/visualization-areachart.js
+++ zeppelin-web/src/app/visualization/builtins/visualization-areachart.js
@@ -46,6 +46,7 @@ export default class AreachartVisualization extends Nvd3ChartVisualization {
 
     this.xLabels = d3Data.xLabels
     super.render(d3Data)
+    this.config.changeXLabel(this.config.xLabelStatus)
   }
 
   /**
@@ -58,10 +59,43 @@ export default class AreachartVisualization extends Nvd3ChartVisualization {
 
   configureChart (chart) {
     let self = this
+    let configObj = self.config
+
     chart.xAxis.tickFormat(function (d) { return self.xAxisTickFormat(d, self.xLabels) })
     chart.yAxis.tickFormat(function (d) { return self.yAxisTickFormat(d) })
     chart.yAxis.axisLabelDistance(50)
     chart.useInteractiveGuideline(true) // for better UX and performance issue. (https://github.com/novus/nvd3/issues/691)
+
+    self.config.changeXLabel = function(type) {
+      switch (type) {
+        case 'default':
+          self.chart._options['showXAxis'] = true
+          self.chart._options['margin'] = {bottom: 50, left: 60}
+          self.chart.xAxis.rotateLabels(0)
+          configObj.xLabelStatus = 'default'
+          break
+        case 'rotate':
+          self.chart._options['showXAxis'] = true
+          self.chart._options['margin'] = {bottom: 140, left: 100}
+          self.chart.xAxis.rotateLabels(-45)
+          configObj.xLabelStatus = 'rotate'
+          break
+        case 'hide':
+          self.chart._options['showXAxis'] = false
+          self.chart._options['margin'] = {bottom: 50, left: 60}
+          d3.select('#' + self.targetEl[0].id + '> svg').select('g.nv-axis.nv-x').selectAll('*').remove()
+          configObj.xLabelStatus = 'hide'
+          break
+      }
+    }
+
+    self.config.isXLabelStatus = function(type) {
+      if (configObj.xLabelStatus === type) {
+        return true
+      } else {
+        return false
+      }
+    }
 
     this.chart.style(this.config.style || 'stack')
 
@@ -73,5 +107,50 @@ export default class AreachartVisualization extends Nvd3ChartVisualization {
         self.emitConfig(self.config)
       }, 500)
     })
+  }
+  getSetting(chart) {
+    let self = this
+    let configObj = self.config
+
+    // default to visualize xLabel
+    if (typeof (configObj.xLabelStatus) === 'undefined') {
+      configObj.changeXLabel('default')
+    }
+
+    return {
+      template: `<div>
+          xAxis :
+      </div>
+
+      <div class='btn-group'>
+        <button type="button"
+              class="xLabel btn btn-default btn-sm"
+              ng-class="{'active' : this.config.isXLabelStatus('default')}"
+              ng-click="save('default')" >
+            Default
+        </button>
+
+        <button type="button"
+              class="btn btn-default btn-sm"
+              ng-class="{'active' : this.config.isXLabelStatus('rotate')}"
+              ng-click="save('rotate')" >
+            Rotate
+        </button>
+
+        <button type="button"
+              class="btn btn-default btn-sm"
+              ng-class="{'active' : this.config.isXLabelStatus('hide')}"
+              ng-click="save('hide')" >
+            Hide
+        </button>
+      </div>`,
+      scope: {
+        config: configObj,
+        save: function(type) {
+          configObj.changeXLabel(type)
+          self.emitConfig(configObj)
+        }
+      }
+    }
   }
 }
