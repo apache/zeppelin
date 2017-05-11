1a3ae426579a58267ad06d4cb33afb29ac822725
1ambda <1amb4a@gmail.com>
2017-05-12 05:39:00 +0900

    [ZEPPELIN-2122] Add execution time for Spell
    ### What is this PR for?
    
    Add execution time for spell results. See the screenshot section.
    
    ### What type of PR is it?
    [Improvement]
    
    ### Todos
    
    NONE
    
    ### What is the Jira issue?
    
    [ZEPPELIN-2122](https://issues.apache.org/jira/browse/ZEPPELIN-2122)
    
    ### How should this be tested?
    
    1. Install any spell (e.g the eco spell)
    2. Execute and check that the paragraph has valid execution time.
    
    ### Screenshots (if appropriate)
    
    ![spell_exec2](https://cloud.githubusercontent.com/assets/4968473/25970951/803ab842-36d5-11e7-8a18-d671466e2aba.gif)
    
    ![2122_spell_execution_time](https://cloud.githubusercontent.com/assets/4968473/25967990/e9edc96a-36ca-11e7-942e-718e37063c1b.gif)
    
    ### Questions:
    * Does the licenses files need update? - NO
    * Is there breaking changes for older versions? - NO
    * Does this needs documentation? - NO
    
    Author: 1ambda <1amb4a@gmail.com>
    
    Closes #2338 from 1ambda/ZEPPEILN-2122/add-execution-time-for-spell and squashes the following commits:
    
    2417428 [1ambda] fix: Set dateStarted in run* func
    0996865 [1ambda] fix: Use string instead of moment obj
    bb895a5 [1ambda] fix: Set dateStarted
    9fb388e [1ambda] fix: Use ISO 8601
    55f8d23 [1ambda] fix: rollback Job class
    c2ab88b [1ambda] feat: Persist, display execution time for spell

--- zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
+++ zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
@@ -254,12 +254,15 @@ function ParagraphCtrl ($scope, $rootScope, $route, $window, $routeParams, $loca
   $scope.propagateSpellResult = function (paragraphId, paragraphTitle,
                                          paragraphText, paragraphResults,
                                          paragraphStatus, paragraphErrorMessage,
-                                         paragraphConfig, paragraphSettingsParam) {
+                                         paragraphConfig, paragraphSettingsParam,
+                                         paragraphDateStarted, paragraphDateFinished) {
     websocketMsgSrv.paragraphExecutedBySpell(
       paragraphId, paragraphTitle,
       paragraphText, paragraphResults,
       paragraphStatus, paragraphErrorMessage,
-      paragraphConfig, paragraphSettingsParam)
+      paragraphConfig, paragraphSettingsParam,
+      paragraphDateStarted, paragraphDateFinished
+    )
   }
 
   $scope.handleSpellError = function (paragraphText, error,
@@ -270,10 +273,15 @@ function ParagraphCtrl ($scope, $rootScope, $route, $window, $routeParams, $loca
     console.error('Failed to execute interpret() in spell\n', error)
 
     if (!propagated) {
+      $scope.paragraph.dateFinished = $scope.getFormattedParagraphTime()
+    }
+
+    if (!propagated) {
       $scope.propagateSpellResult(
         $scope.paragraph.id, $scope.paragraph.title,
         paragraphText, [], $scope.paragraph.status, errorMessage,
-        $scope.paragraph.config, $scope.paragraph.settings.params)
+        $scope.paragraph.config, $scope.paragraph.settings.params,
+        $scope.paragraph.dateStarted, $scope.paragraph.dateFinished)
     }
   }
 
@@ -309,11 +317,16 @@ function ParagraphCtrl ($scope, $rootScope, $route, $window, $routeParams, $loca
     const paragraphText = $scope.spellTransaction.paragraphText
 
     if (!propagated) {
+      $scope.paragraph.dateFinished = $scope.getFormattedParagraphTime()
+    }
+
+    if (!propagated) {
       const propagable = SpellResult.createPropagable(resultsMsg)
       $scope.propagateSpellResult(
         $scope.paragraph.id, $scope.paragraph.title,
         paragraphText, propagable, status, '',
-        $scope.paragraph.config, $scope.paragraph.settings.params)
+        $scope.paragraph.config, $scope.paragraph.settings.params,
+        $scope.paragraph.dateStarted, $scope.paragraph.dateFinished)
     }
   }
 
@@ -330,6 +343,10 @@ function ParagraphCtrl ($scope, $rootScope, $route, $window, $routeParams, $loca
       const splited = paragraphText.split(magic)
       // remove leading spaces
       const textWithoutMagic = splited[1].replace(/^\s+/g, '')
+
+      if (!propagated) {
+        $scope.paragraph.dateStarted = $scope.getFormattedParagraphTime()
+      }
 
       // handle actual result message in promise
       heliumService.executeSpell(magic, textWithoutMagic)
@@ -979,6 +996,10 @@ function ParagraphCtrl ($scope, $rootScope, $route, $window, $routeParams, $loca
 
   $scope.getProgress = function () {
     return $scope.currentProgress || 0
+  }
+
+  $scope.getFormattedParagraphTime = () => {
+    return moment().toISOString()
   }
 
   $scope.getExecutionTime = function (pdata) {
