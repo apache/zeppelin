1b3129c015e9b3950bfdb6abf98de795c4379224
Lee moon soo <moon@apache.org>
2017-06-01 13:19:31 -0700

    polyfill .endswith

--- zeppelin-web/src/app/app.js
+++ zeppelin-web/src/app/app.js
@@ -196,3 +196,18 @@ function bootstrapApplication () {
 angular.element(document).ready(function () {
   auth().then(bootstrapApplication)
 })
+
+// See ZEPPELIN-2577. No graphs visible on IE 11
+// Polyfill. https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+    var subjectString = this.toString();
+    if (typeof position !== 'number' || !isFinite(position) ||
+        Math.floor(position) !== position || position > subjectString.length) {
+      position = subjectString.length;
+    }
+    position -= searchString.length;
+    var lastIndex = subjectString.indexOf(searchString, position);
+    return lastIndex !== -1 && lastIndex === position;
+  };
+}
