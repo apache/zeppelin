40c63707d1fd2552130a713f0500b9a8495d33e5
Lee moon soo <moon@apache.org>
2017-06-01 14:00:04 -0700

    update style

--- zeppelin-web/src/app/app.js
+++ zeppelin-web/src/app/app.js
@@ -200,14 +200,15 @@ angular.element(document).ready(function () {
 // See ZEPPELIN-2577. No graphs visible on IE 11
 // Polyfill. https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
 if (!String.prototype.endsWith) {
+  // eslint-disable-next-line no-extend-native
   String.prototype.endsWith = function(searchString, position) {
-    var subjectString = this.toString();
+    let subjectString = this.toString()
     if (typeof position !== 'number' || !isFinite(position) ||
         Math.floor(position) !== position || position > subjectString.length) {
-      position = subjectString.length;
+      position = subjectString.length
     }
-    position -= searchString.length;
-    var lastIndex = subjectString.indexOf(searchString, position);
-    return lastIndex !== -1 && lastIndex === position;
-  };
+    position -= searchString.length
+    let lastIndex = subjectString.indexOf(searchString, position)
+    return lastIndex !== -1 && lastIndex === position
+  }
 }
