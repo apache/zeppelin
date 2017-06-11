c90a3c2e51f1f1853c179b42ce4b241c518d31e3
Nelson Costa <nelson.costa85@gmail.com>
2017-06-11 20:52:47 +0100

    [ZEPPELIN-2622] Add Zeppelin Git info REST API
    ### What is this PR for?
    Added a new REST API that provides more detailed info. on the exact source code Git commit Id that created the build. Useful to track down bugs and replicate issues.
    
    ### What type of PR is it?
    [Improvement]
    
    ### Todos
    * [ ] - Add more Git properties
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/ZEPPELIN-2622
    
    ### How should this be tested?
    Build Zeppelin. Open http://localhost:8080/api/git
    
    ### Screenshots (if appropriate)
    Expected output: {"status":"OK","message":"Zeppelin Git info","body":{"commitId":"4c2d295","timestamp":"2017-06-06 19:00:01"}}
    
    ### Questions:
    * Does the licenses files need update? N
    * Is there breaking changes for older versions? N
    * Does this needs documentation? Y (Done)
    
    Author: Nelson Costa <nelson.costa85@gmail.com>
    
    Closes #2398 from necosta/zeppelin2622 and squashes the following commits:
    
    dc76a24 [Nelson Costa] [ZEPPELIN-2622] Keep 'About Zeppelin' window only displaying version
    426b1ea [Nelson Costa] [ZEPPELIN-2622] Minor adjustments
    2d20455 [Nelson Costa] [ZEPPELIN-2622] Merged version and git REST APIs
    9ff7d60 [Nelson Costa] [ZEPPELIN-2622] Added missing license
    75795f1 [Nelson Costa] [ZEPPELIN-2622] Add Zeppelin Git info REST API

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/util/Util.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/util/Util.java
@@ -27,13 +27,18 @@ import java.util.Properties;
  */
 public class Util {
   private static final String PROJECT_PROPERTIES_VERSION_KEY = "version";
+  private static final String GIT_PROPERTIES_COMMIT_ID_KEY = "git.commit.id.abbrev";
+  private static final String GIT_PROPERTIES_COMMIT_TS_KEY = "git.commit.time";
 
   private static Properties projectProperties;
+  private static Properties gitProperties;
 
   static {
     projectProperties = new Properties();
+    gitProperties = new Properties();
     try {
       projectProperties.load(Util.class.getResourceAsStream("/project.properties"));
+      gitProperties.load(Util.class.getResourceAsStream("/git.properties"));
     } catch (IOException e) {
       //Fail to read project.properties
     }
@@ -46,6 +51,26 @@ public class Util {
    */
   public static String getVersion() {
     return StringUtils.defaultIfEmpty(projectProperties.getProperty(PROJECT_PROPERTIES_VERSION_KEY),
+            StringUtils.EMPTY);
+  }
+
+  /**
+   * Get Zeppelin Git latest commit id
+   *
+   * @return Latest Zeppelin commit id
+   */
+  public static String getGitCommitId() {
+    return StringUtils.defaultIfEmpty(gitProperties.getProperty(GIT_PROPERTIES_COMMIT_ID_KEY),
+            StringUtils.EMPTY);
+  }
+
+  /**
+   * Get Zeppelin Git latest commit timestamp
+   *
+   * @return Latest Zeppelin commit timestamp
+   */
+  public static String getGitTimestamp() {
+    return StringUtils.defaultIfEmpty(gitProperties.getProperty(GIT_PROPERTIES_COMMIT_TS_KEY),
             StringUtils.EMPTY);
   }
 }
