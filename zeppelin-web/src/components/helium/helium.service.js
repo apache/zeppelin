b6166bb1c13fe9da774997fcecde3b6649724f60
1ambda <1amb4a@gmail.com>
2017-06-26 13:35:15 +0900

    fix: Respect helium vis order in result

--- zeppelin-web/src/components/helium/helium.service.js
+++ zeppelin-web/src/components/helium/helium.service.js
@@ -28,6 +28,7 @@ export default function heliumService ($http, $sce, baseUrlSrv) {
   'ngInject'
 
   let visualizationBundles = []
+  let visualizationPackageOrder = []
   // name `heliumBundles` should be same as `HeliumBundleFactory.HELIUM_BUNDLES_VAR`
   let heliumBundles = []
   // map for `{ magic: interpreter }`
@@ -70,17 +71,23 @@ export default function heliumService ($http, $sce, baseUrlSrv) {
     })
   }
 
-  this.getVisualizationBundles = function () {
+  this.getVisualizationCachedPackages = function () {
     return visualizationBundles
   }
 
+  this.getVisualizationCachedPackageOrder = function () {
+    return visualizationPackageOrder
+  }
+
   /**
-   * @returns {Promise} which returns bundleOrder
+   * @returns {Promise} which returns bundleOrder and cache it in `visualizationPackageOrder`
    */
   this.getVisualizationPackageOrder = function () {
     return $http.get(baseUrlSrv.getRestApiBase() + '/helium/order/visualization')
       .then(function (response, status) {
-        return response.data.body
+        const order = response.data.body
+        visualizationPackageOrder = order
+        return order
       })
       .catch(function (error) {
         console.error('Can not get bundle order', error)
@@ -126,7 +133,6 @@ export default function heliumService ($http, $sce, baseUrlSrv) {
   this.getAllPackageInfo = function () {
     return $http.get(`${baseUrlSrv.getRestApiBase()}/helium/package`)
       .then(function (response, status) {
-        console.warn(Object.keys(response.data.body).length)
         return response.data.body
       })
       .catch(function (error) {
@@ -285,4 +291,11 @@ export default function heliumService ($http, $sce, baseUrlSrv) {
       }
     })
   })
+
+  this.init = function() {
+    this.getVisualizationPackageOrder()
+  }
+
+  // init
+  this.init()
 }
