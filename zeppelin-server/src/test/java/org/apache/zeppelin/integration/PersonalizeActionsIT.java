90cb1982de861694ea2337a58db56218a245ebe0
soralee <sora0728@zepl.com>
2017-06-26 18:51:47 +0900

    fix: improve simple code in paragraph of graph and dynamicform test case

--- zeppelin-server/src/test/java/org/apache/zeppelin/integration/PersonalizeActionsIT.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/integration/PersonalizeActionsIT.java
@@ -147,37 +147,6 @@ public class PersonalizeActionsIT extends AbstractZeppelinIT {
     waitForParagraph(1, "FINISHED");
   }
 
-
-  private void setParagraphSparkData(int number) {
-    waitForParagraph(number, "READY");
-    setTextOfParagraph(number, "%spark\\n " +
-        "import org.apache.commons.io.IOUtils\\n" +
-        "import java.net.URL\\n" +
-        "import java.nio.charset.Charset\\n" +
-        "val bankText = sc.parallelize(" +
-        "IOUtils.toString(new URL(\"https://s3.amazonaws.com/apache-zeppelin/tutorial/bank/bank.csv\")," +
-        "Charset.forName(\"utf8\")).split(\"\\\\n\"))\\n" +
-        "case class Bank(age: Integer, job: String, marital: String, education: String, balance: Integer)\\n" +
-        "\\n" + "val bank = bankText.map(s => s.split(\";\")).filter(s => s(0) != \"\\\\\"age\\\\\"\")" +
-        ".map(s => Bank(s(0).toInt,s(1).replaceAll(\"\\\\\"\", \"\"),s(2).replaceAll(\"\\\\\"\", \"\")," +
-        "s(3).replaceAll(\"\\\\\"\", \"\"),s(5).replaceAll(\"\\\\\"\", \"\").toInt)).toDF()\\n" +
-        "bank.registerTempTable(\"bank\")");
-    runParagraph(number);
-    try {
-      waitForParagraph(number, "FINISHED");
-    } catch (TimeoutException e) {
-      waitForParagraph(number, "ERROR");
-      collector.checkThat("Exception in PersonalizeActionsIT while testGraphAction, status of 1st Spark Paragraph ",
-          "ERROR", CoreMatchers.equalTo("FINISHED"));
-    }
-    WebElement paragraph1Result = driver.findElement(By.xpath(
-        getParagraphXPath(number) + "//div[contains(@id,\"_text\")]"));
-
-    collector.checkThat("Exception in PersonalizeActionsIT while testGraphAction, 1st Paragraph result ",
-        paragraph1Result.getText().toString(), CoreMatchers.containsString(
-            "import org.apache.commons.io.IOUtils"));
-  }
-
   @Test
   public void testSimpleAction() throws Exception {
     if (!endToEndTestEnabled()) {
@@ -270,32 +239,26 @@ public class PersonalizeActionsIT extends AbstractZeppelinIT {
         createNewNote();
       }
       String noteId = driver.getCurrentUrl().substring(driver.getCurrentUrl().lastIndexOf("/") + 1);
-      personalizeActionsIT.setParagraphSparkData(1);
-      setTextOfParagraph(2, "%sql \\n" +
-          "select age, count(1) value\\n" +
-          "from bank \\n" +
-          "where age < 30 \\n" +
-          "group by age \\n" +
-          "order by age");
-      runParagraph(2);
+      setTextOfParagraph(1, "print(s\"\"\"%table\\n" +
+          "name\\tsize\\n" +
+          "sun\\t100\\n" +
+          "moon\\t10\"\"\")");
+
+      runParagraph(1);
       try {
-        waitForParagraph(2, "FINISHED");
+        waitForParagraph(1, "FINISHED");
       } catch (TimeoutException e) {
-        waitForParagraph(2, "ERROR");
-        collector.checkThat("Exception in PersonalizeActionsIT while testGraphAction, status of 2nd Spark Paragraph ",
+        waitForParagraph(1, "ERROR");
+        collector.checkThat("Exception in PersonalizeActionsIT while testGraphAction, status of 1st Spark Paragraph ",
             "ERROR", CoreMatchers.equalTo("FINISHED"));
       }
 
-      pollingWait(By.xpath(getParagraphXPath(2) +
+      pollingWait(By.xpath(getParagraphXPath(1) +
           "//button[contains(@uib-tooltip, 'Bar Chart')]"), MAX_BROWSER_TIMEOUT_SEC).click();
       collector.checkThat("The output of graph mode is changed",
-          driver.findElement(By.xpath(getParagraphXPath(2) + "//button[contains(@class," +
+          driver.findElement(By.xpath(getParagraphXPath(1) + "//button[contains(@class," +
               "'btn btn-default btn-sm ng-binding ng-scope active')]//i")).getAttribute("class"),
           CoreMatchers.equalTo("fa fa-bar-chart"));
-
-      JavascriptExecutor jse = (JavascriptExecutor)driver;
-      jse.executeScript("window.scrollBy(0,-250)", "");
-      ZeppelinITUtils.sleep(1000, false);
 
       pollingWait(By.xpath("//*[@id='actionbar']" +
           "//button[contains(@uib-tooltip, 'Switch to personal mode')]"), MAX_BROWSER_TIMEOUT_SEC).click();
@@ -303,7 +266,7 @@ public class PersonalizeActionsIT extends AbstractZeppelinIT {
           "]//div[@class='modal-footer']//button[contains(.,'OK')]"));
       personalizeActionsIT.logoutUser("admin");
 
-      // step 2 : (user1) make sure it is on personalized mode and active graph is 'Barchart',
+      // step 2 : (user1) make sure it is on personalized mode and active graph is 'Bar chart',
       // try to change active graph to 'Table' and then check result
       personalizeActionsIT.authenticationUser("user1", "password2");
       locator = By.xpath("//*[@id='notebook-names']//a[contains(@href, '" + noteId + "')]");
@@ -318,14 +281,14 @@ public class PersonalizeActionsIT extends AbstractZeppelinIT {
           CoreMatchers.equalTo("Switch to personal mode (owner can change)"));
 
       collector.checkThat("Make sure the output of graph mode is",
-          driver.findElement(By.xpath(getParagraphXPath(2) + "//button[contains(@class," +
+          driver.findElement(By.xpath(getParagraphXPath(1) + "//button[contains(@class," +
               "'btn btn-default btn-sm ng-binding ng-scope active')]//i")).getAttribute("class"),
           CoreMatchers.equalTo("fa fa-bar-chart"));
 
-      pollingWait(By.xpath(getParagraphXPath(2) +
+      pollingWait(By.xpath(getParagraphXPath(1) +
           "//button[contains(@uib-tooltip, 'Table')]"), MAX_BROWSER_TIMEOUT_SEC).click();
       collector.checkThat("The output of graph mode is not changed",
-          driver.findElement(By.xpath(getParagraphXPath(2) + "//button[contains(@class," +
+          driver.findElement(By.xpath(getParagraphXPath(1) + "//button[contains(@class," +
               "'btn btn-default btn-sm ng-binding ng-scope active')]//i")).getAttribute("class"),
           CoreMatchers.equalTo("fa fa-bar-chart"));
       personalizeActionsIT.logoutUser("user1");
@@ -334,6 +297,7 @@ public class PersonalizeActionsIT extends AbstractZeppelinIT {
       handleException("Exception in PersonalizeActionsIT while testGraphAction ", e);
     }
   }
+
   @Test
   public void testDynamicFormAction() throws Exception {
     try {
@@ -348,30 +312,20 @@ public class PersonalizeActionsIT extends AbstractZeppelinIT {
         createNewNote();
       }
       String noteId = driver.getCurrentUrl().substring(driver.getCurrentUrl().lastIndexOf("/") + 1);
-      personalizeActionsIT.setParagraphSparkData(1);
-      setTextOfParagraph(2, "%sql \\n" +
-          "select age, count(1) value \\n" +
-          "from bank \\n" +
-          "where age < ${maxAge=30} \\n" +
-          "group by age \\n" +
-          "order by age \\n");
-      runParagraph(2);
+      setTextOfParagraph(1, "%spark println(\"Status: \"+z.textbox(\"name\", \"Before\")) ");
+      runParagraph(1);
       try {
-        waitForParagraph(2, "FINISHED");
+        waitForParagraph(1, "FINISHED");
       } catch (TimeoutException e) {
-        waitForParagraph(2, "ERROR");
-        collector.checkThat("Exception in PersonalizeActionsIT while testDynamicFormAction, status of 2nd Spark Paragraph ",
+        waitForParagraph(1, "ERROR");
+        collector.checkThat("Exception in PersonalizeActionsIT while testDynamicFormAction, status of 1st Spark Paragraph ",
             "ERROR", CoreMatchers.equalTo("FINISHED"));
       }
 
       collector.checkThat("The output of graph mode is changed",
-          driver.findElement(By.xpath(getParagraphXPath(2) +
-              "//input[contains(@name, 'maxAge')]")).getAttribute("value"),
-          CoreMatchers.equalTo("30"));
-
-      JavascriptExecutor jse = (JavascriptExecutor)driver;
-      jse.executeScript("window.scrollBy(0,-250)", "");
-      ZeppelinITUtils.sleep(1000, false);
+          driver.findElement(By.xpath(getParagraphXPath(1) +
+              "//input[contains(@name, 'name')]")).getAttribute("value"),
+          CoreMatchers.equalTo("Before"));
 
       pollingWait(By.xpath("//*[@id='actionbar']" +
           "//button[contains(@uib-tooltip, 'Switch to personal mode')]"), MAX_BROWSER_TIMEOUT_SEC).click();
@@ -379,8 +333,8 @@ public class PersonalizeActionsIT extends AbstractZeppelinIT {
           "]//div[@class='modal-footer']//button[contains(.,'OK')]"));
       personalizeActionsIT.logoutUser("admin");
 
-      // step 2 : (user1) make sure it is on personalized mode and  dynamic form value is '30',
-      // try to change dynamic form value to '10' and then check result
+      // step 2 : (user1) make sure it is on personalized mode and  dynamic form value is 'Before',
+      // try to change dynamic form value to 'After' and then check result
       personalizeActionsIT.authenticationUser("user1", "password2");
       locator = By.xpath("//*[@id='notebook-names']//a[contains(@href, '" + noteId + "')]");
       element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
@@ -394,37 +348,27 @@ public class PersonalizeActionsIT extends AbstractZeppelinIT {
           CoreMatchers.equalTo("Switch to personal mode (owner can change)"));
 
       collector.checkThat("The output of graph mode is changed",
-          driver.findElement(By.xpath(getParagraphXPath(2) +
-              "//input[contains(@name, 'maxAge')]")).getAttribute("value"),
-          CoreMatchers.equalTo("30"));
+          driver.findElement(By.xpath(getParagraphXPath(1) +
+              "//input[contains(@name, 'name')]")).getAttribute("value"),
+          CoreMatchers.equalTo("Before"));
 
-      pollingWait(By.xpath(getParagraphXPath(2) +
-          "//input[contains(@name, 'maxAge')]"), MAX_BROWSER_TIMEOUT_SEC).clear();
-      pollingWait(By.xpath(getParagraphXPath(2) +
-          "//input[contains(@name, 'maxAge')]"), MAX_BROWSER_TIMEOUT_SEC).sendKeys("10");
+      pollingWait(By.xpath(getParagraphXPath(1) +
+          "//input[contains(@name, 'name')]"), MAX_BROWSER_TIMEOUT_SEC).clear();
+      pollingWait(By.xpath(getParagraphXPath(1) +
+          "//input[contains(@name, 'name')]"), MAX_BROWSER_TIMEOUT_SEC).sendKeys("After");
 
       runParagraph(1);
       try {
         waitForParagraph(1, "FINISHED");
       } catch (TimeoutException e) {
         waitForParagraph(1, "ERROR");
-        collector.checkThat("Exception in PersonalizeActionsIT while testDynamicFormAction, status of 1st Spark Paragraph ",
-            "ERROR", CoreMatchers.equalTo("FINISHED"));
-      }
-
-      runParagraph(2);
-      try {
-        waitForParagraph(2, "FINISHED");
-      } catch (TimeoutException e) {
-        waitForParagraph(2, "ERROR");
         collector.checkThat("Exception in PersonalizeActionsIT while testDynamicFormAction, status of 2nd Spark Paragraph ",
             "ERROR", CoreMatchers.equalTo("FINISHED"));
       }
 
       collector.checkThat("The output of graph mode is changed",
-          driver.findElement(By.xpath(getParagraphXPath(2) +
-              "//div[contains(@class, 'ui-grid-cell-contents ng-binding ng-scope')]")).getText(),
-          CoreMatchers.equalTo("19"));
+          driver.findElement(By.xpath(getParagraphXPath(1) + "//div[contains(@class, 'text plainTextContent')]")).getText(),
+          CoreMatchers.equalTo("Status: Before"));
       personalizeActionsIT.logoutUser("user1");
 
     } catch (Exception e) {
