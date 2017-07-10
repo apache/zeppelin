15473de6b59f8941562f1b6ae10f4c8b13dbaad2
1ambda <1amb4a@gmail.com>
2017-07-10 17:29:23 +0900

    test: Add a basic e2e test for home

---
+++ zeppelin-web/protractor.conf.js
@@ -0,0 +1,37 @@
+exports.config = {
+  baseUrl: 'http://localhost:8080/',
+  directConnect: true,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      'args': ['show-fps-counter=true']
+    }
+  },
+  allScriptsTimeout: 110000,
+
+  framework: 'jasmine',
+  specs: ['e2e/**/*.js'],
+  jasmineNodeOpts: {
+    showTiming: true,
+    showColors: true,
+    isVerbose: true,
+    includeStackTrace: false,
+    defaultTimeoutInterval: 400000,
+    print: function() {}, // remove protractor dot reporter, we are using jasmine-spec-reporter
+  },
+
+  onPrepare: function() {
+    // waiting for angular app is loaded
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().pageLoadTimeout(40000);
+    browser.manage().timeouts().implicitlyWait(25000);
+
+    // add reporter to display executed tests in console
+    var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: true
+      }
+    }));
+  },
+};
