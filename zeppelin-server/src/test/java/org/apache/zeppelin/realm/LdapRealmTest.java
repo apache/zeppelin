9f2f5a05b7a70ac0e290f1b53b033ae1863c3f56
Herval Freire <hfreire@twitter.com>
2017-07-21 12:56:24 -0700

    add license/fix test

--- zeppelin-server/src/test/java/org/apache/zeppelin/realm/LdapRealmTest.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/realm/LdapRealmTest.java
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.apache.zeppelin.realm;
 
 import java.util.Arrays;
@@ -123,14 +141,6 @@ public class LdapRealmTest {
       new HashSet(Arrays.asList("group-one", "zeppelin-role")),
       roles
     );
-
-    // group search matching enabled
-
-
-    // group search filter supplied
-
-    //
-
   }
 
   private NamingEnumeration<SearchResult> enumerationOf(BasicAttributes... attrs) {
@@ -157,7 +167,7 @@ public class LdapRealmTest {
 
       @Override
       public SearchResult nextElement() {
-        BasicAttributes attrs = iterator.next();
+        final BasicAttributes attrs = iterator.next();
         return new SearchResult(null, null, attrs);
       }
     };
