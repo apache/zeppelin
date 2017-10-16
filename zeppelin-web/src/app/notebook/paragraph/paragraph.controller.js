b37e08407f9ab4872e7f91eec812cc72276c83f3
Malay Majithia <malay.majithia@gmail.com>
2017-10-17 05:16:04 +0530

    Fixed lint error

--- zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
+++ zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
@@ -867,7 +867,9 @@ function ParagraphCtrl ($scope, $rootScope, $route, $window, $routeParams, $loca
         exec: function(env, args, request) {
           let iCursor = $scope.editor.getCursorPosition()
           let currentLine = $scope.editor.session.getLine(iCursor.row)
-          let isAllTabs = currentLine.substring(0, iCursor.column - 1).split('').every(function(char) { return (char === '\t' || char === ' ') })
+          let isAllTabs = currentLine.substring(0, iCursor.column - 1).split('').every(function(char) {
+            return (char === '\t' || char === ' ')
+          })
 
           // If user has pressed tab on first line char or if isTabCompletion() is false, keep existing behavior
           // If user has pressed tab anywhere in between and editor mode is not %md, show autocomplete
