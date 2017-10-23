355b475c579bbe358fc860d4847d4297d6673f97
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2017-10-23 11:46:03 +0530

    knox sso

--- zeppelin-web/src/components/navbar/navbar.controller.js
+++ zeppelin-web/src/components/navbar/navbar.controller.js
@@ -86,12 +86,19 @@ function NavCtrl ($scope, $rootScope, $http, $routeParams, $location,
     websocketMsgSrv.getHomeNote()
   }
 
-  function logout () {
+  function logout() {
     let logoutURL = baseUrlSrv.getRestApiBase() + '/login/logout'
 
     // for firefox and safari
     logoutURL = logoutURL.replace('//', '//false:false@')
-    $http.post(logoutURL).error(function () {
+
+    $http.post(logoutURL).then(function () {}, function (response) {
+      if (response.data) {
+        let res = angular.fromJson(response.data).body
+        if (res['redirectURL']) {
+          window.location.href = res['redirectURL'] + window.location.href
+        }
+      }
       // force authcBasic (if configured) to logout
       $http.post(logoutURL).error(function () {
         $rootScope.userName = ''
