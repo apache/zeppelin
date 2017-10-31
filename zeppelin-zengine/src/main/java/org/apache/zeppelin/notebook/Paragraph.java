41411c7a6d56ca3db6cdaf5588f103c2f834718a
lei wang <lei1989@outlook.com>
2017-10-31 22:54:10 +0800

    overwrite method isTerminated for the disabled paragraph with status ready

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/Paragraph.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/Paragraph.java
@@ -219,8 +219,17 @@ public class Paragraph extends Job implements Cloneable, JsonSerializable {
   }
 
   public boolean isEnabled() {
+    //avoid NullPointException when method isTerminated invoked in ParagraphListenerImpl.afterStatusChange
+    if (config == null) {
+      return true;
+    }
     Boolean enabled = (Boolean) config.get("enabled");
     return enabled == null || enabled.booleanValue();
+  }
+
+  @Override
+  public boolean isTerminated() {
+    return !this.isEnabled() || super.isTerminated();
   }
 
   public String getRequiredReplName() {
