851dcb1a392a3b720501982c6c2c4a3d30468a7e
Andrea Santurbano <santand@gmail.com>
2017-11-07 18:39:58 +0100

    [ZEPPELIN-3038] Network visualization not show "source" and "target" node/edge properties
    ### What is this PR for?
    The Network visualization not show "source" and "target" node/edge properties when the graph is flattened to create the table representation.
    
    ### What type of PR is it?
    [Bug Fix]
    
    ### Todos
    * [x] - Fixed
    
    ### Screenshot
    Before:
    ![zeppelin before](https://user-images.githubusercontent.com/1833335/32510305-6ee266c8-c3f0-11e7-9e28-7ed1f304ebcb.PNG)
    
    After:
    ![zeppelin after](https://user-images.githubusercontent.com/1833335/32510324-8121cc3e-c3f0-11e7-9f1f-84bba563aebc.PNG)
    
    ### What is the Jira issue?
    [ZEPPELIN-3038](https://issues.apache.org/jira/browse/ZEPPELIN-3038)
    
    ### How should this be tested?
    Please use this [notebook](https://gist.github.com/conker84/9574127c2389d08164423894aa93b67f)
    
    ### Questions:
    * Does the licenses files need update? no
    * Is there breaking changes for older versions? no
    * Does this needs documentation? no
    
    Author: Andrea Santurbano <santand@gmail.com>
    
    Closes #2653 from conker84/network-fix and squashes the following commits:
    
    d4f19b6 [Andrea Santurbano] removed unused property
    44100c6 [Andrea Santurbano] Removed semicolons
    273c88f [Andrea Santurbano] added test on edges prevent double rendering
    5eeabc2 [Andrea Santurbano] networkdata class now shows fields "source" and "target" + added test case moved the logic related to the visualization from networkdata to visualization-d3network added some optimization

--- zeppelin-web/src/app/tabledata/networkdata.js
+++ zeppelin-web/src/app/tabledata/networkdata.js
@@ -40,42 +40,40 @@ export default class NetworkData extends TableData {
       return
     }
 
-    this.setNodesDefaults()
-    this.setEdgesDefaults()
-
+    this.graph.edges = this.graph.edges || []
     this.networkNodes = angular.equals({}, this.graph.labels || {})
             ? null : {count: this.graph.nodes.length, labels: this.graph.labels}
     this.networkRelationships = angular.equals([], this.graph.types || [])
             ? null : {count: this.graph.edges.length, types: this.graph.types}
 
-    let rows = []
-    let comment = ''
-    let entities = this.graph.nodes.concat(this.graph.edges)
-    let baseColumnNames = [{name: 'id', index: 0, aggr: 'sum'},
-                       {name: 'label', index: 1, aggr: 'sum'}]
-    let internalFieldsToJump = ['count', 'size', 'totalCount',
-      'data', 'x', 'y', 'labels']
-    let baseCols = _.map(baseColumnNames, function(col) { return col.name })
-    let keys = _.map(entities, function(elem) { return Object.keys(elem.data || {}) })
+    const rows = []
+    const comment = ''
+    const entities = this.graph.nodes.concat(this.graph.edges)
+    const baseColumnNames = [{name: 'id', index: 0, aggr: 'sum'}]
+    const containsLabelField = _.find(entities, (entity) => 'label' in entity) != null
+    if (this.graph.labels || this.graph.types || containsLabelField) {
+      baseColumnNames.push({name: 'label', index: 1, aggr: 'sum'})
+    }
+    const internalFieldsToJump = ['count', 'size', 'totalCount',
+      'data', 'x', 'y', 'labels', 'source', 'target']
+    const baseCols = _.map(baseColumnNames, (col) => col.name)
+    let keys = _.map(entities, (elem) => Object.keys(elem.data || {}))
     keys = _.flatten(keys)
-    keys = _.uniq(keys).filter(function(key) {
-      return baseCols.indexOf(key) === -1
-    })
-    let columnNames = baseColumnNames.concat(_.map(keys, function(elem, i) {
+    keys = _.uniq(keys).filter((key) => baseCols.indexOf(key) === -1)
+    const entityColumnNames = _.map(keys, (elem, i) => {
       return {name: elem, index: i + baseColumnNames.length, aggr: 'sum'}
-    }))
+    })
+    const columnNames = baseColumnNames.concat(entityColumnNames)
     for (let i = 0; i < entities.length; i++) {
-      let entity = entities[i]
-      let col = []
-      let col2 = []
+      const entity = entities[i]
+      const col = []
       entity.data = entity.data || {}
       for (let j = 0; j < columnNames.length; j++) {
-        let name = columnNames[j].name
-        let value = name in entity && internalFieldsToJump.indexOf(name) === -1
+        const name = columnNames[j].name
+        const value = name in entity && internalFieldsToJump.indexOf(name) === -1
             ? entity[name] : entity.data[name]
-        let parsedValue = value === null || value === undefined ? '' : value
+        const parsedValue = value === null || value === undefined ? '' : value
         col.push(parsedValue)
-        col2.push({key: name, value: parsedValue})
       }
       rows.push(col)
     }
@@ -83,63 +81,5 @@ export default class NetworkData extends TableData {
     this.comment = comment
     this.columns = columnNames
     this.rows = rows
-  }
-
-  setNodesDefaults() {
-  }
-
-  setEdgesDefaults() {
-    this.graph.edges
-      .sort((a, b) => {
-        if (a.source > b.source) {
-          return 1
-        } else if (a.source < b.source) {
-          return -1
-        } else if (a.target > b.target) {
-          return 1
-        } else if (a.target < b.target) {
-          return -1
-        } else {
-          return 0
-        }
-      })
-    this.graph.edges
-      .forEach((edge, index) => {
-        let prevEdge = this.graph.edges[index - 1]
-        edge.count = (index > 0 && +edge.source === +prevEdge.source && +edge.target === +prevEdge.target
-            ? prevEdge.count : 0) + 1
-        edge.totalCount = this.graph.edges
-          .filter((innerEdge) => +edge.source === +innerEdge.source && +edge.target === +innerEdge.target)
-          .length
-      })
-    this.graph.edges
-      .forEach((edge) => {
-        if (typeof +edge.source === 'number') {
-          edge.source = this.graph.nodes.filter((node) => +edge.source === +node.id)[0] || null
-        }
-        if (typeof +edge.target === 'number') {
-          edge.target = this.graph.nodes.filter((node) => +edge.target === +node.id)[0] || null
-        }
-      })
-  }
-
-  getNetworkProperties() {
-    let baseCols = ['id', 'label']
-    let properties = {}
-    this.graph.nodes.forEach(function(node) {
-      let hasLabel = 'label' in node && node.label !== ''
-      if (!hasLabel) {
-        return
-      }
-      let label = node.label
-      let hasKey = hasLabel && label in properties
-      let keys = _.uniq(Object.keys(node.data || {})
-              .concat(hasKey ? properties[label].keys : baseCols))
-      if (!hasKey) {
-        properties[label] = {selected: 'label'}
-      }
-      properties[label].keys = keys
-    })
-    return properties
   }
 }
