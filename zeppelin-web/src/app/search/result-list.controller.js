d9c086d32d23e5f8c13f9140086b14b531bf8007
Kevin (Sangwoo) Kim <sangwookim.me@gmail.com>
2018-02-19 10:56:41 +0900

    Update result-list.controller.js

--- zeppelin-web/src/app/search/result-list.controller.js
+++ zeppelin-web/src/app/search/result-list.controller.js
@@ -148,7 +148,7 @@ function SearchResultCtrl ($scope, $routeParams, searchService) {
       )
 
       _editor.getSession().setValue(lines.join('\n'))
-      note.searchResult = lines;
+      note.searchResult = lines
     }
   }
 }
