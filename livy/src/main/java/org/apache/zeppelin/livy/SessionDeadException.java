65d1fb943419aa5554922d0a6ca1e6ecf5316bf6
Jeff Zhang <zjffdu@apache.org>
2018-02-26 10:18:31 +0800

    ZEPPELIN-3246. Need option for automatically restart the livy interpreter automatically as zeppelin does not start new Livy session if yarn livy session application is killed
    ### What is this PR for?
    Add one new property `zeppelin.livy.restart_dead_session` to allow livy session to be created automatically. By default it is false, because there's many reason that session is dead, it is encouraged to ask user to check why it is dead and restart interpreter by themselves.
    
    ### What type of PR is it?
    [Feature]
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/ZEPPELIN-3246
    
    ### How should this be tested?
    * Manually tested.
    
    ### Screenshots (if appropriate)
    ![image](https://user-images.githubusercontent.com/164491/36651529-4a22ab36-1ae4-11e8-88dc-23a814e4c1b1.png)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Jeff Zhang <zjffdu@apache.org>
    
    Closes #2818 from zjffdu/ZPPELIN-3246 and squashes the following commits:
    
    89d44f6 [Jeff Zhang] ZEPPELIN-3246. Need option for automatically restart the livy interpreter automatically as zeppelin does not start new Livy session if yarn livy session application is killed

--- livy/src/main/java/org/apache/zeppelin/livy/SessionDeadException.java
+++ livy/src/main/java/org/apache/zeppelin/livy/SessionDeadException.java
@@ -15,32 +15,8 @@
  * limitations under the License.
  */
 
-package org.apache.zeppelin.interpreter;
+package org.apache.zeppelin.livy;
 
+public class SessionDeadException extends LivyException {
 
-/**
- * General Exception for interpreters.
- *
- */
-public class InterpreterException extends Exception {
-
-  public InterpreterException() {
-  }
-
-  public InterpreterException(Throwable e) {
-    super(e);
-  }
-
-  public InterpreterException(String m) {
-    super(m);
-  }
-
-  public InterpreterException(String msg, Throwable t) {
-    super(msg, t);
-  }
-
-  public InterpreterException(String message, Throwable cause, boolean enableSuppression,
-                       boolean writableStackTrace) {
-    super(message, cause, enableSuppression, writableStackTrace);
-  }
 }
