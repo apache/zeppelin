f646dc385aeee354cedb438b6100654aff9abb22
Jeff Zhang <zjffdu@apache.org>
2018-02-27 21:23:36 +0800

    [HOTFIX] Fix branch-0.8 build
    ### What is this PR for?
    This is a hotfix PR for branch-0.8 build
    
    ### What type of PR is it?
    [Bug Fix]
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * No jira created
    
    ### How should this be tested?
    * CI pass
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Jeff Zhang <zjffdu@apache.org>
    
    Closes #2830 from zjffdu/Hotfix_0.8 and squashes the following commits:
    
    59c2af4 [Jeff Zhang] [HOTFIX] Fix branch-0.8 build

--- spark/spark2-shims/src/main/scala/org/apache/zeppelin/spark/Spark2Shims.java
+++ spark/spark2-shims/src/main/scala/org/apache/zeppelin/spark/Spark2Shims.java
@@ -22,6 +22,9 @@ import org.apache.spark.SparkContext;
 import org.apache.spark.scheduler.SparkListener;
 import org.apache.spark.scheduler.SparkListenerJobStart;
 
+/**
+ * Shims for Spark 2.x
+ */
 public class Spark2Shims extends SparkShims {
 
   public void setupSparkListener(final String sparkWebUrl) {
