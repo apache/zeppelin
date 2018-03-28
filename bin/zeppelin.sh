e850b354a844a85a16b9115ba2f6c0381116a151
Jongyoul Lee <jongyoul@gmail.com>
2018-03-28 16:27:03 +0900

    Removed spark-dependencies and related properties/profiles from pom.xml Added SPARK_DISABLE_DOWNLOAD

--- bin/zeppelin.sh
+++ bin/zeppelin.sh
@@ -87,4 +87,19 @@ if [[ ! -d "${ZEPPELIN_PID_DIR}" ]]; then
   $(mkdir -p "${ZEPPELIN_PID_DIR}")
 fi
 
+# TODO(jl): We need to discuss if we keep this feature or not for the future. See ZEPPELIN-3366
+if [[ -z "${SPARK_HOME}" ]] && [[ "${SPARK_DISABLE_DOWNLOAD}" != "true" ]]; then
+  # I didn't make it configurable because it's not decided to be open to public
+  SPARK_DIST_DIR="${ZEPPELIN_HOME}/.spark-dist"
+  if [[ ! -d ${SPARK_DIST_DIR} ]]; then
+    mkdir -p ${SPARK_DIST_DIR}
+    URL=$(curl -fsSL https://www.apache.org/dyn/closer.lua/spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz | grep -o '<strong>[^<]*</strong>' | sed 's/<[^>]*>//g' | head -1)
+    echo "Downloading Spark distribution from ${URL}"
+    curl -fsSL $URL | (cd $SPARK_DIST_DIR && tar zxf -)
+  fi
+  export SPARK_HOME="${SPARK_DIST_DIR}/spark-2.3.0-bin-hadoop2.7"
+  echo "SPARK_HOME is set by \"${SPARK_HOME}\" into conf/zeppelin-env.sh"
+  echo -e "\nSPARK_HOME=\"${SPARK_DIST_DIR}/spark-2.3.0-bin-hadoop2.7\"\n" >> ${ZEPPELIN_CONF_DIR}/zeppelin-env.sh
+fi
+
 exec $ZEPPELIN_RUNNER $JAVA_OPTS -cp $ZEPPELIN_CLASSPATH_OVERRIDES:${ZEPPELIN_CLASSPATH} $ZEPPELIN_SERVER "$@"
