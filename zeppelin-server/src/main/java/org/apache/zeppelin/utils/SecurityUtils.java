c6ccf4189ad50c09749b2a06d5fb61830075ab84
bag_of_tricks <falbani@hortonworks.com>
2018-04-05 12:27:18 -0400

    ZEPPELIN-3332
    Simply fixes NPE by checking for null

--- zeppelin-server/src/main/java/org/apache/zeppelin/utils/SecurityUtils.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/utils/SecurityUtils.java
@@ -55,7 +55,7 @@ public class SecurityUtils {
   private static final HashSet<String> EMPTY_HASHSET = Sets.newHashSet();
   private static boolean isEnabled = false;
   private static final Logger log = LoggerFactory.getLogger(SecurityUtils.class);
-  
+
   public static void setIsEnabled(boolean value) {
     isEnabled = value;
   }
@@ -149,7 +149,9 @@ public class SecurityUtils {
               new SimplePrincipalCollection(subject.getPrincipal(), realm.getName()),
               ((LdapRealm) realm).getContextFactory()
             );
-            roles = new HashSet<>(auth.getRoles());
+            if (auth != null) {
+              roles = new HashSet<>(auth.getRoles());
+            }
           } catch (NamingException e) {
             log.error("Can't fetch roles", e);
           }
