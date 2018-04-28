289cc3820e9623724fd2d30af36dc72de344a1b0
Savalek <def113@mail.ru>
2018-04-28 13:22:34 +0300

    [ZEPPELIN-2572] remove duplicate 'REMOVE_NOTE_FORMS'

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/socket/Message.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/socket/Message.java
@@ -188,7 +188,6 @@ public class Message implements JsonSerializable {
     REMOVE_NOTE_FORMS,            // remove note forms
     INTERPRETER_INSTALL_STARTED,  // [s-c] start to download an interpreter
     INTERPRETER_INSTALL_RESULT,   // [s-c] Status of an interpreter installation
-    REMOVE_NOTE_FORMS,            // remove note forms
     REMOVE_SELECTED_PARAGRAPHS,   // [c-s] remove selected paragraphs
     SELECTED_PARAGRAPHS_REMOVED,  // [s-c] selected paragraphs removed
     CLEAR_SELECTED_PARAGRAPHS_OUTPUT,   // [c-s] clear output selected paragraphs
