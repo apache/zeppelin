489549fb6ba2e5158188aff3932bda7b02a7389a
egorklimov <klim.electronicmail@gmail.com>
2018-07-20 14:23:34 +0300

    [ZEPPELIN-3628] HTML anchor links on paragraph don't work in Google Chrome
    ### What is this PR for?
    Links like [1] work well in Firefox, but don't work in Chrome.
    1. http://zeppelin/#/notebook/NOTEID?paragraph=PARAGRAPHID
    
    ### What type of PR is it?
    Improvement
    
    ### What is the Jira issue?
    issue on Jira https://issues.apache.org/jira/browse/ZEPPELIN-3628
    
    ### How should this be tested?
    * Manual checking (see screenshot below)
    
    ### Screenshots (if appropriate)
    * #### Before
    Chrome (v67.0.3396.99):
    
    ![chrome](https://user-images.githubusercontent.com/6136993/42819776-31f951e6-89dd-11e8-9618-710f61ea550f.gif)
    Firefox (v61.0.1):
    
    ![firefox](https://user-images.githubusercontent.com/6136993/42819794-39c5562c-89dd-11e8-8059-4e8f85855471.gif)
    * #### After
    Chrome (v67.0.3396.99)
    
    ![fixed](https://user-images.githubusercontent.com/6136993/42819862-63095cd6-89dd-11e8-80a0-843e613ca867.gif)
    
    ### Questions:
    * Does the licenses files need update?
    Yes, JQuery.scrollTo changed from 1.4.14 to 2.1.2
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: egorklimov <klim.electronicmail@gmail.com>
    
    Closes #3078 from egorklimov/ZEPPELIN-3628 and squashes the following commits:
    
    3757129dd [egorklimov] Cursor in paragraph text fixed
    8b521f80e [egorklimov] License github link fixed
    e2ead1035 [egorklimov] jQuery ScrollTo license updated
    87ac4ac35 [egorklimov] jQuery ScrollTo updated
    
    (cherry picked from commit e71fac3754f540c91bdb08529b26cd76d18297d4)
    Signed-off-by: Jeff Zhang <zjffdu@apache.org>

--- zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
+++ zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
@@ -1181,7 +1181,9 @@ function ParagraphCtrl($scope, $rootScope, $route, $window, $routeParams, $locat
     bodyEl.finish();
 
     // scroll to scrollTargetPos
-    bodyEl.scrollTo(scrollTargetPos, {axis: 'y', interrupt: true, duration: 100});
+    if (scrollTargetPos) {
+      bodyEl.scrollTo(scrollTargetPos, {axis: 'y', interrupt: true, duration: 100});
+    }
   };
 
   $scope.getEditorValue = function() {
