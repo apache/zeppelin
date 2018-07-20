3ab6ffaa8bdccb930bd79511f1781d532ef4eac3
Jeff Zhang <zjffdu@apache.org>
2018-07-20 17:10:52 +0800

    ZEPPELIN-3643. Put Shiro related thing in one place
    ### What is this PR for?
    Move all shiro related thing in one place (`SecurityUtils`)
    
    ### What type of PR is it?
    [Refactoring]
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/ZEPPELIN-3643
    
    ### How should this be tested?
    * CI pass
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Jeff Zhang <zjffdu@apache.org>
    
    Closes #3091 from zjffdu/ZEPPELIN-3643 and squashes the following commits:
    
    d10482c5c [Jeff Zhang] ZEPPELIN-3643. Put Shiro related thing in one place

--- zeppelin-server/src/main/java/org/apache/zeppelin/utils/SecurityUtils.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/utils/SecurityUtils.java
@@ -16,35 +16,50 @@
  */
 package org.apache.zeppelin.utils;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.Realm;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.apache.shiro.realm.ldap.JndiLdapContextFactory;
+import org.apache.shiro.realm.ldap.JndiLdapRealm;
 import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.JdbcUtils;
 import org.apache.shiro.util.ThreadContext;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.zeppelin.conf.ZeppelinConfiguration;
+import org.apache.zeppelin.realm.ActiveDirectoryGroupRealm;
+import org.apache.zeppelin.realm.LdapRealm;
+import org.apache.zeppelin.server.ZeppelinServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.LdapContext;
+import javax.sql.DataSource;
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.security.Principal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-
-import javax.naming.NamingException;
-
-import org.apache.zeppelin.conf.ZeppelinConfiguration;
-import org.apache.zeppelin.realm.ActiveDirectoryGroupRealm;
-import org.apache.zeppelin.realm.LdapRealm;
-import org.apache.zeppelin.server.ZeppelinServer;
 
 /**
  * Tools for securing Zeppelin.
@@ -53,7 +68,9 @@ public class SecurityUtils {
   private static final String ANONYMOUS = "anonymous";
   private static final HashSet<String> EMPTY_HASHSET = Sets.newHashSet();
   private static boolean isEnabled = false;
-  private static final Logger log = LoggerFactory.getLogger(SecurityUtils.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(SecurityUtils.class);
+  private static Collection<Realm> realms;
+
 
   public static void setIsEnabled(boolean value) {
     isEnabled = value;
@@ -93,7 +110,7 @@ public class SecurityUtils {
     if (subject.isAuthenticated()) {
       principal = extractPrincipal(subject);
       if (ZeppelinServer.notebook.getConf().isUsernameForceLowerCase()) {
-        log.debug("Converting principal name " + principal
+        LOGGER.debug("Converting principal name " + principal
             + " to lower case:" + principal.toLowerCase());
         principal = principal.toLowerCase();
       }
@@ -124,6 +141,81 @@ public class SecurityUtils {
     Collection<Realm> realms = defaultWebSecurityManager.getRealms();
     return realms;
   }
+  
+  /**
+   * Checked if shiro enabled or not.
+   */
+  public static boolean isAuthenticated() {
+    if (!isEnabled) {
+      return false;
+    }
+    return org.apache.shiro.SecurityUtils.getSubject().isAuthenticated();
+  }
+
+
+  /**
+   * Get candidated users based on searchText
+   * @param searchText
+   * @param numUsersToFetch
+   * @return
+   */
+  public static List<String> getMatchedUsers(String searchText, int numUsersToFetch) {
+    List<String> usersList = new ArrayList<>();
+    try {
+      Collection realmsList = SecurityUtils.getRealmsList();
+      if (realmsList != null) {
+        for (Iterator<Realm> iterator = realmsList.iterator(); iterator.hasNext(); ) {
+          Realm realm = iterator.next();
+          String name = realm.getClass().getName();
+          LOGGER.debug("RealmClass.getName: " + name);
+          if (name.equals("org.apache.shiro.realm.text.IniRealm")) {
+            usersList.addAll(SecurityUtils.getUserList((IniRealm) realm));
+          } else if (name.equals("org.apache.zeppelin.realm.LdapGroupRealm")) {
+            usersList.addAll(getUserList((JndiLdapRealm) realm, searchText,
+                numUsersToFetch));
+          } else if (name.equals("org.apache.zeppelin.realm.LdapRealm")) {
+            usersList.addAll(getUserList((LdapRealm) realm, searchText,
+                numUsersToFetch));
+          } else if (name.equals("org.apache.zeppelin.realm.ActiveDirectoryGroupRealm")) {
+            usersList.addAll(getUserList((ActiveDirectoryGroupRealm) realm,
+                searchText, numUsersToFetch));
+          } else if (name.equals("org.apache.shiro.realm.jdbc.JdbcRealm")) {
+            usersList.addAll(getUserList((JdbcRealm) realm));
+          }
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("Exception in retrieving Users from realms ", e);
+    }
+    return usersList;
+  }
+
+  /**
+   * Get matched roles.
+   *
+   * @return
+   */
+  public static List<String> getMatchedRoles() {
+    List<String> rolesList = new ArrayList<>();
+    try {
+      Collection realmsList = SecurityUtils.getRealmsList();
+      if (realmsList != null) {
+        for (Iterator<Realm> iterator = realmsList.iterator(); iterator.hasNext(); ) {
+          Realm realm = iterator.next();
+          String name = realm.getClass().getName();
+          LOGGER.debug("RealmClass.getName: " + name);
+          if (name.equals("org.apache.shiro.realm.text.IniRealm")) {
+            rolesList.addAll(getRolesList((IniRealm) realm));
+          } else if (name.equals("org.apache.zeppelin.realm.LdapRealm")) {
+            rolesList.addAll(getRolesList((LdapRealm) realm));
+          }
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("Exception in retrieving Users from realms ", e);
+    }
+    return rolesList;
+  }
 
   /**
    * Return the roles associated with the authenticated user if any otherwise returns empty set.
@@ -131,9 +223,9 @@ public class SecurityUtils {
    *
    * @return shiro roles
    */
-  public static HashSet<String> getRoles() {
+  public static HashSet<String> getAssociatedRoles() {
     if (!isEnabled) {
-      return EMPTY_HASHSET;
+      return  Sets.newHashSet();
     }
     Subject subject = org.apache.shiro.SecurityUtils.getSubject();
     HashSet<String> roles = new HashSet<>();
@@ -150,14 +242,14 @@ public class SecurityUtils {
         } else if (name.equals("org.apache.zeppelin.realm.LdapRealm")) {
           try {
             AuthorizationInfo auth = ((LdapRealm) realm).queryForAuthorizationInfo(
-                    new SimplePrincipalCollection(subject.getPrincipal(), realm.getName()),
-                    ((LdapRealm) realm).getContextFactory()
+                new SimplePrincipalCollection(subject.getPrincipal(), realm.getName()),
+                ((LdapRealm) realm).getContextFactory()
             );
             if (auth != null) {
               roles = new HashSet<>(auth.getRoles());
             }
           } catch (NamingException e) {
-            log.error("Can't fetch roles", e);
+            LOGGER.error("Can't fetch roles", e);
           }
           break;
         } else if (name.equals("org.apache.zeppelin.realm.ActiveDirectoryGroupRealm")) {
@@ -179,12 +271,204 @@ public class SecurityUtils {
   }
 
   /**
-   * Checked if shiro enabled or not.
+   * Function to extract users from shiro.ini.
    */
-  public static boolean isAuthenticated() {
-    if (!isEnabled) {
-      return false;
+  private static List<String> getUserList(IniRealm r) {
+    List<String> userList = new ArrayList<>();
+    Map getIniUser = r.getIni().get("users");
+    if (getIniUser != null) {
+      Iterator it = getIniUser.entrySet().iterator();
+      while (it.hasNext()) {
+        Map.Entry pair = (Map.Entry) it.next();
+        userList.add(pair.getKey().toString().trim());
+      }
     }
-    return org.apache.shiro.SecurityUtils.getSubject().isAuthenticated();
+    return userList;
+  }
+
+
+  /***
+   * Get user roles from shiro.ini.
+   *
+   * @param r
+   * @return
+   */
+  private static List<String> getRolesList(IniRealm r) {
+    List<String> roleList = new ArrayList<>();
+    Map getIniRoles = r.getIni().get("roles");
+    if (getIniRoles != null) {
+      Iterator it = getIniRoles.entrySet().iterator();
+      while (it.hasNext()) {
+        Map.Entry pair = (Map.Entry) it.next();
+        roleList.add(pair.getKey().toString().trim());
+      }
+    }
+    return roleList;
+  }
+
+  /**
+   * Function to extract users from LDAP.
+   */
+  private static List<String> getUserList(JndiLdapRealm r, String searchText, int numUsersToFetch) {
+    List<String> userList = new ArrayList<>();
+    String userDnTemplate = r.getUserDnTemplate();
+    String userDn[] = userDnTemplate.split(",", 2);
+    String userDnPrefix = userDn[0].split("=")[0];
+    String userDnSuffix = userDn[1];
+    JndiLdapContextFactory cf = (JndiLdapContextFactory) r.getContextFactory();
+    try {
+      LdapContext ctx = cf.getSystemLdapContext();
+      SearchControls constraints = new SearchControls();
+      constraints.setCountLimit(numUsersToFetch);
+      constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
+      String[] attrIDs = {userDnPrefix};
+      constraints.setReturningAttributes(attrIDs);
+      NamingEnumeration result = ctx.search(userDnSuffix, "(" + userDnPrefix + "=*" + searchText +
+          "*)", constraints);
+      while (result.hasMore()) {
+        Attributes attrs = ((SearchResult) result.next()).getAttributes();
+        if (attrs.get(userDnPrefix) != null) {
+          String currentUser = attrs.get(userDnPrefix).toString();
+          userList.add(currentUser.split(":")[1].trim());
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("Error retrieving User list from Ldap Realm", e);
+    }
+    LOGGER.info("UserList: " + userList);
+    return userList;
+  }
+
+  /**
+   * Function to extract users from Zeppelin LdapRealm.
+   */
+  private static List<String> getUserList(LdapRealm r, String searchText, int numUsersToFetch) {
+    List<String> userList = new ArrayList<>();
+    LOGGER.debug("SearchText: " + searchText);
+    String userAttribute = r.getUserSearchAttributeName();
+    String userSearchRealm = r.getUserSearchBase();
+    String userObjectClass = r.getUserObjectClass();
+    JndiLdapContextFactory cf = (JndiLdapContextFactory) r.getContextFactory();
+    try {
+      LdapContext ctx = cf.getSystemLdapContext();
+      SearchControls constraints = new SearchControls();
+      constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
+      constraints.setCountLimit(numUsersToFetch);
+      String[] attrIDs = {userAttribute};
+      constraints.setReturningAttributes(attrIDs);
+      NamingEnumeration result = ctx.search(userSearchRealm, "(&(objectclass=" +
+          userObjectClass + ")("
+          + userAttribute + "=*" + searchText + "*))", constraints);
+      while (result.hasMore()) {
+        Attributes attrs = ((SearchResult) result.next()).getAttributes();
+        if (attrs.get(userAttribute) != null) {
+          String currentUser;
+          if (r.getUserLowerCase()) {
+            LOGGER.debug("userLowerCase true");
+            currentUser = ((String) attrs.get(userAttribute).get()).toLowerCase();
+          } else {
+            LOGGER.debug("userLowerCase false");
+            currentUser = (String) attrs.get(userAttribute).get();
+          }
+          LOGGER.debug("CurrentUser: " + currentUser);
+          userList.add(currentUser.trim());
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("Error retrieving User list from Ldap Realm", e);
+    }
+    return userList;
+  }
+
+  /***
+   * Get user roles from shiro.ini for Zeppelin LdapRealm.
+   *
+   * @param r
+   * @return
+   */
+  private static List<String> getRolesList(LdapRealm r) {
+    List<String> roleList = new ArrayList<>();
+    Map<String, String> roles = r.getListRoles();
+    if (roles != null) {
+      Iterator it = roles.entrySet().iterator();
+      while (it.hasNext()) {
+        Map.Entry pair = (Map.Entry) it.next();
+        LOGGER.debug("RoleKeyValue: " + pair.getKey() +
+            " = " + pair.getValue());
+        roleList.add((String) pair.getKey());
+      }
+    }
+    return roleList;
+  }
+
+  private static List<String> getUserList(ActiveDirectoryGroupRealm r, String searchText,
+                                   int numUsersToFetch) {
+    List<String> userList = new ArrayList<>();
+    try {
+      LdapContext ctx = r.getLdapContextFactory().getSystemLdapContext();
+      userList = r.searchForUserName(searchText, ctx, numUsersToFetch);
+    } catch (Exception e) {
+      LOGGER.error("Error retrieving User list from ActiveDirectory Realm", e);
+    }
+    return userList;
+  }
+
+  /**
+   * Function to extract users from JDBCs.
+   */
+  private static List<String> getUserList(JdbcRealm obj) {
+    List<String> userlist = new ArrayList<>();
+    Connection con = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    DataSource dataSource = null;
+    String authQuery = "";
+    String retval[];
+    String tablename = "";
+    String username = "";
+    String userquery;
+    try {
+      dataSource = (DataSource) FieldUtils.readField(obj, "dataSource", true);
+      authQuery = (String) FieldUtils.readField(obj, "authenticationQuery", true);
+      LOGGER.info(authQuery);
+      String authQueryLowerCase = authQuery.toLowerCase();
+      retval = authQueryLowerCase.split("from", 2);
+      if (retval.length >= 2) {
+        retval = retval[1].split("with|where", 2);
+        tablename = retval[0];
+        retval = retval[1].split("where", 2);
+        if (retval.length >= 2) {
+          retval = retval[1].split("=", 2);
+        } else {
+          retval = retval[0].split("=", 2);
+        }
+        username = retval[0];
+      }
+
+      if (StringUtils.isBlank(username) || StringUtils.isBlank(tablename)) {
+        return userlist;
+      }
+
+      userquery = String.format("SELECT %s FROM %s", username, tablename);
+    } catch (IllegalAccessException e) {
+      LOGGER.error("Error while accessing dataSource for JDBC Realm", e);
+      return Lists.newArrayList();
+    }
+
+    try {
+      con = dataSource.getConnection();
+      ps = con.prepareStatement(userquery);
+      rs = ps.executeQuery();
+      while (rs.next()) {
+        userlist.add(rs.getString(1).trim());
+      }
+    } catch (Exception e) {
+      LOGGER.error("Error retrieving User list from JDBC Realm", e);
+    } finally {
+      JdbcUtils.closeResultSet(rs);
+      JdbcUtils.closeStatement(ps);
+      JdbcUtils.closeConnection(con);
+    }
+    return userlist;
   }
 }
