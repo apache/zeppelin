a129815f15a2ab669e3c83c7326d13ec20ab7289
Vinay <vinayakshedgeri@gmail.com>
2018-12-07 11:04:14 +0530

    Update app.js

--- zeppelin-web/src/app/app.js
+++ zeppelin-web/src/app/app.js
@@ -15,8 +15,8 @@
  * limitations under the License.
  */
 
-import "babel-polyfill";
-import "core-js/modules/es7.promise.finally";
+import 'babel-polyfill';
+import 'core-js/modules/es7.promise.finally';
 import 'headroom.js';
 import 'headroom.js/dist/angular.headroom';
 
