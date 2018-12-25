949ead1c6073ed5252d4d68151cf7f0dec477782
liuxunorg <33611720@qq.com>
2018-12-25 23:19:28 +0800

    [ZEPPELIN-3920] Paragraph support custom menu display with multiple URL links
    ### What is this PR for?
    
    Now the execution of the paragraph in zeppelin only supports the display of a single URL link, for example: Spark paragraph execution is a Spark JOB URL link.
    The machine learning task in the hadoop submarine needs to provide the user with a tensorflow Parameter server, a tensorflow worker server log link in YARN, and a tensorboard link, so a custom menu display that supports multiple URL links is now available.
    
    ### What type of PR is it?
    [Improvement]
    
    ### Todos
    * [x] Add testRuntimeInfos test cases in NotebookServerTest.java
    * [x] modify paragraph-control.html
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/ZEPPELIN-3920
    
    ### How should this be tested?
    CI pass
    
    ### Screenshots (if appropriate)
    
    ![alt text](https://github.com/liuxunorg/images/blob/master/zeppelin/multi-job-url.gif?raw=true "multi-job-url.gif")
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No

--- zeppelin-server/src/test/java/org/apache/zeppelin/socket/NotebookServerTest.java
+++ zeppelin-server/src/test/java/org/apache/zeppelin/socket/NotebookServerTest.java
@@ -35,6 +35,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
+import java.util.Map;
 import javax.inject.Provider;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.zeppelin.conf.ZeppelinConfiguration;
@@ -417,6 +418,54 @@ public class NotebookServerTest extends AbstractTestRestApi {
               createdNote.getId()).getId(), defaultInterpreterId);
     }
     notebook.removeNote(createdNote.getId(), anonymous);
+  }
+
+  @Test
+  public void testRuntimeInfos() {
+    // mock note
+    String msg = "{\"op\":\"IMPORT_NOTE\",\"data\":" +
+        "{\"note\":{\"paragraphs\": [{\"text\": \"Test " +
+        "paragraphs import\"," + "\"progressUpdateIntervalMs\":500," +
+        "\"config\":{},\"settings\":{}}]," +
+        "\"name\": \"Test Zeppelin notebook import\",\"config\": " +
+        "{}}}}";
+    Message messageReceived = notebookServer.deserializeMessage(msg);
+    Note note = null;
+    try {
+      note = notebookServer.importNote(null, messageReceived);
+    } catch (NullPointerException e) {
+      //broadcastNoteList(); failed nothing to worry.
+      LOG.error("Exception in NotebookServerTest while testImportNotebook, failed nothing to " +
+          "worry ", e);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    assertNotEquals(null, notebook.getNote(note.getId()));
+    assertNotEquals(null, note.getParagraph(0));
+
+    String nodeId = note.getId();
+    String paragraphId = note.getParagraph(0).getId();
+
+    // update RuntimeInfos
+    Map<String, String> infos = new java.util.HashMap<>();
+    infos.put("jobUrl", "jobUrl_value");
+    infos.put("jobLabel", "jobLabel_value");
+    infos.put("label", "SPARK JOB");
+    infos.put("tooltip", "View in Spark web UI");
+    infos.put("noteId", nodeId);
+    infos.put("paraId", paragraphId);
+
+    notebookServer.onParaInfosReceived(nodeId, paragraphId, "spark", infos);
+    Paragraph paragraph = note.getParagraph(paragraphId);
+
+    // check RuntimeInfos
+    assertTrue(paragraph.getRuntimeInfos().containsKey("jobUrl"));
+    List<Map<String, String>> list = paragraph.getRuntimeInfos().get("jobUrl").getValue();
+    assertEquals(1, list.size());
+    assertEquals(2, list.get(0).size());
+    assertEquals(list.get(0).get("jobUrl"), "jobUrl_value");
+    assertEquals(list.get(0).get("jobLabel"), "jobLabel_value");
   }
 
   private NotebookSocket createWebSocket() {
--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/ParagraphRuntimeInfo.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/ParagraphRuntimeInfo.java
@@ -1,19 +1,24 @@
 package org.apache.zeppelin.notebook;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Store runtime infos of each para
  *
  */
 public class ParagraphRuntimeInfo {
-
   private String propertyName;  // Name of the property
   private String label;         // Label to be used in UI
   private String tooltip;       // Tooltip text toshow in UI
   private String group;         // The interpretergroup from which the info was derived
-  private List<String> values;  // values for the property
+
+  // runtimeInfos job url or dropdown-menu key in
+  // zeppelin-web/src/app/notebook/paragraph/paragraph-control.html
+  private List<Map<String, String>> values;  // values for the key-value pair property
   private String interpreterSettingId;
   
   public ParagraphRuntimeInfo(String propertyName, String label, 
@@ -29,8 +34,13 @@ public class ParagraphRuntimeInfo {
     this.values = new ArrayList<>();
   }
 
-  public void addValue(String value) {
-    values.add(value);
+  public void addValue(Map<String, String> mapValue) {
+    values.add(mapValue);
+  }
+
+  @VisibleForTesting
+  public List<Map<String, String>> getValue() {
+    return values;
   }
   
   public String getInterpreterSettingId() {
