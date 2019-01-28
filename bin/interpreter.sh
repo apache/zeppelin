41f4630b80681f5cce02f0700371fee9e31fc0f7
Jeff Zhang <zjffdu@apache.org>
2019-01-28 14:09:59 +0800

    ZEPPELIN-3887. Print INTERPRETER_RUN_COMMAND in interpreter.sh for debugging purpose

--- bin/interpreter.sh
+++ bin/interpreter.sh
@@ -246,6 +246,7 @@ if [[ ! -z "$ZEPPELIN_IMPERSONATE_USER" ]] && [[ -n "${suid}" || -z "${SPARK_SUB
     INTERPRETER_RUN_COMMAND+="'"
 fi
 
+echo "Interpreter launch command: $INTERPRETER_RUN_COMMAND"
 eval $INTERPRETER_RUN_COMMAND &
 pid=$!
 
