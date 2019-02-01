0d3ac8a56f3c7f53ecf2888ea853252794daa88a
fdai <fdai@twitter.com>
2019-02-01 10:49:07 -0800

    [ZEPPELIN-3978] Change Jetty Server to use QueuedThreadPool and make it configurable
    ### What is this PR for?
    By default Jetty Server only supports 200 threads.
    On our daily use cases, we normally have more than 100 users on the single Zeppelin Server, which will create more than 200 threads in Jetty.
    
    That could cause slowness or stuck because users could not receive WebSocket message.
    
    Add configurable param feature to scale up the performance for Single Zeppelin Server whenever needs it
    
    ### What type of PR is it?
    Improvement
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/ZEPPELIN-3978
    [ZEPPELIN-3978]
    
    ### How should this be tested?
    * First time? Setup Travis CI as described on https://zeppelin.apache.org/contribution/contributions.html#continuous-integration
    * Strongly recommended: add automated unit tests for any new or changed behavior
    * Outline any manual steps to test the PR here.
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: fdai <fdai@twitter.com>
    
    Closes #3294 from fred521/mainStream/fdai_increase_thread_pool_number and squashes the following commits:
    
    62986a225 [fdai] add description to template for new param
    23e79a953 [fdai] make the param to be configurable
    ff5b6a140 [fdai] custom the thread number to have better performance

--- zeppelin-server/src/main/java/org/apache/zeppelin/server/ZeppelinServer.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/server/ZeppelinServer.java
@@ -76,6 +76,8 @@ import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.ThreadPool;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.glassfish.hk2.api.ServiceLocator;
@@ -276,7 +278,11 @@ public class ZeppelinServer extends ResourceConfig {
   }
 
   private static Server setupJettyServer(ZeppelinConfiguration conf) {
-    final Server server = new Server();
+    ThreadPool threadPool =
+      new QueuedThreadPool(conf.getInt(ConfVars.ZEPPELIN_SERVER_JETTY_THREAD_POOL_MAX),
+                           conf.getInt(ConfVars.ZEPPELIN_SERVER_JETTY_THREAD_POOL_MIN),
+                           conf.getInt(ConfVars.ZEPPELIN_SERVER_JETTY_THREAD_POOL_TIMEOUT));
+    final Server server = new Server(threadPool);
     ServerConnector connector;
 
     if (conf.useSsl()) {
