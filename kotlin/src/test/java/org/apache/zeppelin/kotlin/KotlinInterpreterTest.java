0a71d93dc65afd591db48ff0eebf159c9d57ca9c
dkaznacheev <dmitry.kaznacheev@gmail.com>
2019-08-16 12:23:47 +0300

    Added output test

--- kotlin/src/test/java/org/apache/zeppelin/kotlin/KotlinInterpreterTest.java
+++ kotlin/src/test/java/org/apache/zeppelin/kotlin/KotlinInterpreterTest.java
@@ -24,24 +24,28 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Properties;
 import org.apache.zeppelin.interpreter.InterpreterContext;
 import org.apache.zeppelin.interpreter.InterpreterException;
+import org.apache.zeppelin.interpreter.InterpreterOutput;
+import org.apache.zeppelin.interpreter.InterpreterOutputListener;
 import org.apache.zeppelin.interpreter.InterpreterResult;
+import org.apache.zeppelin.interpreter.InterpreterResultMessageOutput;
 import org.apache.zeppelin.kotlin.receiver.ZeppelinKotlinReceiver;
 
-/**
- * KotlinInterpreterTest
- */
+
 public class KotlinInterpreterTest {
 
   private static KotlinInterpreter interpreter;
   private static InterpreterContext context;
 
+  private static volatile String output = "";
+
   @BeforeClass
   public static void setUp() throws InterpreterException {
-    context = InterpreterContext.builder().build();
+    context = getInterpreterContext();
     interpreter = new KotlinInterpreter(new Properties());
 
     String cp = System.getProperty("java.class.path") + File.pathSeparator +
@@ -59,12 +63,17 @@ public class KotlinInterpreterTest {
   private static void testCodeForResult(String code, String expected) throws Exception {
     InterpreterResult result = interpreter.interpret(code, context);
 
-    assertEquals(SUCCESS, result.code());
-    assertEquals(1, result.message().size());
+    String value;
+    if (result.message().isEmpty()) {
+      value = "";
+    } else {
+      String message = result.message().get(0).getData().trim();
+      System.err.println(message);
+      // "res0 : kotlin.Int = 1" -> "kotlin.Int = 1"
+      value = message.substring(message.indexOf(':') + 2);
+    }
 
-    String message = result.message().get(0).getData().trim();
-    // "res0 : kotlin.Int = 1" -> "kotlin.Int = 1"
-    String value = message.substring(message.indexOf(':') + 2);
+    assertEquals(SUCCESS, result.code());
     assertEquals(expected, value);
   }
 
@@ -97,5 +106,40 @@ public class KotlinInterpreterTest {
     InterpreterResult result = interpreter.interpret("prinln(1)", context);
     assertEquals(ERROR, result.code());
     assertEquals("Unresolved reference: prinln", result.message().get(0).getData().trim());
+  }
+
+  @Test
+  public void testOutput() throws Exception {
+    testCodeForResult("println(\"Hello Kotlin\")", "");
+    assertEquals("Hello Kotlin\n", output);
+  }
+
+  private static InterpreterContext getInterpreterContext() {
+    output = "";
+    InterpreterContext context = InterpreterContext.builder()
+        .setInterpreterOut(new InterpreterOutput(null))
+        .build();
+    context.out = new InterpreterOutput(
+        new InterpreterOutputListener() {
+          @Override
+          public void onUpdateAll(InterpreterOutput out) {
+
+          }
+
+          @Override
+          public void onAppend(int index, InterpreterResultMessageOutput out, byte[] line) {
+            try {
+              output = out.toInterpreterResultMessage().getData();
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          }
+
+          @Override
+          public void onUpdate(int index, InterpreterResultMessageOutput out) {
+
+          }
+        });
+    return context;
   }
 }
