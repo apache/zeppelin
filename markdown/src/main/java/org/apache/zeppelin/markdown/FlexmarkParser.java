0d9239ef333e04e0788dbccfd1d00314b97a4370
Bhavik Patel <bhavikpatel552@gmail.com>
2019-09-17 12:30:47 +0530

    [ZEPPELIN-4311] Supporting new parser for markdown as pegdown parser …
    ### What is this PR for?
    Supporting new parser flexmark for markdown, as pegdown parser is deprecated and facing issue with pegdown parser.
    
    ### What type of PR is it?
    [Bug Fix | Improvement]
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/ZEPPELIN-4311
    
    ### How should this be tested?
    CI Pass
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Bhavik Patel <bhavikpatel552@gmail.com>
    
    Closes #3443 from bhavikpatel9977/ZEPPELIN-4311 and squashes the following commits:
    
    59edd8d73 [Bhavik Patel] Incroporated the review changes
    0f5980381 [Bhavik Patel] added license and documentation
    e28f68226 [Bhavik Patel] Added license
    e4b54ce4a [Bhavik Patel] [ZEPPELIN-4311] Supporting new parser for markdown as pegdown parser is deprecated and facing issue with parser
    
    Change-Id: I4aa3bc93267b3a5d5003af176f84aa37e4c742ff

---
+++ markdown/src/main/java/org/apache/zeppelin/markdown/FlexmarkParser.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.zeppelin.markdown;
+
+
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.typographic.TypographicExtension;
+import com.vladsch.flexmark.ext.wikilink.WikiLinkExtension;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+/**
+ * Flexmark Parser
+ */
+public class FlexmarkParser implements MarkdownParser {
+  private static final Logger LOGGER = LoggerFactory.getLogger(FlexmarkParser.class);
+  private Parser parser;
+  private HtmlRenderer renderer;
+
+  public FlexmarkParser() {
+    MutableDataSet options = new MutableDataSet();
+    options.set(Parser.EXTENSIONS, Arrays.asList(StrikethroughExtension.create(),
+        TablesExtension.create(),
+        UMLExtension.create(),
+        AutolinkExtension.create(),
+        WikiLinkExtension.create(),
+        TypographicExtension.create()));
+    options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
+    parser = Parser.builder(options).build();
+    renderer = HtmlRenderer.builder(options).build();
+  }
+
+  @Override
+  public String render(String markdownText) {
+    Node document = parser.parse(markdownText);
+    String html = renderer.render(document);
+    return wrapWithMarkdownClassDiv(html);
+  }
+
+  /**
+   * wrap with markdown class div to styling DOM using css.
+   */
+  public static String wrapWithMarkdownClassDiv(String html) {
+    return new StringBuilder()
+        .append("<div class=\"markdown-body\">\n")
+        .append(html)
+        .append("\n</div>")
+        .toString();
+  }
+}
