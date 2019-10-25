ec4475ca6a0f03306eef6e78fc45fc16c09673e9
Hsuan Lee <hsuangm@gmail.com>
2019-10-25 09:56:39 +0800

    chore: update LICENSE and add pom.xml in zeppelin-frontend

--- zeppelin-frontend/src/app/app.component.spec.ts
+++ zeppelin-frontend/src/app/app.component.spec.ts
@@ -27,17 +27,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'zeppelin'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('zeppelin');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('zeppelin app is running!');
-  });
 });
