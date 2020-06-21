90c1136691242a494940d0be623e1a861845abef
1721563048@qq.com <a18652727118>
2020-06-21 16:34:03 +0800

    [ZEPPELIN-4881] add new function for reorder this array without Head revision, put Head revision in the first

--- zeppelin-web/src/app/notebook/notebook.controller.js
+++ zeppelin-web/src/app/notebook/notebook.controller.js
@@ -54,6 +54,15 @@ function NotebookCtrl($scope, $route, $routeParams, $location, $rootScope,
     return moment.unix(date).format('MMMM Do YYYY, h:mm a');
   };
 
+  // reorder this array without Head revision, put Head revision in the first
+  $scope.revisionSort = function(array) {
+    let orderArr = array.slice(1).sort((a, b) => {
+      return b.time-a.time;
+    });
+    orderArr.unshift(array[0]);
+    return orderArr;
+  };
+
   $scope.interpreterSettings = [];
   $scope.interpreterBindings = [];
   $scope.isNoteDirty = null;
