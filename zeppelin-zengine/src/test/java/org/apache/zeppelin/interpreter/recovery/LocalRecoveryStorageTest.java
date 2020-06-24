9042d3c9ab43e28002e947c0f1ad0d67207a2730
Jeff Zhang <zjffdu@apache.org>
2020-06-24 13:03:38 +0800

    [ZEPPELIN-4907]. Add LocalRecoveryStorage
    ### What is this PR for?
    
    This PR add LocalRecoveryStorage based on java native local file system. Without this PR, user can only enable recovery by using FileSystemRecoveryStorage which depends on hadoop. This implementation doesn't depends on hadoop, user can enable recovery just by using this RecoveryStorage.
    
    ### What type of PR is it?
    [Feature]
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/ZEPPELIN-4907
    
    ### How should this be tested?
    * CI pass
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Jeff Zhang <zjffdu@apache.org>
    
    Closes #3822 from zjffdu/ZEPPELIN-4907 and squashes the following commits:
    
    9d10a10ac [Jeff Zhang] [ZEPPELIN-4907]. Add LocalRecoveryStorage

--- zeppelin-zengine/src/test/java/org/apache/zeppelin/interpreter/recovery/LocalRecoveryStorageTest.java
+++ zeppelin-zengine/src/test/java/org/apache/zeppelin/interpreter/recovery/LocalRecoveryStorageTest.java
@@ -37,14 +37,13 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
-public class FileSystemRecoveryStorageTest extends AbstractInterpreterTest {
-
+public class LocalRecoveryStorageTest extends AbstractInterpreterTest {
   private File recoveryDir = null;
 
   @Before
   public void setUp() throws Exception {
     System.setProperty(ZeppelinConfiguration.ConfVars.ZEPPELIN_RECOVERY_STORAGE_CLASS.getVarName(),
-        FileSystemRecoveryStorage.class.getName());
+            LocalRecoveryStorage.class.getName());
     recoveryDir = Files.createTempDir();
     System.setProperty(ZeppelinConfiguration.ConfVars.ZEPPELIN_RECOVERY_DIR.getVarName(), recoveryDir.getAbsolutePath());
     super.setUp();
@@ -65,9 +64,9 @@ public class FileSystemRecoveryStorageTest extends AbstractInterpreterTest {
     Interpreter interpreter1 = interpreterSetting.getDefaultInterpreter("user1", "note1");
     RemoteInterpreter remoteInterpreter1 = (RemoteInterpreter) interpreter1;
     InterpreterContext context1 = InterpreterContext.builder()
-        .setNoteId("noteId")
-        .setParagraphId("paragraphId")
-        .build();
+            .setNoteId("noteId")
+            .setParagraphId("paragraphId")
+            .build();
     remoteInterpreter1.interpret("hello", context1);
 
     assertEquals(1, interpreterSettingManager.getRecoveryStorage().restore().size());
@@ -84,18 +83,18 @@ public class FileSystemRecoveryStorageTest extends AbstractInterpreterTest {
     Interpreter interpreter1 = interpreterSetting.getDefaultInterpreter("user1", "note1");
     RemoteInterpreter remoteInterpreter1 = (RemoteInterpreter) interpreter1;
     InterpreterContext context1 = InterpreterContext.builder()
-        .setNoteId("noteId")
-        .setParagraphId("paragraphId")
-        .build();
+            .setNoteId("noteId")
+            .setParagraphId("paragraphId")
+            .build();
     remoteInterpreter1.interpret("hello", context1);
     assertEquals(1, interpreterSettingManager.getRecoveryStorage().restore().size());
 
     Interpreter interpreter2 = interpreterSetting.getDefaultInterpreter("user2", "note2");
     RemoteInterpreter remoteInterpreter2 = (RemoteInterpreter) interpreter2;
     InterpreterContext context2 = InterpreterContext.builder()
-        .setNoteId("noteId")
-        .setParagraphId("paragraphId")
-        .build();
+            .setNoteId("noteId")
+            .setParagraphId("paragraphId")
+            .build();
     remoteInterpreter2.interpret("hello", context2);
 
     assertEquals(2, interpreterSettingManager.getRecoveryStorage().restore().size());
@@ -106,5 +105,4 @@ public class FileSystemRecoveryStorageTest extends AbstractInterpreterTest {
     interpreterSetting.close();
     assertEquals(0, interpreterSettingManager.getRecoveryStorage().restore().size());
   }
-
 }
