d7652919312c6fa705f8e8452cd853d98bcd6d32
Jeff Zhang <zjffdu@apache.org>
2020-08-11 22:52:45 +0800

    [ZEPPELIN-4990] execution.savepoint.path doesn't work in %flink.conf
    ### What is this PR for?
    
    This is a trivial PR which fix the issue of `execution.savepoint.path` doesn't work in `%flink.conf`. After this PR,
     we set `execution.savepoint.path` in the following order:
    
       * 1. Use savepoint path stored in paragraph config, this is recorded by zeppelin when paragraph is canceled,
       * 2. Use checkpoint path stored in pararaph config, this is recorded by zeppelin in flink job progress poller.
       * 3. Use local property 'execution.savepoint.path' if user set it.
       * 4. Otherwise remove 'execution.savepoint.path' when user didn't specify it in %flink.conf
    
    ### What type of PR is it?
    [Bug Fix | Improvement ]
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/ZEPPELIN-4990
    
    ### How should this be tested?
    * CI pass
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Jeff Zhang <zjffdu@apache.org>
    
    Closes #3876 from zjffdu/ZEPPELIN-4990 and squashes the following commits:
    
    a78f8c00d [Jeff Zhang] [ZEPPELIN-4990]. execution.savepoint.path doesn't work in %flink.conf
    365ecf815 [Jeff Zhang] [minor] change default value of resumeFromSavepoint to false
    
    (cherry picked from commit b3f5ee9fb698f67f579f2dbf8b512806f61bb5a9)
    Signed-off-by: Jeff Zhang <zjffdu@apache.org>

--- flink/interpreter/src/main/java/org/apache/zeppelin/flink/FlinkStreamSqlInterpreter.java
+++ flink/interpreter/src/main/java/org/apache/zeppelin/flink/FlinkStreamSqlInterpreter.java
@@ -56,9 +56,6 @@ public class FlinkStreamSqlInterpreter extends FlinkSqlInterrpeter {
 
   @Override
   public void callInnerSelect(String sql, InterpreterContext context) throws IOException {
-    flinkInterpreter.setSavePointIfNecessary(context);
-    flinkInterpreter.setParallelismIfNecessary(context);
-
     String streamType = context.getLocalProperties().get("type");
     if (streamType == null) {
       throw new IOException("type must be specified for stream sql");
