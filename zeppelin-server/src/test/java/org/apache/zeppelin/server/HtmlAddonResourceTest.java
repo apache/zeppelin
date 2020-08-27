cb06507f6323a9bf7a338ad0ffccc1adb9464c1f
Andreas Weise <a.weise@avm.de>
2020-08-27 17:05:47 +0200

    [ZEPPELIN-5019] Enable configurable HTML addons
    ### What is this PR for?
    Currently its hard to integrate certain plotting libraries in one single zeppelin notebook efficiently. efficiently here means in the sense of how often library code needs to be embedded into a single notebook. while few plotting libraries provide APIs to properly enable lazy loading of their JS library (e.g. bokeh), others do not (e.g. plotly), which results in large overhead in notebooks with couple of plots, where each plot embeds the library code again and again. attempts to only embed library code in the first paragraph is not feasible, as its not guaranteed, that this paragraph will be loaded at first by the browser. Any attempt to load libraries via paragraph code will end up in such synchronization issues when the library does not support synchronization on its own.
    
    A general solution for that problem is to enable the loading of 3rd party JS libraries within the root HTML.
    
    Hence this issue will introduce a new optional zeppelin configuration that allows administrators to add global addons to the zeppelin root HTML, so analysts can rely on that the JS library is already loaded for every paragraph.
    
    As there exist two places in HTML where initial loading may be placed, the issue will introduce the following two configuration properties:
    
    - `zeppelin.server.html.head.addon` > addon html code to be placed at the end of the html->head section 
    - `zeppelin.server.html.body.addon` > addon html code to be placed at the end of the html->body section
    
    Each property may contain any valid HTML code, e.g.
    
    ```
    <script defer src="https://url/to/my/lib.min.js" /><script defer src="https://url/to/other/lib.min.js" />
    ```
    
    ### What type of PR is it?
    Improvement
    
    ### What is the Jira issue?
    https://issues.apache.org/jira/browse/ZEPPELIN-5019
    
    ### How should this be tested?
    * Unit Test added for index.html of `zeppelin-web`
    * can also be manually tested by adding config properties mentioned above. Then check the resulting source of the zeppelin web application (old and new ui)
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? no
    * Is there breaking changes for older versions? no
    * Does this needs documentation? yes
    
    Author: Andreas Weise <a.weise@avm.de>
    
    Closes #3892 from weand/ZEPPELIN-5019 and squashes the following commits:
    
    29e7a0222 [Andreas Weise] ZEPPELIN-5019 Enable configurable HTML addons

---
+++ zeppelin-server/src/test/java/org/apache/zeppelin/server/HtmlAddonResourceTest.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.zeppelin.server;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.eclipse.jetty.util.resource.Resource;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.google.common.io.CharStreams;
+
+public class HtmlAddonResourceTest {
+
+    private final static String TEST_BODY_ADDON = "<!-- foo -->";
+    private final static String TEST_HEAD_ADDON = "<!-- bar -->";
+
+    private final static String FILE_PATH_INDEX_HTML_ZEPPELIN_WEB = "../zeppelin-web/dist/index.html";
+    private final static String FILE_PATH_INDEX_HTML_ZEPPELIN_WEB_ANGULAR = "../zeppelin-web-angular/dist/zeppelin/index.html";
+
+    @Test
+    public void testZeppelinWebHtmlAddon() throws IOException {
+        final Resource addonResource = getHtmlAddonResource(FILE_PATH_INDEX_HTML_ZEPPELIN_WEB);
+
+        final String content;
+        try (final Reader reader = new InputStreamReader(addonResource.getInputStream())) {
+            content = CharStreams.toString(reader);
+        }
+
+        assertThat(content, containsString(TEST_BODY_ADDON));
+        assertThat(content, containsString(TEST_HEAD_ADDON));
+
+    }
+
+    @Test
+    @Ignore // ignored due to zeppelin-web-angular not build for core tests
+    public void testZeppelinWebAngularHtmlAddon() throws IOException {
+        final Resource addonResource = getHtmlAddonResource(FILE_PATH_INDEX_HTML_ZEPPELIN_WEB_ANGULAR);
+
+        final String content;
+        try (final Reader reader = new InputStreamReader(addonResource.getInputStream())) {
+            content = CharStreams.toString(reader);
+        }
+
+        assertThat(content, containsString(TEST_BODY_ADDON));
+        assertThat(content, containsString(TEST_HEAD_ADDON));
+
+    }
+
+    private Resource getHtmlAddonResource(final String indexHtmlPath) {
+        return getHtmlAddonResource(indexHtmlPath, TEST_BODY_ADDON, TEST_HEAD_ADDON);
+    }
+
+    private Resource getHtmlAddonResource(final String indexHtmlPath, final String bodyAddon, final String headAddon) {
+        final Resource indexResource = Resource.newResource(new File(indexHtmlPath));
+        return new HtmlAddonResource(indexResource, TEST_BODY_ADDON, TEST_HEAD_ADDON);
+    }
+
+}
