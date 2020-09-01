3b994f35e039f741afd5fe5345d2974586ece81a
Philipp Dallig <philipp.dallig@gmail.com>
2020-09-01 14:21:26 +0200

    [ZEPPELIN-5024] Improve Travis-CI
    ### What is this PR for?
    Travis-CI:
     - remove all environment variables from the build matrix, to use the same cache for all jobs
     - activate the mysql service only when this service is needed
     - activate the xvfb service, when is necessary and possible
     - removed Bower Caching to remove too many complicated lines in Travis-ci
     - Giving the test names
     - Installing R only once with conda, previously it was installed twice with 'testing/install_R.sh' and 'testing/install_external_dependencies.sh
     - remove the R-Cache, because the installation with conda is quite fast
     - Delete 'test/install_R.sh' because it is no longer used
    
    Other:
     - Ignore the tests in 'HeliumApplicationFactoryTest.java' to get the JUnit tests running in the IDE + remove exclude in 'travis.yml
     - Remove deprecation warning in maven-surefire-plugin
     - Helium works better with an absolute path, because a relative path in PATH, is not a good idea for local testing
     - Remove JVM language dependent asserts
    
    ### What type of PR is it?
    Improvement
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/ZEPPELIN-5024
    
    ### How should this be tested?
    * Travis-CI: https://travis-ci.org/github/Reamer/zeppelin/builds/723116251
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Philipp Dallig <philipp.dallig@gmail.com>
    
    Closes #3897 from Reamer/test_refactoring and squashes the following commits:
    
    5f0dc196d [Philipp Dallig] Remove zeppelin artifacts from m2 cache
    e44957dee [Philipp Dallig] Remove old install_R.sh
    cb0ee7ff2 [Philipp Dallig] Install R with conda, because then we need no sudo rights
    b57906b43 [Philipp Dallig] Remove Spark distribution from cache, because the version is differs between tests
    b2995a181 [Philipp Dallig] Give specific jobs an unique cache
    837ff9d7f [Philipp Dallig] Remove env in each job to use the same cache
    4153765fb [Philipp Dallig] Remove bower_components cache
    7ae02b19d [Philipp Dallig] Only run mysql, if we need mysql for integration tests
    d70e2011a [Philipp Dallig] Use service for xvfb - display tests
    e0a6ba3db [Philipp Dallig] Naming build jobs
    ab1694e56 [Philipp Dallig] forkMode is deprecated, migrate to new properties
    f0fe75167 [Philipp Dallig] The String "command not found" and "No such file or directory" depends on your JVM language.
    c0b807104 [Philipp Dallig] Disable HeliumApplicationFactoryTest in SourceCode
    2b13a0e99 [Philipp Dallig] The maven-frontend-plugin works better with an absolute path. A relative path in the PATH environment variable can be problematic.

--- testing/install_external_dependencies.sh
+++ testing/install_external_dependencies.sh
@@ -20,20 +20,23 @@
 set -ev
 touch ~/.environ
 
-# Install Python dependencies for Python specific tests
+# Install conda for Python and R dependencies
 if [[ -n "$PYTHON" ]] ; then
   wget "https://repo.continuum.io/miniconda/Miniconda${PYTHON}-4.6.14-Linux-x86_64.sh" -O miniconda.sh
+else
+  wget "https://repo.continuum.io/miniconda/Miniconda3-4.6.14-Linux-x86_64.sh" -O miniconda.sh
+fi
+bash miniconda.sh -b -p "$HOME/miniconda"
+rm -fv miniconda.sh
+echo "export PATH='$HOME/miniconda/bin:$PATH'" >> ~/.environ
+source ~/.environ
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda config --add channels conda-forge
 
-  bash miniconda.sh -b -p "$HOME/miniconda"
-  echo "export PATH='$HOME/miniconda/bin:$PATH'" >> ~/.environ
-  source ~/.environ
-
-  hash -r
-  conda config --set always_yes yes --set changeps1 no
-  conda update -q conda
-  conda info -a
-  conda config --add channels conda-forge
-
+if [[ -n "$PYTHON" ]] ; then
   if [[ "$PYTHON" == "2" ]] ; then
     pip install -q numpy==1.14.5 pandas==0.21.1 matplotlib==2.1.1 scipy==1.2.1 grpcio==1.19.0 bkzep==0.6.1 hvplot==0.5.2 \
     protobuf==3.7.0 pandasql==0.7.3 ipython==5.8.0 ipykernel==4.10.0 bokeh==1.3.4 panel==0.6.0 holoviews==1.12.3
@@ -56,21 +59,8 @@ fi
 
 # Install R dependencies if R is true
 if [[ "$R" == "true" ]] ; then
-  echo "R_LIBS=~/R" > ~/.Renviron
-  echo "export R_LIBS=~/R" >> ~/.environ
-  source ~/.environ
-
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-  sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
-  sudo apt-get update
-  sudo apt-get install r-base
-
-  mkdir -p ~/R
-  R -e "install.packages('evaluate', repos = 'https://cloud.r-project.org', lib='~/R')"  > /dev/null 2>&1
-  R -e "install.packages('base64enc', repos = 'https://cloud.r-project.org', lib='~/R')"  > /dev/null 2>&1
-  R -e "install.packages('knitr', repos = 'https://cloud.r-project.org', lib='~/R')"  > /dev/null 2>&1
-  R -e "install.packages('ggplot2', repos = 'https://cloud.r-project.org', lib='~/R')"  > /dev/null 2>&1
-  R -e "install.packages('IRkernel', repos = 'https://cloud.r-project.org', lib='~/R');IRkernel::installspec()" > /dev/null 2>&1
-  R -e "install.packages('shiny', repos = 'https://cloud.r-project.org', lib='~/R')" > /dev/null 2>&1
-  R -e "install.packages('googleVis', repos = 'https://cloud.r-project.org', lib='~/R')" > /dev/null 2>&1
+  conda install -y --quiet r-base r-evaluate r-base64enc r-knitr r-ggplot2 r-irkernel r-shiny r-googlevis
+  R -e "IRkernel::installspec()"
+  echo "R_LIBS=~/miniconda/lib/R/library" > ~/.Renviron
+  echo "export R_LIBS=~/miniconda/lib/R/library" >> ~/.environ
 fi
