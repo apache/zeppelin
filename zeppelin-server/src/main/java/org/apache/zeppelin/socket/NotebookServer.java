aa4d0797bc7be6e300cdb540246ffaa76a695a5d
Timo Olkkonen <timo.p.olkkonen@gmail.com>
2020-10-11 07:32:27 +0300

    Add comment on why we need to broadcast paragraph states after failed runall.

--- zeppelin-server/src/main/java/org/apache/zeppelin/socket/NotebookServer.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/socket/NotebookServer.java
@@ -1490,6 +1490,8 @@ public class NotebookServer extends WebSocketServlet
 
     if (!getNotebookService().runAllParagraphs(noteId, paragraphs, getServiceContext(fromMessage),
         new WebSocketServiceCallback<Paragraph>(conn))) {
+      // If one paragraph fails, we need to broadcast paragraph states to the client,
+      // or paragraphs not run will stay in PENDING state.
       Note note = getNotebookService().getNote(noteId, getServiceContext(fromMessage), new SimpleServiceCallback());
       if (note != null) {
         for (Paragraph p : note.getParagraphs()) {
