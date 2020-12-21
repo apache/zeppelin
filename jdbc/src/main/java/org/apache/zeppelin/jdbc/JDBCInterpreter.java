364453879cbac7c5ff886dfa71c01b3f3168dbc5
Jeff Zhang <zjffdu@apache.org>
2020-12-21 18:15:52 +0800

    [ZEPPELIN-5169]. Hive set statement doesn't work for some hive version when there's empty line ahead

--- jdbc/src/main/java/org/apache/zeppelin/jdbc/JDBCInterpreter.java
+++ jdbc/src/main/java/org/apache/zeppelin/jdbc/JDBCInterpreter.java
@@ -717,6 +717,10 @@ public class JDBCInterpreter extends KerberosInterpreter {
       List<String>  sqlArray = sqlSplitter.splitSql(sql);
       for (String sqlToExecute : sqlArray) {
         LOGGER.info("Execute sql: " + sqlToExecute);
+        if (sqlToExecute.trim().startsWith("set ")) {
+          // some version of hive doesn't work with set statement with empty line ahead.
+          sqlToExecute = sqlToExecute.trim();
+        }
         statement = connection.createStatement();
 
         // fetch n+1 rows in order to indicate there's more rows available (for large selects)
