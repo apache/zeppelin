3f49c78754c2815ca0c2fc2939e20313912ad7ba
Jeff Zhang <zjffdu@apache.org>
2020-12-31 22:26:23 +0800

    [ZEPPELIN-5185]. HiveMonitor thread is never finished sometimes

--- jdbc/src/main/java/org/apache/zeppelin/jdbc/hive/HiveUtils.java
+++ jdbc/src/main/java/org/apache/zeppelin/jdbc/hive/HiveUtils.java
@@ -71,11 +71,14 @@ public class HiveUtils {
     final ProgressBar progressBar = progressBarTemp;
     final long timeoutThreshold = Long.parseLong(
             jdbcInterpreter.getProperty("zeppelin.jdbc.hive.timeout.threshold", "" + 60 * 1000));
+    final long queryInterval = Long.parseLong(jdbcInterpreter.getProperty("zeppelin.jdbc.hive.monitor.query_interval",
+            DEFAULT_QUERY_PROGRESS_INTERVAL + ""));
     Thread thread = new Thread(() -> {
       boolean jobLaunched = false;
       long jobLastActiveTime = System.currentTimeMillis();
       while (hiveStmt.hasMoreLogs() && !Thread.interrupted()) {
         try {
+          Thread.sleep(queryInterval);
           List<String> logs = hiveStmt.getQueryLog();
           String logsOutput = StringUtils.join(logs, System.lineSeparator());
           LOGGER.debug("Hive job output: " + logsOutput);
@@ -122,10 +125,9 @@ public class HiveUtils {
               break;
             }
           }
-          // refresh logs every 1 second.
-          Thread.sleep(DEFAULT_QUERY_PROGRESS_INTERVAL);
         } catch (Exception e) {
-          LOGGER.warn("Fail to write output", e);
+          LOGGER.warn("Fail to monitor hive statement", e);
+          break;
         } finally {
           try {
             // Sometimes, maybe hiveStmt was closed unnormally, hiveStmt.hasMoreLogs() will be true,
