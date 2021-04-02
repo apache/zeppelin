034154206ce66b73a7439f7a7ed4102130ffd1ba
Jeff Zhang <zjffdu@apache.org>
2021-04-02 16:19:28 +0800

    [ZEPPELIN-5288]. Add rest api to reload note
    ### What is this PR for?
    
    Add simple rest api to reload note, Unit test is added.
    
    ### What type of PR is it?
    [ Improvement]
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    *https://issues.apache.org/jira/browse/ZEPPELIN-5288
    
    ### How should this be tested?
    * CI pass
    
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the licenses files need update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Jeff Zhang <zjffdu@apache.org>
    
    Closes #4079 from zjffdu/ZEPPELIN-5288 and squashes the following commits:
    
    1ee165850 [Jeff Zhang] address comment
    83dac3954 [Jeff Zhang] [ZEPPELIN-5288]. Add rest api to reload note

--- zeppelin-server/src/main/java/org/apache/zeppelin/rest/NotebookRestApi.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/rest/NotebookRestApi.java
@@ -327,15 +327,17 @@ public class NotebookRestApi extends AbstractRestApi {
    * Get note of this specified noteId.
    *
    * @param noteId
+   * @param reload
    * @return
    * @throws IOException
    */
   @GET
   @Path("{noteId}")
   @ZeppelinApi
-  public Response getNote(@PathParam("noteId") String noteId) throws IOException {
+  public Response getNote(@PathParam("noteId") String noteId,
+                          @QueryParam("reload") boolean reload) throws IOException {
     Note note =
-            notebookService.getNote(noteId, getServiceContext(), new RestServiceCallback());
+            notebookService.getNote(noteId, reload, getServiceContext(), new RestServiceCallback());
     return new JsonResponse<>(Status.OK, "", note).build();
   }
 
