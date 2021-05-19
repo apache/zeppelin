1d77c42cc3f137e041e5245630909e841618deb3
Philipp Dallig <philipp.dallig@gmail.com>
2021-05-19 08:11:27 +0200

    [ZEPPELIN-5420] Remove deprecated R module
    ### What is this PR for?
    This module removes the deprecated R module.
    
    ### What type of PR is it?
    -  Refactoring
    
    ### What is the Jira issue?
    * https://issues.apache.org/jira/browse/ZEPPELIN-5420
    
    ### Questions:
    * Does the licenses files need update? Yes
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    Author: Philipp Dallig <philipp.dallig@gmail.com>
    
    Closes #4145 from Reamer/remove_r_module and squashes the following commits:
    
    70153aa2f [Philipp Dallig] Remove deprecated R module

--- r/R/rzeppelin/R/zzz.R
+++
@@ -1,9 +0,0 @@
-typeMap <- list()
-typeMap[[INTEGER]] <- integer(0)
-typeMap[[DOUBLE]] <- double(0)
-typeMap[[BOOLEAN]] <- integer(0)
-typeMap[[STRING]] <- character(0)
-
-.onAttach <- function(libname, pkgname) {
-
-}
