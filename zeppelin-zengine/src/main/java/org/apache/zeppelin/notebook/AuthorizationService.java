b2b2952957093100da193f1a70d47c889284d799
Jaehoon Jeong <jaehoon.jeong@woowahan.com>
2021-08-06 01:13:29 +0900

    zeppelin-zengine: block notebook listed when user didn't have owner/reader/admin

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/AuthorizationService.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/AuthorizationService.java
@@ -273,8 +273,6 @@ public class AuthorizationService implements ClusterEventListener {
   public boolean isReader(String noteId, Set<String> entities) {
     return isMember(entities, getReaders(noteId)) ||
             isMember(entities, getOwners(noteId)) ||
-            isMember(entities, getWriters(noteId)) ||
-            isMember(entities, getRunners(noteId)) ||
             isAdmin(entities);
   }
 
