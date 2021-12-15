8f6fd480195625515530f454a9c701f6042f06e5
jasmine-song <791259487@qq.com>
2021-12-15 14:12:39 +0800

    clear output don't reload

--- zeppelin-web-angular/src/app/core/paragraph-base/paragraph-base.ts
+++ zeppelin-web-angular/src/app/core/paragraph-base/paragraph-base.ts
@@ -103,6 +103,12 @@ export abstract class ParagraphBase extends MessageListenersManager {
   paragraphData(data: MessageReceiveDataTypeMap[OP.PARAGRAPH]) {
     const oldPara = this.paragraph;
     const newPara = data.paragraph;
+    if (!newPara.results) {
+      newPara.results = {};
+    }
+    if (!newPara.results.msg) {
+      newPara.results.msg = [];
+    }
     if (this.isUpdateRequired(oldPara, newPara)) {
       this.updateParagraph(oldPara, newPara, () => {
         if (newPara.results && newPara.results.msg) {
