3fed1f9c582c93da3a6b061fb975afbbdf505cef
BaptisteMeynier <baptiste.meynier@gmail.com>
2022-01-08 14:15:58 +0100

    ZEPPELIN-5626: Correct style

--- elasticsearch/src/main/java/org/apache/zeppelin/elasticsearch/client/ElasticsearchClientType.java
+++ elasticsearch/src/main/java/org/apache/zeppelin/elasticsearch/client/ElasticsearchClientType.java
@@ -1,15 +1,15 @@
 package org.apache.zeppelin.elasticsearch.client;
 
 public enum ElasticsearchClientType {
-    HTTP(true), HTTPS(true), TRANSPORT(false), UNKNOWN(false);
+  HTTP(true), HTTPS(true), TRANSPORT(false), UNKNOWN(false);
 
-    private final boolean isHttp;
+  private final boolean isHttp;
 
-    ElasticsearchClientType(boolean isHttp) {
+  ElasticsearchClientType(boolean isHttp) {
         this.isHttp = isHttp;
     }
 
-    public boolean isHttp() {
+  public boolean isHttp() {
         return isHttp;
     }
 }
--- elasticsearch/src/main/java/org/apache/zeppelin/elasticsearch/client/ElasticsearchClientTypeBuilder.java
+++ elasticsearch/src/main/java/org/apache/zeppelin/elasticsearch/client/ElasticsearchClientTypeBuilder.java
@@ -6,35 +6,35 @@ import java.util.Arrays;
 
 public class ElasticsearchClientTypeBuilder {
 
-    private static final ElasticsearchClientType DEFAULT_ELASTICSEARCH_CLIENT_TYPE = ElasticsearchClientType.TRANSPORT;
+  private static final ElasticsearchClientType DEFAULT_ELASTICSEARCH_CLIENT_TYPE = ElasticsearchClientType.TRANSPORT;
 
-    public static Build withPropertyValue(String propertyValue) {
-        return new Builder(propertyValue);
-    }
+  public static Build withPropertyValue(String propertyValue) {
+    return new Builder(propertyValue);
+  }
 
-    public interface Build {
-        ElasticsearchClientType build();
-    }
+  public interface Build {
+    ElasticsearchClientType build();
+  }
 
-    private static class Builder implements Build {
-        private final String propertyValue;
+  private static class Builder implements Build {
+    private final String propertyValue;
 
-        private Builder(String propertyValue) {
+    private Builder(String propertyValue) {
             this.propertyValue = propertyValue;
         }
 
-        @Override
-        public ElasticsearchClientType build() {
-            boolean isEmpty = StringUtils.isEmpty(propertyValue);
-            return isEmpty ? DEFAULT_ELASTICSEARCH_CLIENT_TYPE : getElasticsearchClientType(propertyValue);
-        }
+    @Override
+    public ElasticsearchClientType build() {
+      boolean isEmpty = StringUtils.isEmpty(propertyValue);
+      return isEmpty ? DEFAULT_ELASTICSEARCH_CLIENT_TYPE : getElasticsearchClientType(propertyValue);
+    }
 
-        private ElasticsearchClientType getElasticsearchClientType(String propertyValue){
-            boolean isExistingValue =
+    private ElasticsearchClientType getElasticsearchClientType(String propertyValue){
+      boolean isExistingValue =
                     Arrays
                             .stream(ElasticsearchClientType.values())
                             .anyMatch(clientType -> clientType.toString().equalsIgnoreCase(propertyValue));
-            return isExistingValue ? ElasticsearchClientType.valueOf(propertyValue.toUpperCase()) : ElasticsearchClientType.UNKNOWN;
-        }
+      return isExistingValue ? ElasticsearchClientType.valueOf(propertyValue.toUpperCase()) : ElasticsearchClientType.UNKNOWN;
     }
+  }
 }
