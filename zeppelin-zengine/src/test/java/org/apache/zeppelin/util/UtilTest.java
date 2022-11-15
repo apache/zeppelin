e96880f5ec8c4f7a99ad710a8f97ef461129c55f
Guanhua Li <guanhua_li@foxmail.com>
2022-11-15 15:24:43 +0800

    [ZEPPELIN-5612] Upgrade junit 4 to 5 (#4321)
    * [ZEPPELIN-5612] Upgrade junit 4 to 5
    
    * Refactor `UtilTest.java` with jUnit 5 as an example.
    
    * update for review

--- zeppelin-zengine/src/test/java/org/apache/zeppelin/util/UtilTest.java
+++ zeppelin-zengine/src/test/java/org/apache/zeppelin/util/UtilTest.java
@@ -17,19 +17,19 @@
 
 package org.apache.zeppelin.util;
 
-import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class UtilTest {
 
     @Test
     public void getVersionTest() {
-        assertNotNull(Util.getVersion());
+        Assertions.assertNotNull(Util.getVersion());
     }
 
     @Test
     public void getGitInfoTest() {
-        assertNotNull(Util.getGitCommitId());
-        assertNotNull(Util.getGitTimestamp());
+        Assertions.assertNotNull(Util.getGitCommitId());
+        Assertions.assertNotNull(Util.getGitTimestamp());
     }
 }
