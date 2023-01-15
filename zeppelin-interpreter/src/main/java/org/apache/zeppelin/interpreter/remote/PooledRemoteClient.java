3230f0a6dfa331cea4ce150c98182b73ad87c3e6
Jongyoul Lee <jongyoul@gmail.com>
2023-01-15 20:34:33 +0900

    [ZEPPELIN-5875] Add comments for changes

--- zeppelin-interpreter/src/main/java/org/apache/zeppelin/interpreter/remote/PooledRemoteClient.java
+++ zeppelin-interpreter/src/main/java/org/apache/zeppelin/interpreter/remote/PooledRemoteClient.java
@@ -45,6 +45,8 @@ public class PooledRemoteClient<T extends TServiceClient> implements AutoCloseab
     GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
     poolConfig.setMaxTotal(connectionPoolSize);
     poolConfig.setMaxIdle(connectionPoolSize);
+    // ZEPPELIN-5875 maven-shade-plugin issue
+    // `org/apache/zeppelin/shaded/org.apache.zeppelin.shaded.org.apache.commons.pool2.impl.DefaultEvictionPolicy`
     poolConfig.setEvictionPolicyClassName(DefaultEvictionPolicy.class.getName());
     this.clientPool = new GenericObjectPool<>(remoteClientFactory, poolConfig);
   }
