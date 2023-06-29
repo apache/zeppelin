02cbd6ee2425f0be87dee1b444580b0c2cb7e9ce
Jongyoul Lee <jongyoul@gmail.com>
2023-06-29 16:18:12 +0900

    [HOTFIX] Change the location of bank.csv to fix the integration tests (#4623)

--- livy/src/test/resources/livy_tutorial_1.scala
+++ livy/src/test/resources/livy_tutorial_1.scala
@@ -8,7 +8,7 @@ import java.nio.charset.Charset
 // load bank data
 val bankText = sc.parallelize(
   IOUtils.toString(
-    new URL("https://s3.amazonaws.com/apache-zeppelin/tutorial/bank/bank.csv"),
+    new URL("https://raw.githubusercontent.com/apache/zeppelin/master/testing/resources/bank.csv"),
     Charset.forName("utf8")).split("\n"))
 
 case class Bank(age: Integer, job: String, marital: String, education: String, balance: Integer)
--- zeppelin-integration/src/test/java/org/apache/zeppelin/integration/SparkParagraphIT.java
+++ zeppelin-integration/src/test/java/org/apache/zeppelin/integration/SparkParagraphIT.java
@@ -68,7 +68,7 @@ public class SparkParagraphIT extends AbstractZeppelinIT {
       import org.apache.commons.io.IOUtils
       import java.net.URL
       import java.nio.charset.Charset
-      val bankText = sc.parallelize(IOUtils.toString(new URL("https://s3.amazonaws.com/apache-zeppelin/tutorial/bank/bank.csv"),Charset.forName("utf8")).split("\n"))
+      val bankText = sc.parallelize(IOUtils.toString(new URL("https://raw.githubusercontent.com/apache/zeppelin/master/testing/resources/bank.csv"),Charset.forName("utf8")).split("\n"))
       case class Bank(age: Integer, job: String, marital: String, education: String, balance: Integer)
 
       val bank = bankText.map(s => s.split(";")).filter(s => s(0) != "\"age\"").map(s => Bank(s(0).toInt,s(1).replaceAll("\"", ""),s(2).replaceAll("\"", ""),s(3).replaceAll("\"", ""),s(5).replaceAll("\"", "").toInt)).toDF()
@@ -77,7 +77,7 @@ public class SparkParagraphIT extends AbstractZeppelinIT {
       setTextOfParagraph(2, "import org.apache.commons.io.IOUtils\\n" +
           "import java.net.URL\\n" +
           "import java.nio.charset.Charset\\n" +
-          "val bankText = sc.parallelize(IOUtils.toString(new URL(\"https://s3.amazonaws.com/apache-zeppelin/tutorial/bank/bank.csv\"),Charset.forName(\"utf8\")).split(\"\\\\n\"))\\n" +
+          "val bankText = sc.parallelize(IOUtils.toString(new URL(\"https://raw.githubusercontent.com/apache/zeppelin/master/testing/resources/bank.csv\"),Charset.forName(\"utf8\")).split(\"\\\\n\"))\\n" +
           "case class Bank(age: Integer, job: String, marital: String, education: String, balance: Integer)\\n" +
           "\\n" +
           "val bank = bankText.map(s => s.split(\";\")).filter(s => s(0) != \"\\\\\"age\\\\\"\").map(s => Bank(s(0).toInt,s(1).replaceAll(\"\\\\\"\", \"\"),s(2).replaceAll(\"\\\\\"\", \"\"),s(3).replaceAll(\"\\\\\"\", \"\"),s(5).replaceAll(\"\\\\\"\", \"\").toInt)).toDF()\\n" +
