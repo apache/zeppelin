e65b5430e43c076c138a1f56e3f2aba1324118f2
Jongyoul Lee <jongyoul@gmail.com>
2024-02-19 21:44:39 +0900

    [ZEPPELIN-5990] Disable sensitive configuration for JDBC url (#4709)
    * [ZEPPELIN-5990] Disable sensitive configuration for JDBC url
    
    * [ZEPPELIN-5990] Disable sensitive configuration for JDBC url

--- jdbc/src/test/java/org/apache/zeppelin/jdbc/JDBCInterpreterTest.java
+++ jdbc/src/test/java/org/apache/zeppelin/jdbc/JDBCInterpreterTest.java
@@ -747,6 +747,21 @@ public class JDBCInterpreterTest extends BasicJDBCTestCaseAdapter {
     assertEquals(3, resultMessages.size());
   }
 
+  @Test
+  void testValidateConnectionUrl() throws IOException, InterpreterException {
+    Properties properties = new Properties();
+    properties.setProperty("default.driver", "org.h2.Driver");
+    properties.setProperty("default.url", getJdbcConnection() + ";allowLoadLocalInfile=true");
+    properties.setProperty("default.user", "");
+    properties.setProperty("default.password", "");
+    JDBCInterpreter jdbcInterpreter = new JDBCInterpreter(properties);
+    jdbcInterpreter.open();
+    InterpreterResult interpreterResult = jdbcInterpreter.interpret("SELECT 1", context);
+    assertEquals(InterpreterResult.Code.ERROR, interpreterResult.code());
+    assertEquals("Connection URL contains improper configuration",
+            interpreterResult.message().get(0).getData());
+  }
+
   private InterpreterContext getInterpreterContext() {
     return InterpreterContext.builder()
             .setAuthenticationInfo(new AuthenticationInfo("testUser"))
