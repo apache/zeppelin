9ed3540863746eed0547b4cc7486426a78f056b7
Chan Ho Lee <chanho0325@gmail.com>
2024-08-27 23:27:38 +0900

    Change configuration name (zeppelin.angular.war -> zeppelin.war)

--- zeppelin-server/src/main/java/org/apache/zeppelin/server/ZeppelinServer.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/server/ZeppelinServer.java
@@ -226,7 +226,7 @@ public class ZeppelinServer implements AutoCloseable {
         });
 
     // Multiple Web UI
-    final WebAppContext defaultWebApp = setupWebAppContext(contexts, zConf, zConf.getString(ConfVars.ZEPPELIN_ANGULAR_WAR), zConf.getServerContextPath());
+    final WebAppContext defaultWebApp = setupWebAppContext(contexts, zConf, zConf.getString(ConfVars.ZEPPELIN_WAR), zConf.getServerContextPath());
     final WebAppContext classicWebApp = setupWebAppContext(contexts, zConf, zConf.getString(ConfVars.ZEPPELIN_CLASSIC_WAR), WEB_APP_CONTEXT_CLASSIC);
 
     initWebApp(defaultWebApp);
