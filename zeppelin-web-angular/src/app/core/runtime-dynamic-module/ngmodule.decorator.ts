3c2b0fc478d21d6f0de190da05783cd15430ff1c
Chan Ho Lee <chanho0325@gmail.com>
2024-09-03 01:00:03 +0900

    Add comment

--- zeppelin-web-angular/src/app/core/runtime-dynamic-module/ngmodule.decorator.ts
+++ zeppelin-web-angular/src/app/core/runtime-dynamic-module/ngmodule.decorator.ts
@@ -1,2 +1,4 @@
+// importing NgModule and Component from @angular/core
+// and re-exporting them to prevent AOT compiler from removing them
 import { Component, NgModule } from '@angular/core';
 export { NgModule, Component };
