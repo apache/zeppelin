12dfddcfc9c1f2d2088405535828fc0df0537c43
Aman Singh Chauhan <aman.chauhan@meesho.com>
2024-10-10 13:53:11 +0530

    updated Gson object

--- jdbc/src/main/java/org/apache/zeppelin/jdbc/ValidationResponse.java
+++ jdbc/src/main/java/org/apache/zeppelin/jdbc/ValidationResponse.java
@@ -1,6 +1,7 @@
 package org.apache.zeppelin.jdbc;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class ValidationResponse {
@@ -46,19 +47,28 @@ public class ValidationResponse {
         Gson gson = new Gson();
         ValidationResponse response = new ValidationResponse();
 
-        JsonObject jsonObject = gson.fromJson(jsonResponse, JsonObject.class);
+        JsonElement jsonElement = gson.fromJson(jsonResponse, JsonElement.class);
 
-        if (jsonObject.has("pre_submit_fail") && !jsonObject.get("pre_submit_fail").isJsonNull()) {
-            response.setPreSubmitFail(jsonObject.get("pre_submit_fail").getAsBoolean());
-        }
-        if (jsonObject.has("fail_fast") && !jsonObject.get("fail_fast").isJsonNull()) {
-            response.setFailFast(jsonObject.get("fail_fast").getAsBoolean());
-        }
-        if (jsonObject.has("failed_by_deprecated_table") && !jsonObject.get("failed_by_deprecated_table").isJsonNull()) {
-            response.setFailedByDeprecatedTable(jsonObject.get("failed_by_deprecated_table").getAsBoolean());
-        }
-        if (jsonObject.has("message") && !jsonObject.get("message").isJsonNull()) {
-            response.setMessage(jsonObject.get("message").getAsString());
+        if (jsonElement.isJsonObject()) {
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+            if (jsonObject.has("pre_submit_fail") && !jsonObject.get("pre_submit_fail").isJsonNull()) {
+                response.setPreSubmitFail(jsonObject.get("pre_submit_fail").getAsBoolean());
+            }
+            if (jsonObject.has("fail_fast") && !jsonObject.get("fail_fast").isJsonNull()) {
+                response.setFailFast(jsonObject.get("fail_fast").getAsBoolean());
+            }
+            if (jsonObject.has("failed_by_deprecated_table") && !jsonObject.get("failed_by_deprecated_table").isJsonNull()) {
+                response.setFailedByDeprecatedTable(jsonObject.get("failed_by_deprecated_table").getAsBoolean());
+            }
+            if (jsonObject.has("message") && !jsonObject.get("message").isJsonNull()) {
+                response.setMessage(jsonObject.get("message").getAsString());
+            }
+        } else {
+            response.setPreSubmitFail(false);
+            response.setFailFast(false);
+            response.setFailedByDeprecatedTable(false);
+            response.setMessage(""); // Default message
         }
         return response;
     }
