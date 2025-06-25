810ffcfb22fca73e28a530bece5c4e1beaf58204
PJ Fanning <pjfanning@users.noreply.github.com>
2025-06-25 15:21:07 +0100

    line too long

--- jdbc/src/test/java/org/apache/zeppelin/jdbc/JDBCInterpreterTest.java
+++ jdbc/src/test/java/org/apache/zeppelin/jdbc/JDBCInterpreterTest.java
@@ -781,7 +781,8 @@ public class JDBCInterpreterTest extends BasicJDBCTestCaseAdapter {
   void testValidateConnectionH2UrlWithInit() throws IOException, InterpreterException {
     Properties properties = new Properties();
     properties.setProperty("default.driver", "org.h2.Driver");
-    properties.setProperty("default.url", getJdbcConnection() + ";INIT=RUNSCRIPT FROM 'http://localhost/init.sql'");
+    properties.setProperty("default.url", getJdbcConnection() +
+      ";INIT=RUNSCRIPT FROM 'http://localhost/init.sql'");
     properties.setProperty("default.user", "");
     properties.setProperty("default.password", "");
     JDBCInterpreter jdbcInterpreter = new JDBCInterpreter(properties);
