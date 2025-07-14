b5a20773a2043c016ecdcda5a748b87cfbb1b0ca
proceane <proceane1@gmail.com>
2025-07-14 16:32:23 +0900

    [ZEPPELIN-6237] Add shutdown in ZeppelinServer

--- zeppelin-server/src/main/java/org/apache/zeppelin/server/ZeppelinServer.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/server/ZeppelinServer.java
@@ -364,6 +364,7 @@ public class ZeppelinServer implements AutoCloseable {
         if (sharedServiceLocator != null) {
           if (!zConf.isRecoveryEnabled()) {
             sharedServiceLocator.getService(InterpreterSettingManager.class).close();
+            sharedServiceLocator.shutdown();
           }
           sharedServiceLocator.getService(Notebook.class).close();
         }
