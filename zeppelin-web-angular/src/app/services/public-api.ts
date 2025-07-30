948d9b9e0921a9101360dbd1b4935664f4a5d8e3
Chan Ho Lee <chanho0325@gmail.com>
2025-07-30 22:58:53 +0900

    Export `ClassicVisualizationService` in public API

--- zeppelin-web-angular/src/app/services/public-api.ts
+++ zeppelin-web-angular/src/app/services/public-api.ts
@@ -31,3 +31,4 @@ export * from './configuration.service';
 export * from './credential.service';
 export * from './notebook-repos.service';
 export * from './angular-drag-drop.service';
+export * from './classic-visualization.service';
