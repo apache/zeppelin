ad6f4892ab87b06d2f8e78653d36ef91d0e84e4b
proceane <proceane1@gmail.com>
2025-08-10 22:33:58 +0900

    [ZEPPELIN-6273] Conform AngularInterpreterTest to Checkstyle(AvoidStarImport, NewlineAtEndOfFile)

--- angular/src/test/java/org/apache/zeppelin/angular/AngularInterpreterTest.java
+++ angular/src/test/java/org/apache/zeppelin/angular/AngularInterpreterTest.java
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Properties;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AngularInterpreterTest {
 
