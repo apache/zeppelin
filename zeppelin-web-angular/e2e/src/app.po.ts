f7fedaa10c7d4ae124a74a3f523c9ee0f641dd3e
SeungYoung Oh <seung-00@naver.com>
2025-08-21 01:00:28 +0900

    [ZEPPELIN-6287] Fix lint errors in zeppelin-web-angular modules

--- zeppelin-web-angular/e2e/src/app.po.ts
+++ zeppelin-web-angular/e2e/src/app.po.ts
@@ -14,7 +14,7 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+    return browser.get(browser.baseUrl);
   }
 
   getTitleText() {
