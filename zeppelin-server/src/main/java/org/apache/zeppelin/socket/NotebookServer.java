8d73bfc50c5746913b762a5d83acf6d9628efc10
eunhwa99 <68810660+eunhwa99@users.noreply.github.com>
2025-08-26 22:59:11 +0900

    [ZEPPELIN-6300] Replace deprecated StringUtils.equals to Strings.CS.equals
    ### What is this PR for?
    Replaces deprecated `StringUtils.equals` with `Strings.CS.equals` in `NotebookServer` for null-safe comparison.
    
    ### What type of PR is it?
    Refactoring
    
    ### Todos
    * [ ] - Task
    
    ### What is the Jira issue?
    * [ZEPPELIN-6300](https://issues.apache.org/jira/browse/ZEPPELIN-6300)
    
    ### How should this be tested?
    * Run existing unit tests for `NotebookServer` to confirm behavior is unchanged. 
    ### Screenshots (if appropriate)
    
    ### Questions:
    * Does the license files need to update? No
    * Is there breaking changes for older versions? No
    * Does this needs documentation? No
    
    
    Closes #5049 from eunhwa99/ZEPPELIN-6300.
    
    Signed-off-by: Philipp Dallig <philipp.dallig@gmail.com>

--- zeppelin-server/src/main/java/org/apache/zeppelin/socket/NotebookServer.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/socket/NotebookServer.java
@@ -51,6 +51,7 @@ import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Strings;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.thrift.TException;
 import org.apache.zeppelin.common.Message;
@@ -812,8 +813,8 @@ public class NotebookServer implements AngularObjectRegistryListener,
 
       List<AngularObject> angularObjects = note.getAngularObjects(interpreterGroup.getId());
       for (AngularObject ao : angularObjects) {
-        if (StringUtils.equals(ao.getNoteId(), note.getId())
-            && StringUtils.equals(ao.getParagraphId(), paragraph.getId())) {
+        if (Strings.CS.equals(ao.getNoteId(), note.getId())
+            && Strings.CS.equals(ao.getParagraphId(), paragraph.getId())) {
           pushAngularObjectToRemoteRegistry(ao.getNoteId(), ao.getParagraphId(),
               ao.getName(), ao.get(), registry, interpreterGroup.getId(), conn);
         }
