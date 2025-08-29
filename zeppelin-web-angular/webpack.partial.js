77393e471db57d230b999979a7161f48e8e5344a
YONGJAE LEE <dev.yongjaelee@gmail.com>
2025-08-30 00:19:44 +0900

    [ZEPPELIN-6305] chore: apply npm run lint:fix

--- zeppelin-web-angular/webpack.partial.js
+++ zeppelin-web-angular/webpack.partial.js
@@ -16,12 +16,42 @@ module.exports = {
   plugins: [
     new MonacoWebpackPlugin({
       languages: [
-        'bat', 'cpp', 'csharp', 'csp', 'css', 'dockerfile', 'go', 'handlebars', 'html',  'java', 'javascript', 'json',
-        'less', 'lua', 'markdown', 'mysql', 'objective', 'perl', 'pgsql', 'php', 'powershell', 'python', 'r', 'ruby',
-        'rust', 'scheme', 'scss', 'shell', 'sql', 'swift', 'typescript', 'vb', 'xml', 'yaml'
+        'bat',
+        'cpp',
+        'csharp',
+        'csp',
+        'css',
+        'dockerfile',
+        'go',
+        'handlebars',
+        'html',
+        'java',
+        'javascript',
+        'json',
+        'less',
+        'lua',
+        'markdown',
+        'mysql',
+        'objective',
+        'perl',
+        'pgsql',
+        'php',
+        'powershell',
+        'python',
+        'r',
+        'ruby',
+        'rust',
+        'scheme',
+        'scss',
+        'shell',
+        'sql',
+        'swift',
+        'typescript',
+        'vb',
+        'xml',
+        'yaml'
       ],
       features: ['!accessibilityHelp']
     })
   ]
 };
-
