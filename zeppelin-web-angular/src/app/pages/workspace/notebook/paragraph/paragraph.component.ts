f49113f4da9fb7bf2ab25f42b90c95a39e81d2e0
YONGJAE LEE <dev.yongjaelee@gmail.com>
2025-09-04 22:08:18 +0900

    [ZEPPELIN-6298] fix: prevented blur when executing focusEditor in REMOVE_PARAGRAPH

--- zeppelin-web-angular/src/app/pages/workspace/notebook/paragraph/paragraph.component.ts
+++ zeppelin-web-angular/src/app/pages/workspace/notebook/paragraph/paragraph.component.ts
@@ -88,6 +88,8 @@ export class NotebookParagraphComponent extends ParagraphBase implements OnInit,
   private destroy$ = new Subject();
   private mode: Mode = 'command';
   waitConfirmFromEdit = false;
+  // Added a variable to prevent the automatically triggered blur action after executing focusEditor when receiving PARAGRAPH_REMOVED
+  ignoreBlur = false;
 
   switchMode(mode: Mode): void {
     if (mode === this.mode) {
@@ -138,6 +140,10 @@ export class NotebookParagraphComponent extends ParagraphBase implements OnInit,
   }
 
   blurEditor() {
+    if (this.ignoreBlur) {
+      this.ignoreBlur = false;
+      return;
+    }
     this.paragraph.focus = false;
     (this.host.nativeElement as HTMLElement).focus();
     this.saveParagraph();
@@ -149,6 +155,10 @@ export class NotebookParagraphComponent extends ParagraphBase implements OnInit,
   }
 
   onEditorBlur() {
+    if (this.ignoreBlur) {
+      this.notebookParagraphCodeEditorComponent?.setRestorePosition();
+      return;
+    }
     // Ignore events triggered by open the confirm box in edit mode
     if (!this.waitConfirmFromEdit) {
       this.switchMode('command');
