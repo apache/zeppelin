5ca3379a80ac6db33b5b01dc45347aabedc27ba6
ChanHo Lee <chanholee@apache.org>
2025-10-23 21:14:25 +0900

    Migrate eslint config to flat config

---
+++ zeppelin-web-angular/projects/zeppelin-sdk/eslint.config.mjs
@@ -0,0 +1,53 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default defineConfig([
+  globalIgnores(['!**/*']),
+  {
+    extends: compat.extends('../../.eslintrc.json')
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 5,
+      sourceType: 'script',
+      parserOptions: {
+        project: ['projects/zeppelin-sdk/tsconfig.lib.json', 'projects/zeppelin-sdk/tsconfig.spec.json'],
+        createDefaultProgram: true
+      }
+    },
+    rules: {
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'lib',
+          style: 'kebab-case'
+        }
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'lib',
+          style: 'camelCase'
+        }
+      ]
+    }
+  },
+  {
+    files: ['**/*.html'],
+    rules: {}
+  }
+]);
