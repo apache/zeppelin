dd6c2f2563f6152be135100ee8d6321a26f1b3cc
ChanHo Lee <chanholee@apache.org>
2026-05-30 19:22:54 +0900

    refactor(core): import DOCUMENT from @angular/core for Angular 20
    Auto-applied by 'ng update @angular/core --from=19 --to=20'.
    
    Angular 20 deprecates re-exporting DOCUMENT from @angular/common and
    exports it from @angular/core instead. The migration moves the import in
    the two services that inject it (shortcut.service, code-editor.service).
    Same token, no behavior change.

--- zeppelin-web-angular/src/app/services/shortcut.service.ts
+++ zeppelin-web-angular/src/app/services/shortcut.service.ts
@@ -10,8 +10,7 @@
  * limitations under the License.
  */
 
-import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, DOCUMENT } from '@angular/core';
 import { EventManager } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 
