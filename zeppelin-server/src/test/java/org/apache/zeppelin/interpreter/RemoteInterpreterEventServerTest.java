2589039030e859b4c5bcbd4095441c4622c806c2
gyowoo1113 <yereube@naver.com>
2026-07-26 20:13:23 +0900

    [ZEPPELIN-6543] Handle invokeMethod serialization failure as InterpreterRPCException

---
+++ zeppelin-server/src/test/java/org/apache/zeppelin/interpreter/RemoteInterpreterEventServerTest.java
@@ -0,0 +1,79 @@
+package org.apache.zeppelin.interpreter;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+import org.apache.zeppelin.conf.ZeppelinConfiguration;
+import org.apache.zeppelin.interpreter.remote.InvokeResourceMethodEventMessage;
+import org.apache.zeppelin.interpreter.remote.RemoteInterpreterProcess;
+import org.apache.zeppelin.interpreter.thrift.InterpreterRPCException;
+import org.apache.zeppelin.resource.Resource;
+import org.apache.zeppelin.resource.ResourceId;
+import org.junit.jupiter.api.Test;
+
+public class RemoteInterpreterEventServerTest {
+  
+  @Test
+  void invokeMethodThrowsRpcExceptionWhenSerializationFails() throws Exception {
+    ZeppelinConfiguration zConf = mock(ZeppelinConfiguration.class);
+    InterpreterSettingManager manager = mock(InterpreterSettingManager.class);
+    RemoteInterpreterEventServer server = new RemoteInterpreterEventServer(zConf, manager);
+
+    ManagedInterpreterGroup interpreterGroup = mock(ManagedInterpreterGroup.class);
+    RemoteInterpreterProcess remoteInterpreterProcess = mock(RemoteInterpreterProcess.class);
+      
+    when(manager.getInterpreterGroupById("pool-id"))
+        .thenReturn(interpreterGroup);
+    when(interpreterGroup.getRemoteInterpreterProcess())
+        .thenReturn(remoteInterpreterProcess);
+    when(remoteInterpreterProcess.isRunning())
+        .thenReturn(true);
+      
+    ByteBuffer remoteResult = Resource.serializeObject(new SerializableOnlyOnce());
+    doReturn(remoteResult)
+        .when(remoteInterpreterProcess)
+        .callRemoteFunction(any());
+      
+    ResourceId resourceId = ResourceId.fromJson(
+        "{\"resourcePoolId\":\"pool-id\",\"name\":\"resource-name\",\"noteId\":\"note-id\",\"paragraphId\":\"paragraph-id\"}"
+    );
+
+    InvokeResourceMethodEventMessage message = new InvokeResourceMethodEventMessage(
+        resourceId
+        , "someMethod"
+        , null
+        , null
+        , null);
+
+    InterpreterRPCException exception = assertThrows(
+        InterpreterRPCException.class,
+        () -> server.invokeMethod("caller-group-id", message.toJson()));
+      
+    assertTrue(exception.toString().contains("failed on second serialization"));
+  }
+  private static class SerializableOnlyOnce implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final int FAILURE_SERIALIZATION_COUNT = 2;
+
+    private int serializationCount;
+
+    private void writeObject(ObjectOutputStream outputStream) throws IOException {
+      serializationCount++;
+
+      if (serializationCount == FAILURE_SERIALIZATION_COUNT) {
+        throw new IOException("failed on second serialization");
+      }
+      
+      outputStream.defaultWriteObject();
+    }
+  }
+}
